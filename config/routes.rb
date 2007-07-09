4ec0b488e696c1d7abcb74830be9380f79df38a6
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-07-09 20:45:41 +0000

    ActionItem8: moving / rule up in routes.rb
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@50 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- config/routes.rb
+++ config/routes.rb
@@ -9,15 +9,15 @@ ActionController::Routing::Routes.draw do |map|
   # map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
   # This route can be invoked with purchase_url(:id => product.id)
 
+  # You can have the root of your site routed by hooking up '' 
+  # -- just remember to delete public/index.html.
+  map.connect '', :controller => "home"
+
   # administrative tasks for a virtual community
   map.connect 'admin/:controller/:action/:id'
 
   # profile customization for profiles
   map.connect 'customize/:profile/:controller/:action/:id'
-
-  # You can have the root of your site routed by hooking up '' 
-  # -- just remember to delete public/index.html.
-  map.connect '', :controller => "home"
 
   # Allow downloading Web Service WSDL as a file with an extension
   # instead of a file named 'wsdl'
