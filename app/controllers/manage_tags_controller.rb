c2a63906deef02eee63f1befccf9dc68a5afce86
MoisesMachado <MoisesMachado@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-07-09 19:06:09 +0000

    ActionItem4: Added edit and destroy tags
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@45 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/controllers/manage_tags_controller.rb
+++ app/controllers/manage_tags_controller.rb
@@ -9,7 +9,7 @@ class ManageTagsController < ApplicationController
   end
 
   def new
-    @tags = Tag.find_all
+    @parent_tags = Tag.find_all
     @tag = Tag.new
   end
 
@@ -23,5 +23,28 @@ class ManageTagsController < ApplicationController
     else
       render :action => 'new'
     end
+  end
+
+  def edit
+    @parent_tags = Tag.find_all
+    @tag = Tag.find(params[:id])
+  end
+
+  def update
+    @tag = Tag.find(params[:id])
+    @tag.name = params[:tag][:name]
+    @tag.parent = params[:parent_id] != "0" ? Tag.find(params[:parent_id].to_i) : nil
+    if @tag.save
+      flash[:notice] = _('Tag was successfully updated.')
+      redirect_to :action => 'list'
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    redirect_to :action => 'list'
   end
 end
