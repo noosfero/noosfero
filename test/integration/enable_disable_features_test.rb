2d0f28ade4bf6274aea8d1fa2b13af08a5a8354a
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-07-23 17:55:39 +0000

    ActionItem3: adding an extra POST in the integration test
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@117 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- test/integration/enable_disable_features_test.rb
+++ test/integration/enable_disable_features_test.rb
@@ -6,15 +6,25 @@ class EnableDisableFeaturesTest < ActionController::IntegrationTest
   def test_enable_features
     uses_host 'anhetegua.net'
     login 'johndoe', 'test'
+
     get '/admin/features'
     assert_response :success
     assert_tag :tag => 'input', :attributes => { :name => 'features[feature1]' }
     assert_tag :tag => 'input', :attributes => { :name => 'features[feature2]' }
     assert_tag :tag => 'input', :attributes => { :name => 'features[feature3]' }
-    post 'admin/features/update'
+
+    post '/admin/features/update'
     assert_response :redirect
+
     follow_redirect!
     assert_response :success
     assert_equal '/admin/features', path
+
+    post '/admin/features/update', :features => { 'feature1' => '1' }
+    assert_response :redirect
+
+    follow_redirect!
+    assert_equal '/admin/features', path
+
   end
 end
