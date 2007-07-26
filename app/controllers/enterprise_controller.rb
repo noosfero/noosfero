462c0b8972c1db2b5e45269ac3086661fd9e682b
DanielaFeitosa <DanielaFeitosa@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-07-26 20:03:53 +0000

    ActionItem6: enterprise controller update
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@179 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/controllers/enterprise_controller.rb
+++ app/controllers/enterprise_controller.rb
@@ -1,6 +1,12 @@
 # Manage enterprises by providing an interface to register, activate and manage them
 class EnterpriseController < ApplicationController
 
+  def register
+    unless logged_in?
+      redirect_to :controller => 'account'
+    end
+  end
+
   def register_form
     @vitual_communities = VirtualCommunity.find(:all)
   end
@@ -13,10 +19,10 @@ class EnterpriseController < ApplicationController
     @enterprise = Enterprise.new(params[:enterprise])
     if @enterprise.save
       flash[:notice] = _('Enterprise was succesfully created')
-      redirect_to :action => ''
+      redirect_to :action => 'register'
     else
       flash[:notice] = _('Enterprise was not created')
-      render :text => 'Não Salvou'
+      render :action => 'choose_validation_entity_or_net'
     end
   end
 end
