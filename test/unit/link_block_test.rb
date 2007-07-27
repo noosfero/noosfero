bc8bfd7457ab1e61470a20a534bb7dfdeaef5b14
MoisesMachado <MoisesMachado@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-07-27 21:16:42 +0000

    ActionItem0: Making unit test of content
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@201 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- test/unit/link_block_test.rb
+++ test/unit/link_block_test.rb
@@ -4,7 +4,8 @@ class LinkBlockTest < Test::Unit::TestCase
   fixtures :blocks
 
   # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_content
+    l = LinkBlock.new
+    assert_kind_of Array, l.content
   end
 end
