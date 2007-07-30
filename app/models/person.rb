67d72e2173b41a48fc01fccf20044e577b261804
MoisesMachado <MoisesMachado@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-07-30 18:22:39 +0000

    ActionItem6: affiliation relation for person model fixed
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@211 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,8 +1,13 @@
 class Person < Profile
   belongs_to :user
-  has_many :affiliations
-  has_many :related_profiles, :class_name => 'Profile', :through => :affiliations
-  has_many :friends, :class_name => 'Person'
+  has_many :personal_affiliations, :class_name => 'Affiliation'
+  has_many :related_profiles, :class_name => 'Profile', :through => :personal_affiliations, :source => 'profile'
+
+  has_many :friendships
+  has_many :friends, :class_name => 'Person', :through => :friendships
+
+  has_many :other_friendships
+  has_many :other_friend, :class_name => 'Person', :through => :other_friendships, :foreign_key => 'friend_id'
 
   def my_enterprises
     related_profiles.select{ |p| p.kind_of?(Enterprise) }
