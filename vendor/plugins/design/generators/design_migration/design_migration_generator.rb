3dd95c76d3f3470f91135848e2eb789e7f8aaaa4
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-07-31 13:20:34 +0000

    r270@sede:  terceiro | 2007-07-29 23:52:23 -0300  ActionItem0: missed actual generator
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@274 3f533792-8f58-4932-b0fe-aaf55b0a4547

---
+++ vendor/plugins/design/generators/design_migration/design_migration_generator.rb
@@ -0,0 +1,12 @@
+class DesignMigrationGenerator < Rails::Generator::Base
+  def manifest
+    record do |m|
+      m.migration_template 'migration.rb', 'db/migrate'
+    end
+  end
+
+  def file_name
+    "design_migration"
+  end
+end
+
