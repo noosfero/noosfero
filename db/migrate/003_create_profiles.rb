9a139d204c2174e73ab0d9fb1162b95181585a2d
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-07-31 13:18:12 +0000

    r266@sede:  terceiro | 2007-07-29 23:39:51 -0300  reordering migrations
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@270 3f533792-8f58-4932-b0fe-aaf55b0a4547

---
+++ db/migrate/003_create_profiles.rb
@@ -0,0 +1,32 @@
+class CreateProfiles < ActiveRecord::Migration
+  def self.up
+    create_table :profiles do |t|
+      t.column :name,                                   :string
+      t.column :type,                                   :string
+      t.column :identifier,                             :string
+      t.column :virtual_community_id,                   :integer
+      t.column :flexible_template_template,             :string, :default => "default"
+      t.column :flexible_template_theme,                :string, :default => "default"
+      t.column :flexible_template_icon_theme,           :string, :default => "default"
+      t.column :active,                                 :boolean, :default => "false"
+      
+      #person fields
+      t.column :user_id,                                :integer
+      
+      #enterprise fields
+      t.column :address,                                :string
+      t.column :contact_phone,                          :string
+      t.column :contact_person,                         :string
+      t.column :acronym,                                :string
+      t.column :foundation_year,                        :integer, :limit => 4
+      t.column :legal_form,                             :string
+      t.column :economic_activity,                      :string
+      t.column :management_information,                 :string
+
+    end
+  end
+
+  def self.down
+    drop_table :profiles
+  end
+end
