edb0955b6c939b14f5648608cee9c463258a9bb0
LeandroNunes <LeandroNunes@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-08-01 15:13:32 +0000

    ActionItem0: fixing bugs in test of blocks
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@292 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- test/unit/block_test.rb
+++ test/unit/block_test.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class BlockTest < Test::Unit::TestCase
-  fixtures :blocks, :profiles
+  fixtures :design_blocks, :profiles
 
   def setup
     @profile = Profile.find(1)
@@ -10,53 +10,53 @@ class BlockTest < Test::Unit::TestCase
   def test_setup_assumptions
     assert @profile.valid?
   end
-
-  # Replace this with your real tests.
-  def test_create
-    count = Block.count 
-    b = Block.new
-    assert !b.valid?
-    assert b.errors.invalid?(:box_id)
-    assert b.errors.invalid?(:position)
-    
-    box = Box.new
-    box.owner = @profile
-    box.number = 1000
-    assert box.save
-    b.box = box
-    assert !b.valid?
-    assert b.errors.invalid?(:position)
-
-    b.position=1
-    assert b.save
-
-    assert_equal count + 1, Block.count
-  end
-
-  def test_box_presence
-    b = Block.new
-    b.position = 1000
-    assert !b.valid?
-    assert b.errors.invalid?(:box_id)
-
-    box = Box.new
-    box.owner = @profile
-    box.number = 1000
-    assert box.save
-    b.box = box
-    assert b.valid?
-
-  end
-
-  def test_destroy
-    b = Block.find(1)
-    assert b.destroy 
-  end
-
-  def test_valid_fixtures
-    Block.find(:all).each do |b|
-      assert b.valid?
-    end
-  end
+#TODO remove it this tests are now on design plugin
+#  # Replace this with your real tests.
+#  def test_create
+#    count = Block.count 
+#    b = Block.new
+#    assert !b.valid?
+#    assert b.errors.invalid?(:box_id)
+#    assert b.errors.invalid?(:position)
+#    
+#    box = Box.new
+#    box.owner = @profile
+#    box.number = 1000
+#    assert box.save
+#    b.box = box
+#    assert !b.valid?
+#    assert b.errors.invalid?(:position)
+#
+#    b.position=1
+#    assert b.save
+#
+#    assert_equal count + 1, Block.count
+#  end
+#
+#  def test_box_presence
+#    b = Block.new
+#    b.position = 1000
+#    assert !b.valid?
+#    assert b.errors.invalid?(:box_id)
+#
+#    box = Box.new
+#    box.owner = @profile
+#    box.number = 1000
+#    assert box.save
+#    b.box = box
+#    assert b.valid?
+#
+#  end
+#
+#  def test_destroy
+#    b = Block.find(1)
+#    assert b.destroy 
+#  end
+#
+#  def test_valid_fixtures
+#    Block.find(:all).each do |b|
+#      assert b.valid?
+#    end
+#  end
 
 end
--- test/unit/list_block_test.rb
+++ test/unit/list_block_test.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ListBlockTest < Test::Unit::TestCase
-  fixtures :blocks
+  fixtures :design_blocks
 
   # Replace this with your real tests.
   def test_truth
