744b14a867fbc2ce55338b4a9cf6d0c6d9b28f43
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-08-16 18:20:24 +0000

    ActionItem0: removing unused stuff
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@344 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/controllers/edit_template_controller.rb
+++ app/controllers/edit_template_controller.rb
@@ -1,22 +1,12 @@
 class EditTemplateController < ApplicationController
 
-  design_editor :holder => 'virtual_community', :autosave => true, :block_types => :block_types, :block_helper_types => :block_helper_types
+  design_editor :holder => 'virtual_community', :autosave => true, :block_types => :block_types
   
   def block_types
     { 
       'ListBlock' => _("List Block"), 
       'LinkBlock' => _("Link Block"),
       'Design::MainBlock' => _('Main content block'),
-    }
-  end
-
-  # FIXME: is this really needed? Why should we let the user say how a
-  # particular box must be displayed? IMO the box itself must have authority to
-  # say how it wants to be drawn -- terceiro
-  def block_helper_types
-    { 
-      'list_content' => _("Simple List Content"), 
-      'plain_content' => _("Link Block"),
     }
   end
 
