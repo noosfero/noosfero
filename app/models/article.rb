cebdf7c4db6caf902d1374e0e55ffde9a30083fb
MoisesMachado <MoisesMachado@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-09-07 22:35:33 +0000

    ActionItem4: redirected the keywords of articles of comatose to the acts_as_taggable plugin
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@428 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,15 @@
 class Article < Comatose::Page
-end
-class Article < Comatose::Page
+  acts_as_taggable  
+  
+  def keywords
+    tag_list.to_s
+  end
+  
+  def keywords=(list_tag)
+    self.tag_list = list_tag
+  end
+
+  def has_keyword?(keyword)
+    tags.map{|t| t.name.downcase}.include?(keyword.downcase)
+  end
 end
