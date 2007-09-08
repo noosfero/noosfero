430392514009afc5bde58ce40d5b110e5788ba8a
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-09-08 14:15:53 +0000

    ActionItem8: reverting unecessary change
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@430 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- db/migrate/008_add_comatose_support.rb
+++ db/migrate/008_add_comatose_support.rb
@@ -29,7 +29,7 @@ class AddComatoseSupport < ActiveRecord::Migration
     end
     Comatose::Page.create_versioned_table
     puts "Creating the default 'Home Page'..."
-    Comatose::Page.create!( :title=>'Home Page', :body=>"h1. Welcome\n\nYour content goes here...", :author=>'System' , :parent_id => 0).update_attribute(:parent_id, nil)
+    Comatose::Page.create!( :title=>'Home Page', :body=>"h1. Welcome\n\nYour content goes here...", :author=>'System' )
   end
 
   def self.down
