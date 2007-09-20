da79e4ad0d041cf1775d47ccd96d0baa9d19062a
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-09-20 19:32:54 +0000

    ActionItem65: testing Noosfero module
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@504 3f533792-8f58-4932-b0fe-aaf55b0a4547

---
+++ test/unit/noosfero_test.rb
@@ -0,0 +1,20 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'noosfero'
+
+class NoosferoTest < Test::Unit::TestCase
+
+  def test_should_list_controllers_in_directory
+    Dir.expects(:glob).with("#{RAILS_ROOT}/app/controllers/lala/*_controller.rb").returns(["app/controllers/lala/system_admin_controller.rb", "app/controllers/lala/environment_admin_controller.rb", "app/controllers/lala/public_controller.rb", "app/controllers/lala/profile_admin_controller.rb"]).once
+    assert_equal ["system_admin", "environment_admin", "public", "profile_admin"], Noosfero.controllers_in_directory('lala')
+  end
+
+  def test_should_generate_pattern_for_controllers_in_directory
+    Dir.expects(:glob).with("#{RAILS_ROOT}/app/controllers/lala/*_controller.rb").returns(["app/controllers/lala/system_admin_controller.rb", "app/controllers/lala/environment_admin_controller.rb", "app/controllers/lala/public_controller.rb", "app/controllers/lala/profile_admin_controller.rb"]).once
+    assert_equal(/(system_admin|environment_admin|public|profile_admin)/, Noosfero.pattern_for_controllers_in_directory('lala'))
+  end
+
+  def test_should_generate_empty_pattern_for_empty_dir
+    Dir.stubs(:glob).returns([])
+    assert_equal(//, Noosfero.pattern_for_controllers_in_directory('lala'))
+  end
+end
