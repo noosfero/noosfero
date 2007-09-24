da92889479ab6b46c513662c1eb5a01d270f3ea4
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-09-24 19:46:26 +0000

    ActionItem70: implemented categories
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@527 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/controllers/environment_admin/categories_controller.rb
+++ app/controllers/environment_admin/categories_controller.rb
@@ -2,4 +2,41 @@ class CategoriesController < EnvironmentAdminController
   def index
     @categories = environment.top_level_categories
   end
+
+  # posts back
+  def new
+    @category = Category.new(params[:category])
+    @category.environment = environment
+    if params[:parent_id]
+      @category.parent = environment.categories.find(params[:parent_id])
+    end
+    if request.post?
+      begin
+        @category.save!
+        redirect_to :action => 'index'
+      rescue Exception => e
+        render :action => 'new'
+      end
+    end
+  end
+
+  # posts back
+  def edit
+    begin
+      @category = environment.categories.find(params[:id])
+      if request.post?
+        @category.update_attributes!(params[:category])
+        redirect_to :action => 'index'
+      end
+    rescue Exception => e
+      render :action => 'edit'
+    end
+  end
+
+  post_only :remove
+  def remove
+    environment.categories.find(params[:id]).destroy
+    redirect_to :action => 'index'
+  end
+
 end
