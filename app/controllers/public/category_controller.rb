7cd48a402bc72a7e4ac8539bae8b6621b623298f
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-09-25 16:49:39 +0000

    ActionItem70: adding placeholder for view action in category_controller
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@535 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/controllers/public/category_controller.rb
+++ app/controllers/public/category_controller.rb
@@ -4,14 +4,14 @@ class CategoryController < ApplicationController
   def load_category
     path = params[:path].join('/')
     @category = environment.categories.find_by_path(path)
-  end
-
-  # list categories
-  def index
+    if @category.nil?
+      render :text => ('No such category (%s).' % path), :status => 404
+    end
   end
 
   # view the summary of one category
   def view
+    # TODO: load articles, documents, etc so the view can list them.
   end
 
 end
