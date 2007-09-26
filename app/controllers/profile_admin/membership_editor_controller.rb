b4853226f5a8fcd87c6848118926fc3b70651145
MoisesMachado <MoisesMachado@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-09-26 13:09:04 +0000

    ActionItem5: removing the old enterprise controller
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@559 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/controllers/profile_admin/membership_editor_controller.rb
+++ app/controllers/profile_admin/membership_editor_controller.rb
@@ -3,4 +3,31 @@ class MembershipEditorController < ProfileAdminController
   def index
     @memberships = current_user.person.memberships
   end
+
+  def new_enteprise
+    @enterprise = Enterprise.new()
+    @vitual_communities = Environment.find(:all)
+    @validation_entities = Organization.find(:all)
+  end
+
+  def create_enteprise
+    @enterprise = Enterprise.new(params[:enterprise])
+    @enterprise.organization_info = OrganizationInfo.new(params[:organization])
+    if @enterprise.save
+      @enterprise.people << @person
+      flash[:notice] = _('The enterprise was succesfully created, the validation entity will cotact you as soon as your enterprise is approved')
+      redirect_to :action => 'index'
+    else
+      flash[:notice] = _('Enterprise was not created')
+      @vitual_communities = Environment.find(:all)
+      @validation_entities = Organization.find(:all)
+      render :action => 'register_form'
+    end
+  end
+  
+  # Search enterprises by name or tags
+  def search
+    @tagged_enterprises = Enterprise.search(params[:query])
+  end
+
 end
--- app/helpers/enterprise_helper.rb
+++
@@ -1,2 +0,0 @@
-module EnterpriseHelper
-end
