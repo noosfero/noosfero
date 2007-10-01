e112fd33f93953952cd3baeb9e1487b3833e1edb
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-10-01 17:56:45 +0000

    ActionItem85: bumping up version number
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@594 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- lib/noosfero.rb
+++ lib/noosfero.rb
@@ -1,6 +1,6 @@
 module Noosfero
   PROJECT = 'noosfero'
-  VERSION = '0.2.0'
+  VERSION = '0.3.0'
   SVN_ROOT = 'https://svn.colivre.coop.br/svn/noosfero'
 
   def self.controllers_in_directory(dir)
