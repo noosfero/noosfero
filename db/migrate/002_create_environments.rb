7ff5ef27091c0b5ffce7a6dce6e5f026b6187895
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-10-04 19:14:09 +0000

    ActionItem96: adding contact_email to environment
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@619 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- db/migrate/002_create_environments.rb
+++ db/migrate/002_create_environments.rb
@@ -2,7 +2,10 @@ class CreateEnvironments < ActiveRecord::Migration
   def self.up
     create_table :environments do |t|
       t.column :name,       :string
+      t.column :contact_email, :string
+
       t.column :is_default, :boolean
+
       t.column :settings,   :text
       t.column :design_data, :text
     end
