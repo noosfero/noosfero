2bc9351da94664a6ffda00aab1625cb48f5febb4
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-10-11 18:03:25 +0000

    ActionItem102: adding HABTM between Region and organization
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@660 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- test/unit/region_test.rb
+++ test/unit/region_test.rb
@@ -5,4 +5,15 @@ class RegionTest < Test::Unit::TestCase
   should 'be a subclass of category' do
     assert_equal Category, Region.superclass
   end
+
+  should 'have an array of validators' do
+    region = Region.new
+    assert_raise ActiveRecord::AssociationTypeMismatch do
+      region.validators = [ 1 ]
+    end
+    assert_nothing_raised do
+      region.validators = [ Organization.new ]
+    end
+  end
+
 end
