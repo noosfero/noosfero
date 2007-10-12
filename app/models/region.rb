3a9f5dcca7dc088836ba66a659b827683e7381c6
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-10-12 13:43:42 +0000

    ActionItem102: doc work
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@666 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/models/region.rb
+++ app/models/region.rb
@@ -2,6 +2,9 @@
 class Region < Category
   has_and_belongs_to_many :validators, :class_name => Organization.name, :join_table => :region_validators
 
+  # searches for organizations that could become validators for this region.
+  # <tt>search</tt> is passed as is to ferret's find_by_contents on Organizatio
+  # find_by_contents on Organization class.
   def search_possible_validators(search)
     Organization.find_by_contents(search).reject {|item| self.validator_ids.include?(item.id) }
   end
