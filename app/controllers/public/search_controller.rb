54fc60915b42181fff7b933a663937dd57607ab7
MoisesMachado <MoisesMachado@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-10-20 05:30:33 +0000

    ActionItem85: removed the search in the contents of the articles
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@788 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/controllers/public/search_controller.rb
+++ app/controllers/public/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController  
   def index
     @query = params[:query] || ''
-    @results = Article.find_tagged_with(@query) + Article.find_by_contents(@query)
+    # TODO: uncomment find_by_contents when ferret start working 
+    @results = Article.find_tagged_with(@query) #+ Article.find_by_contents(@query)
   end
 end
