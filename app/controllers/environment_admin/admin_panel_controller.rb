126891fb82cf068e937c2b2a0f98b36e1b209f46
LeandroNunes <LeandroNunes@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-10-20 05:33:29 +0000

    ActionItem85: bli
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@789 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/controllers/environment_admin/admin_panel_controller.rb
+++ app/controllers/environment_admin/admin_panel_controller.rb
@@ -1,4 +1,13 @@
 class AdminPanelController < EnvironmentAdminController
+
   protect [:index], 'view_environment_admin_panel', :environment
+
+  design :holder => 'environment'
+
+  before_filter :load_default_enviroment
+
+  def load_default_enviroment
+    Environment.default
+  end
 
 end
