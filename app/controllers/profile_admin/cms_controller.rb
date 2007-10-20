e035ee75310410abca3db9fd51fecf2de03954da
MoisesMachado <MoisesMachado@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-10-20 02:31:15 +0000

    ActionItem85: applied the permission check to the cms controller
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@765 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/controllers/profile_admin/cms_controller.rb
+++ app/controllers/profile_admin/cms_controller.rb
@@ -1,9 +1,15 @@
 class CmsController < ComatoseAdminController
   extend PermissionCheck
-
+  
   ApplicationController.needs_profile
   
   define_option :page_class, Article
   
-#   protect [:edit, :new, :reorder, :delete], :post_content, :profile
+  protect [:edit, :new, :reorder, :delete], 'post_content', :profile
+
+  protected
+
+  def profile
+    Profile.find_by_identifier(params[:profile]) 
+  end
 end
