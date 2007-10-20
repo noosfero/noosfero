44062abc91e662d938d2e8f1282334fbc2048187
MoisesMachado <MoisesMachado@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-10-20 00:33:03 +0000

    ActionItem111: fixed some interface issues
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@757 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,10 +1,14 @@
 # Represents any organization of the system and has an organization_info object to hold its info
-class Organization < Profile
+class Organization < Profile  
   has_one :organization_info
 
   belongs_to :region
 
   has_one :validation_info
+
+  after_create do |org|
+      OrganizationInfo.create!(:organization_id => org.id)
+  end
 
   def validation_methodology
     methodology = self.validation_info ? self.validation_info.validation_methodology : nil
