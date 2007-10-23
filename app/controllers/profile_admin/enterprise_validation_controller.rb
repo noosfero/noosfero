cc9eeb043eab650ee07d1c9977b431fcf4cd1dad
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-10-23 17:40:15 +0000

    ActionItem16: adding enterprise_validation controller
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@816 3f533792-8f58-4932-b0fe-aaf55b0a4547

---
+++ app/controllers/profile_admin/enterprise_validation_controller.rb
@@ -0,0 +1,3 @@
+class EnterpriseValidationController < EnterpriseAdminController
+
+end
---
+++ test/functional/enterprise_validation_test.rb
@@ -0,0 +1,30 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'enterprise_validation_controller'
+
+# Re-raise errors caught by the controller.
+class EnterpriseValidationController; def rescue_action(e) raise e end; end
+
+class EnterpriseValidationControllerTest < Test::Unit::TestCase
+
+  fixtures :users
+
+  def setup
+    @controller = EnterpriseValidationController.new
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+    login_as 'ze'
+  end
+
+  should 'list pending validations on index' do
+    flunk 'not yet'
+  end
+
+  should 'prompt for needed data when approving or rejecting enterprise' do
+    flunk 'not yet'
+  end
+
+  should 'be able to actually validate enterprise on request' do
+    flunk 'not yet'
+  end
+
+end
