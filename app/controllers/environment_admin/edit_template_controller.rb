ee41e5ff771f6b878a6db5923ff64905845037d6
LeandroNunes <LeandroNunes@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-10-26 13:54:49 +0000

    ActionItem19: fixing some little bugs to design plugin works
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@831 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/controllers/environment_admin/edit_template_controller.rb
+++ app/controllers/environment_admin/edit_template_controller.rb
@@ -1,12 +1,13 @@
 class EditTemplateController < EnvironmentAdminController
   
   design_editor :holder => 'environment', :autosave => true, :block_types => :block_types
-  
+
+  #FIXME This is wrong
+  #See the FavoriteLinksController considerations and choose the better way
   def block_types
-    { 
-      'ListBlock' => _("List Block"), 
-      'LinkBlock' => _("Link Block"),
-    }
+    %w[
+       FavoriteLinks
+     ]
   end
 
   def index
