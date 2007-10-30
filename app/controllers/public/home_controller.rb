2b065b7def09e55179e90db8de24c6d182db5546
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-10-30 20:12:19 +0000

    ActionItem114: still removing workarounds
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@838 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/controllers/public/home_controller.rb
+++ app/controllers/public/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < PublicController
 
   design :holder => 'environment'
+
   def index
-    redirect_to homepage_path(:profile => 'noosfero') if Profile.find_by_identifier('noosfero')
   end
+
 end
