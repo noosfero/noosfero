876d11aa585069679f2cd46b3a199c2fd58204b4
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-11-24 16:37:13 +0000

    ActionItem21: reworking Article
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@939 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,25 +1,14 @@
-class Article
+class Article < ActiveRecord::Base
 
   acts_as_taggable  
-  
-#  acts_as_ferret :fields => [:title, :body]
-  def keywords
-    tag_list.to_s
-  end
-  
-  def keywords=(list_tag)
-    self.tag_list = list_tag
-  end
 
-  def has_keyword?(keyword)
-    tags.map{|t| t.name.downcase}.include?(keyword.downcase)
-  end
+  acts_as_filesystem
+
+  acts_as_versioned
 
   def profile(reload = false)
     @profile = nil if reload
     @profile ||= Profile.find_by_identifier(self.full_path.split(/\//).first)
   end
-
-  # FIXME add code from Category to make article acts as a "file system"
 
 end
