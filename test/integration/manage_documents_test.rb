9eeb492c3fc64d0858a935c135bc134bf65f7e49
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-12-01 18:59:04 +0000

    ActionItem21: writing integration test
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1005 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- test/integration/manage_documents_test.rb
+++ test/integration/manage_documents_test.rb
@@ -5,8 +5,26 @@ class ManageDocumentsTest < ActionController::IntegrationTest
   all_fixtures
 
   def test_creation_of_a_new_article
-    # FIXME
-    fail 'need to be rewritten'
+    create_user('myuser')
+
+    login('myuser', 'myuser')
+
+    assert_tag :tag => 'a', :attributes => { :href => '/myprofile/myuser'  }
+    get '/myprofile/myuser'
+    
+    assert_tag :tag => 'a', :attributes => { :href => '/myprofile/myuser/cms' }
+    get '/myprofile/myuser/cms/new'
+
+    assert_tag :tag => 'form', :attributes => { :action => '/myprofile/myuser/cms/new', :method => /post/i }
+
+    assert_difference Article, :count do
+      post '/myprofile/myuser/cms/new', :article => { :name => 'my article', :body => 'this is the body of ther article'}
+    end
+
+    assert_response :redirect
+    follow_redirect!
+    a = Article.find_by_path('my-article')
+    assert_equal "/myprofile/myuser/cms/view/#{a.id}", path
   end
 
   def test_update_of_an_existing_article
