1d28968ea07fcd0616130e477e3844d124552bb0
MoisesMachado <MoisesMachado@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-12-02 02:02:27 +0000

    ActionItem93: cosmetic changes and a column added to describe needed products
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1044 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- db/migrate/018_create_consumptions.rb
+++ db/migrate/018_create_consumptions.rb
@@ -1,8 +1,10 @@
 class CreateConsumptions < ActiveRecord::Migration
   def self.up
     create_table :consumptions do |t|
-      t.column :product_category_id, :integer
-      t.column :profile_id, :integer
+      t.column :product_category_id,     :integer
+      t.column :profile_id,              :integer
+
+      t.column :aditional_specifications, :text
     end
   end
 
