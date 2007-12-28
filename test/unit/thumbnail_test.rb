26a4d4aa805135d3ce6906e0f178f7e60368004e
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2007-12-28 22:42:08 +0000

    ActionItem135: adding one test
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1158 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- test/unit/thumbnail_test.rb
+++ test/unit/thumbnail_test.rb
@@ -2,6 +2,12 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class ThumbnailTest < Test::Unit::TestCase
 
-  # FIXME add some tests
+  should 'use sensible options' do
+    assert_equal :file_system, Thumbnail.attachment_options[:storage]
+
+    Thumbnail.attachment_options[:content_type].each do |item|
+      assert_match 'image/', item 
+    end
+  end
   
 end
