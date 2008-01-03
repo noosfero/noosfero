25e5aec0f125234c1e5f49576df36550ce03cf72
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-01-03 01:02:16 +0000

    ActionItem141: expressing new type requirement in test
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1168 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- test/unit/tiny_mce_article_test.rb
+++ test/unit/tiny_mce_article_test.rb
@@ -2,8 +2,9 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class TinyMceArticleTest < Test::Unit::TestCase
 
+  # this test can be removed when we get real tests for TinyMceArticle 
   should 'be an article' do
-    assert_subclass Article, TinyMceArticle
+    assert_subclass TextArticle, TinyMceArticle
   end
 
 end
