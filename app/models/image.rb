5c74595a3d60710a7ad53db7b7cf94019073a7e5
MoisesMachado <MoisesMachado@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-01-14 21:22:04 +0000

    ActionItem121: changed the name of the default environment to anhentegua in the anhentegua script and added an icon thumbnail to the image model
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1188 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/models/image.rb
+++ app/models/image.rb
@@ -5,7 +5,7 @@ class Image < ActiveRecord::Base
                  :storage => :file_system, 
                  :max_size => 500.kilobytes,
                  :resize_to => '320x200>',
-                 :thumbnails => { :thumb => '100x100>', :minor => '64x64>', :portrait => 'x95' }
+                 :thumbnails => { :thumb => '100x100>', :minor => '64x64>', :portrait => 'x95' , :icon => '20x20'}
 
   validates_as_attachment
 end
