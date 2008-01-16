3473b0878c24c591a4aa2b4f5a077dab5a1783dd
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-01-16 17:28:17 +0000

    ActionItem152: moving javascript code to application.js
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1195 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,34 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function noosfero_init() {
+  focus_first_field();
+  detect_internet_explorer();
+}
+
+/* Adds a class to "msie" to the body element if a Microsoft browser is
+ * detected. This is needed to workaround several of their limitations.
+ */
+function detect_internet_explorer() {
+  if ( navigator.appName.indexOf("Microsoft") > -1 ) {
+    document.body.className += " msie";
+  }
+}
+
+/* If applicable, find the first field in which the user can type and move the
+ * keyboard focus to it.
+ */
+function focus_first_field() {
+  form = document.forms[0];
+  if (form == undefined) {
+    return;
+  }
+
+  for (var i = 0; i < form.elements.length; i++) {
+    field = form.elements[i];
+    if (field.type == 'text' || field.type == 'textarea') {
+      field.focus();
+      return;
+    }
+  }
+}
