68f54838d2cb86e84776103a2dc40a6299284e99
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-01-18 18:26:11 +0000

    ActionItem152: removing debugging code
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1209 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/models/main_block.rb
+++ app/models/main_block.rb
@@ -1,7 +1,11 @@
 class MainBlock < Block
 
+  def self.description
+    _('Block for main content (i.e. your articles, photos, etc)')
+  end
+
   def content(main_content = nil)
-    main_content + "(id: ##{self.id})"
+    main_content
   end
 
 end
