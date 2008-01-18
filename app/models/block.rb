e340547251abbe29ad8f9fce64e96b1d738109f3
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-01-18 21:11:57 +0000

    ActionItem152: storing settings in a block
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1235 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/models/block.rb
+++ app/models/block.rb
@@ -2,16 +2,17 @@ class Block < ActiveRecord::Base
   acts_as_list :scope => :box
   belongs_to :box
 
+  serialize :settings, Hash
+  def settings
+    self[:settings] ||= Hash.new
+  end
+
   def self.description
     _('A dummy block.')
   end
 
   def content(main_content = nil)
     "This is block number %d" % self.id
-  end
-
-  def editor
-    { :controller => 'block_editor', :id => self.id }
   end
 
 end
--- test/unit/block_test.rb
+++ test/unit/block_test.rb
@@ -6,4 +6,13 @@ class BlockTest < Test::Unit::TestCase
     assert_kind_of String, Block.description
   end
 
+  should 'store settings in a hash' do
+    block = Block.new
+
+    assert_kind_of Hash, block.settings
+    block.save!
+
+    assert_kind_of Hash, Block.find(block.id).settings
+  end
+
 end
