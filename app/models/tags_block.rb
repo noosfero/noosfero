999ca1fa73017503ad42f8a4dfa19fdcaa461f02
AurelioAHeckert <AurelioAHeckert@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-01-30 23:09:18 +0000

    ActionItem152: detalhes
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1287 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/models/tags_block.rb
+++ app/models/tags_block.rb
@@ -12,7 +12,7 @@ class TagsBlock < Block
     help_textile(
               _('The tag is created when you add some one to your article.
                  Try to add some tags to some articles and see your tag cloud to grow.'),
-              _('How tags works here?') ) +
+              _('How tags works here?'), :class => 'help_tags' ) +
     tag_cloud( owner.tags, :id,
                owner.generate_url(:controller => 'profile', :action => 'tag') + '/',
                :max_size => 20, :min_size => 10 )
