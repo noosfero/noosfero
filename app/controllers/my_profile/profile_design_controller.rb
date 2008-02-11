9c3cf81682fa06be0e1bdd188b3298e92208b2d8
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-02-11 19:27:11 +0000

    ActionItem154: removing duplicated block
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1351 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/controllers/my_profile/profile_design_controller.rb
+++ app/controllers/my_profile/profile_design_controller.rb
@@ -3,7 +3,7 @@ class ProfileDesignController < BoxOrganizerController
   needs_profile
 
   def available_blocks
-    blocks = [ ArticleBlock, TagsBlock, RecentDocumentsBlock, ProfileInfoBlock, TagsBlock ]
+    blocks = [ ArticleBlock, TagsBlock, RecentDocumentsBlock, ProfileInfoBlock ]
 
     if profile.has_members?
       blocks << MembersBlock
