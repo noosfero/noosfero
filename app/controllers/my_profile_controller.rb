9564a160a7950d0f908a0422e935abcec8c4fbe3
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-02-19 19:51:46 +0000

    ActionItem170: ActionController::Base#render is now protected
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1449 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/controllers/my_profile_controller.rb
+++ app/controllers/my_profile_controller.rb
@@ -15,7 +15,7 @@ class MyProfileController < ApplicationController
     before_filter do |controller|
       unless controller.send(:profile).kind_of?(some_class)
         controller.instance_variable_set('@message',  _("This action is not available for \"%s\".") % controller.send(:profile).name)
-        controller.render :file => File.join(RAILS_ROOT, 'app', 'views', 'shared', 'access_denied.rhtml'), :layout => true, :status => 403
+        controller.send(:render, :file => File.join(RAILS_ROOT, 'app', 'views', 'shared', 'access_denied.rhtml'), :layout => true, :status => 403)
       end
     end
   end
