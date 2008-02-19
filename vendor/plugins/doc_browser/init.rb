4c3b35d5d8966b5bb00f8cf771bf683391908e59
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-02-19 19:35:27 +0000

    ActionItem170: removing doc_controller, it's not that usefull anyway
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1447 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- vendor/plugins/doc_browser/init.rb
+++
@@ -1,6 +0,0 @@
-if ENV['RAILS_ENV'] == 'development'
-  controllers_path = File.join(File.dirname(__FILE__), 'controllers')
-  $LOAD_PATH << controllers_path
-  Dependencies.load_paths << controllers_path
-  config.controller_paths << controllers_path
-end
