4e02ff773e133cb44e87b5c295e98e62ab379935
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-02-21 18:35:39 +0000

    ActionItem172: aliasing assert_block as ok to write tests like in Perl. Idea from links in comments to http://www.oreillynet.com/ruby/blog/2008/02/assert2.html and later from http://www.oreillynet.com/ruby/blog/2008/02/do_not_question_assert_20.html
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1463 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -95,6 +95,8 @@ class Test::Unit::TestCase
     user
   end
 
+  alias :ok :assert_block
+
   def assert_equivalent(enum1, enum2)
     assert( ((enum1 - enum2) == []) && ((enum2 - enum1) == []), "<#{enum1.inspect}> expected to be equivalent to <#{enum2.inspect}>")
   end
