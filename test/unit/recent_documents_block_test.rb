fc87be858c7037046e69972d1987a55cabb2c165
MoisesMachado <MoisesMachado@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-02-29 19:42:04 +0000

    ActionItem193: tested the exclusion of non text arguments of the recent documents list
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1536 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- test/unit/recent_documents_block_test.rb
+++ test/unit/recent_documents_block_test.rb
@@ -3,18 +3,16 @@ require File.dirname(__FILE__) + '/../test_helper'
 class RecentDocumentsBlockTest < Test::Unit::TestCase
 
   def setup
-    profile = create_user('testinguser').person
-    profile.articles.build(:name => 'first').save!
-    profile.articles.build(:name => 'second').save!
-    profile.articles.build(:name => 'third').save!
-    profile.articles.build(:name => 'fourth').save!
-    profile.articles.build(:name => 'fifth').save!
+    @profile = create_user('testinguser').person
+    ['first', 'second', 'third', 'fourth', 'fifth'].each do |name|
+      @profile.articles << TextArticle.create(:name => name)
+    end
 
     box = Box.create!(:owner => profile)
     @block = RecentDocumentsBlock.create!(:box_id => box.id)
 
   end
-  attr_reader :block
+  attr_reader :block, :profile
 
   should 'describe itself' do
     assert_not_equal Block.description, RecentDocumentsBlock.description
@@ -40,6 +38,16 @@ class RecentDocumentsBlockTest < Test::Unit::TestCase
     assert_match /href=.*\/testinguser\/third/, output
     assert_no_match /href=.*\/testinguser\/second/, output
     assert_no_match /href=.*\/testinguser\/first/, output
+  end
+
+  should 'not list rss feed articles' do
+    profile.articles << RssFeed.create(:name => 'sixth')
+    profile.save!
+
+    output = block.content
+
+    assert_match /href=.*\/testinguser\/first/, output
+    assert_no_match /href=.*\/testinguser\/sixth/, output
   end
 
 end
