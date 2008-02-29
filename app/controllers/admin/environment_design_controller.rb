5d1530aec2b4fb33a4299538248a9698b2876da3
MoisesMachado <MoisesMachado@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-02-29 14:15:05 +0000

    ActionItem177: fixed a misuse of protect
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1532 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/controllers/admin/environment_design_controller.rb
+++ app/controllers/admin/environment_design_controller.rb
@@ -1,6 +1,6 @@
 class EnvironmentDesignController < BoxOrganizerController
   
-  protect 'edit_environment_design'
+  protect 'edit_environment_design', :environment
 
   def available_blocks
     @available_blocks ||= [ LoginBlock, EnvironmentStatisticsBlock, RecentDocumentsBlock, ProfileListBlock ]
