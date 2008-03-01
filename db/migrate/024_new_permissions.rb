00c813822f768dc15154927d787708b679eee156
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-03-01 20:16:43 +0000

    ActionItem177: avoiding duplicated logic in migration. Using Profile::Roles module-level methods
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1541 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- db/migrate/024_new_permissions.rb
+++ db/migrate/024_new_permissions.rb
@@ -1,20 +1,20 @@
 class NewPermissions < ActiveRecord::Migration
   def self.up
-    admin = Role.find_by_key('profile_admin')
+    admin = Profile::Roles.admin
     admin.permissions += ['manage_friends', 'validate_enterprise', 'peform_task']
     admin.save
 
-    moderator = Role.find_by_key('profile_moderator')
+    moderator = Profile::Roles.moderator
     moderator.permissions += ['manage_friends', 'peform_task']
     moderator.save
   end
 
   def self.down
-    admin = Role.find_by_key('profile_admin')
+    admin = Profile::Roles.admin
     admin.permissions -= ['manage_friends', 'validate_enterprise', 'peform_task']
     admin.save
 
-    moderator = Role.find_by_key('profile_moderator')
+    moderator = Profile::Roles.moderator
     moderator.permissions -= ['manage_friends', 'peform_task']
     moderator.save
   end
