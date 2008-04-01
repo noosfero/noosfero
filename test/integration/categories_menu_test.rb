9e93def48e622adfe4cc81992c4e7d5a715b9841
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-04-01 20:48:36 +0000

    ActionItem155: integrating the categories menu with the assets menu
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1630 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- test/integration/categories_menu_test.rb
+++ test/integration/categories_menu_test.rb
@@ -2,27 +2,27 @@ require "#{File.dirname(__FILE__)}/../test_helper"
 
 class CategoriesMenuTest < ActionController::IntegrationTest
 
-  should 'display link to categories' do
+  def setup
     Category.delete_all
-    cat1 = Category.create!(:name => 'Food', :environment => Environment.default, :display_color => 1)
-    cat2 = Category.create!(:name => 'Vegetables', :environment => Environment.default, :parent => cat1)
+    @cat1 = Category.create!(:name => 'Food', :environment => Environment.default, :display_color => 1)
+    @cat2 = Category.create!(:name => 'Vegetables', :environment => Environment.default, :parent => @cat1)
+  end
 
+  should 'display link to categories' do
     get '/'
-
     assert_tag :tag => 'a', :attributes => { :href => '/cat/food/vegetables' }
-
   end
 
   should 'display link to sub-categories' do
-    Category.delete_all
-    cat1 = Category.create!(:name => 'Food', :environment => Environment.default)
-    cat2 = Category.create!(:name => 'Vegetables', :environment => Environment.default, :parent => cat1)
-
     get '/cat/food'
-
     # there must be a link to the subcategory
     assert_tag :tag => 'a', :attributes => { :href => '/cat/food/vegetables' }
+  end
 
+  should 'link to other assets in the same category when viewing an asset' do
+    get '/assets/articles/food/vegetables'
+    assert_no_tag :tag => 'a', :attributes => { :href => '/cat/food/vegetables' }
+    assert_tag :tag => 'a', :attributes => { :href => '/assets/enterprises/food/vegetables' }
   end
 
 end
