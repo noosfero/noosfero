300ce8a4a98624421261e2d45d0a30c721d0c87c
JoenioCosta <JoenioCosta@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-04-01 17:13:12 +0000

    ActionItem146: fixing warning 'constant already defined'
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1622 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- vendor/plugins/inverse_captcha/lib/inverse_captcha.rb
+++ vendor/plugins/inverse_captcha/lib/inverse_captcha.rb
@@ -2,9 +2,9 @@ module InverseCaptcha
 
   module ClassMethods
     def inverse_captcha(opt = {})
-      InverseCaptcha.const_set("ICAPTCHA_FIELD", opt[:field])
-      InverseCaptcha.const_set("ICAPTCHA_LABEL", opt[:label] || N_("Don't fill this field"))
-      InverseCaptcha.const_set("ICAPTCHA_STYLECLASS", opt[:class] || 'ghost')
+      InverseCaptcha.const_set("ICAPTCHA_FIELD", opt[:field]) unless InverseCaptcha.const_defined? "ICAPTCHA_FIELD"
+      InverseCaptcha.const_set("ICAPTCHA_LABEL", opt[:label] || N_("Don't fill this field")) unless InverseCaptcha.const_defined? "ICAPTCHA_LABEL"
+      InverseCaptcha.const_set("ICAPTCHA_STYLECLASS", opt[:class] || 'ghost') unless InverseCaptcha.const_defined? "ICAPTCHA_STYLECLASS"
       self.send(:include, InverseCaptcha)
     end
   end
