39a4ebdb8ed10766f75bfba94cf0775eb3593982
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-04-07 15:29:03 +0000

    ActionItem261: adding script to extract data from sies database
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1644 3f533792-8f58-4932-b0fe-aaf55b0a4547

---
+++ script/extract_sies_data.rb
@@ -0,0 +1,58 @@
+$LOAD_PATH.unshift('/usr/share/rails/activerecord/lib')
+$LOAD_PATH.unshift('/usr/share/rails/activesupport/lib')
+
+require 'activerecord'
+require 'active_support'
+
+LIMIT = 5
+
+ActiveRecord::Base.establish_connection(
+  :adapter => 'mysql',
+  :host => 'localhost',
+  :database => 'farejador',
+  :username => 'root',
+  :password => 'root'
+)
+
+class Enterprise < ActiveRecord::Base
+  set_table_name 'cons_dadosbasicos'
+end
+
+class Category < ActiveRecord::Base
+  set_table_name 'lista_produtos'
+end
+
+class Dumper
+  def initialize
+    @seq = 0
+    @seqs = {}
+  end
+
+  def pretty(str, alt = nil)
+    if alt.nil?
+      str
+    else
+      str + ' (' + alt + ')'
+    end
+  end
+
+  def dump(cat, parent = nil)
+
+    @seqs[cat] = @seq
+    puts "cat#{@seq} = Category.create!(:name => #{pretty(cat.nome, cat.nome_alt).inspect}, :parent => #{parent ? 'cat' + @seqs[parent].to_s : 'nil' })"
+    @seq = @seq + 1
+
+    Category.find(:all, :conditions => { :id_mae => cat.id }).each do |child|
+      dump(child, cat)
+    end
+
+  end
+
+end
+
+dumper = Dumper.new
+Category.find(:all, :conditions => 'id_mae is null or id_mae = -1').each do |cat|
+  dumper.dump(cat, nil)
+end
+
+# puts Enterprise.find(:all, :limit => LIMIT).to_xml
