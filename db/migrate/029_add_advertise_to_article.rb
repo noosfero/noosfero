cf967b93b3ddf86bcb85566399bc136f0d20f625
JoenioCosta <JoenioCosta@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-04-18 22:09:26 +0000

    ActionItem194: *oops* creating fields for versioned table
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1670 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- db/migrate/029_add_advertise_to_article.rb
+++ db/migrate/029_add_advertise_to_article.rb
@@ -1,10 +1,16 @@
 class AddAdvertiseToArticle < ActiveRecord::Migration
   def self.up 
     # show in recent content?
-    add_column :articles,  :advertise, :boolean, :default => true
+    add_column :articles, :advertise, :boolean, :default => true
+
+    # add this column by hand while acts_as_versioned dont have a method for update versioned table
+    add_column :article_versions, :advertise, :boolean, :default => true
   end
 
   def self.down
     remove_column :articles, :advertise
+
+    # remove this column by hand while acts_as_versioned dont have a method for downdate versioned table
+    remove_column :article_versions, :advertise
   end
 end
