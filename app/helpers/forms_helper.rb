dd6b5db71bb4b056982ebd1ab4e3e171504e291b
AurelioAHeckert <AurelioAHeckert@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-04-28 03:15:52 +0000

    ActionItem260: Search formatation apear ok now. May be better, but this is ok.
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1709 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -10,4 +10,16 @@ module FormsHelper
 
   end
 
+  def labelled_radio_button( human_name, name, value, checked = false, options = {} )
+    options[:id] ||= 'radio-' + rand.to_s
+    radio_button_tag( name, value, checked, options ) +
+    content_tag( 'label', human_name, :for => options[:id] )
+  end
+
+  def labelled_check_box( human_name, name, value = "1", checked = false, options = {} )
+    options[:id] ||= 'checkbox-' + rand.to_s
+    check_box_tag( name, value, checked, options ) +
+    content_tag( 'label', human_name, :for => options[:id] )
+  end
+
 end
