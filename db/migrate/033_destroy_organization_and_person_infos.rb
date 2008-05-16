6741d167e1b91ab1ec8d7700673e98dd6f0ef2d2
MoisesMachado <MoisesMachado@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-05-16 14:56:30 +0000

    ActionItem380: fixed the migrations that do not function without the removed code
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1803 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- db/migrate/033_destroy_organization_and_person_infos.rb
+++ db/migrate/033_destroy_organization_and_person_infos.rb
@@ -1,18 +1,20 @@
 class DestroyOrganizationAndPersonInfos < ActiveRecord::Migration
   def self.up
     Person.find(:all).each do |i|
-      i.name = i.info.name unless i.info.name.nil?
-      i.address = i.info.address unless i.info.address.nil?
-      for field in [ :photo, :contact_information, :birth_date, :sex, :city, :state, :country ] do
-        i.send("#{field}=", i.info.send(field))
+      info = ActiveRecord::Base.connection.select_one("select * from person_infos where person_id = #{i.id}")
+      i.name = info["name"] unless info["name"].nil?
+      i.address = info["address"] unless info["address"].nil?
+      [ "photo", "contact_information", "birth_date", "sex", "city", "state", "country" ].each do |field|
+        i.send("#{field}=", info[field])
       end
       i.save!
     end
     drop_table :person_infos
 
     Organization.find(:all).each do |i|
-      for field in [ :contact_person, :contact_email, :acronym, :foundation_year, :legal_form, :economic_activity, :management_information, :validated ] do
-        i.send("#{field}=", i.info.send(field))
+      info = ActiveRecord::Base.connection.select_one("select * from organization_infos where organization_id = #{i.id}")
+      [ "contact_person", "contact_email", "acronym", "foundation_year", "legal_form", "economic_activity", "management_information", "validated" ].each do |field|
+        i.send("#{field}=", info[field])
       end
       i.save!
     end
