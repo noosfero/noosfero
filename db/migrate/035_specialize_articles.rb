a2ee849662ce12be9a7bebbeffcb3e34412ff400
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-06-04 17:06:34 +0000

    ActionItem392: updating all 'untyped' articles to be explicitly TinyMCE articles
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1862 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- db/migrate/035_specialize_articles.rb
+++ db/migrate/035_specialize_articles.rb
@@ -1,9 +1,9 @@
 class SpecializeArticles < ActiveRecord::Migration
   def self.up
-    execute "update articles set type = 'TinyMceArticle' where type = 'Article'"
+    execute "update articles set type = 'TinyMceArticle' where type = 'Article' or type is null or type = ''"
   end
 
   def self.down
-    raise ActiveRecord::Migration::IrreversibleMigration, 'cannot reverse this'
+    # nothing
   end
 end
