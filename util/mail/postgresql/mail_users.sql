112706ac4205c0c0e7bc3ead48c25580ebd43872
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-06-10 15:29:18 +0000

    ActionItem44: adding files and some documentation
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@1983 3f533792-8f58-4932-b0fe-aaf55b0a4547

---
+++ util/mail/postgresql/mail_users.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE VIEW mail_users
+AS
+SELECT
+  users.login || '@' || domains.name                      as username,
+  '{MD5}' || encode(decode(users.crypted_password,'hex'), 'base64')
+                                                          as passwd,
+  ''                                                      as clearpasswd,
+  5000                                                    as uid,
+  5000                                                    as gid,
+  '/home/vmail/' || domains.name                          as home,
+  users.login                                             as maildir,
+  NULL                                                    as quota,
+  profiles.name                                           as fullname,
+  ''                                                      as options
+from users
+JOIN profiles on
+  (profiles.user_id = users.id and
+   profiles.type = 'Person')
+JOIN environments on
+  (environments.id = profiles.environment_id)
+JOIN domains on
+  (domains.owner_id = environments.id and
+   domains.owner_type = 'Environment');
+
