50267ca4a0de5c8dfdc9ead272e5d67870463851
JoenioCosta <JoenioCosta@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-06-13 21:15:12 +0000

    ActionItem441: tests for add member task done!
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@2024 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- test/functional/tasks_controller_test.rb
+++ test/functional/tasks_controller_test.rb
@@ -62,12 +62,45 @@ class TasksControllerTest < Test::Unit::TestCase
     ok('task should be cancelled') { t.status == Task::Status::CANCELLED }
   end
 
-  should 'affiliate roles to user after finish add member task'
+  should 'affiliate roles to user after finish add member task' do
+    t = AddMember.create!(:person => profile, :community => profile)
+    count = profile.members.size
+    post :close, :decision => 'finish', :id => t.id
+    profile.reload
+    assert_equal count + 1, profile.members.size
+  end
 
-  should 'display form to add members task'
+  should 'display custom form to add members task' do
+    t = AddMember.create!(:person => profile, :community => profile)
+    get :index, :profile => profile.identifier
+    assert_tag :tag => 'form', :attributes => { :action => "/myprofile/#{profile.identifier}/tasks/close/#{t.id}" }
+  end
 
-  should 'display all roles checked if target has no members'
+  should 'display member role checked if target has members' do
+    profile.affiliate(profile, Profile::Roles.admin)
+    assert_equal 1, profile.members.size
+    t = AddMember.create!(:person => profile, :community => profile)
+    get :index, :profile => profile.identifier
+    assert_tag :tag => 'input', :attributes => { :name => 'task[roles][]', :checked => 'checked', :value => Profile::Roles.member.id }
+  end
 
-  should 'display member role checked if target has members'
+  should 'display roles besides role member unchecked if target has members' do
+    profile.affiliate(profile, Profile::Roles.admin)
+    assert_equal 1, profile.members.size
+    t = AddMember.create!(:person => profile, :community => profile)
+    get :index, :profile => profile.identifier
+    Role.find(:all).select{ |r| r.has_kind?('Profile') and r.id != Profile::Roles.member.id }.each do |i|
+      assert_no_tag :tag => 'input', :attributes => { :name => 'task[roles][]', :checked => 'checked', :value => i.id }
+    end
+  end
+
+  should 'display all roles checked if target has no members' do
+    assert_equal 0, profile.members.size
+    t = AddMember.create!(:person => profile, :community => profile)
+    get :index, :profile => profile.identifier
+    Role.find(:all).select{ |r| r.has_kind?('Profile') }.each do |i|
+      assert_tag :tag => 'input', :attributes => { :name => 'task[roles][]', :checked => 'checked', :value => i.id }
+    end
+  end
 
 end
