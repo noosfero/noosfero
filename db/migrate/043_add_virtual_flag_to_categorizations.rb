b026a547e94f06992af9ce229522a315248036ae
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-06-21 04:32:12 +0000

    ActionItem466: creating the hierarchies for the existing categorizations
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@2080 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- db/migrate/043_add_virtual_flag_to_categorizations.rb
+++ db/migrate/043_add_virtual_flag_to_categorizations.rb
@@ -1,7 +1,16 @@
 class AddVirtualFlagToCategorizations < ActiveRecord::Migration
   def self.up
     add_column :articles_categories, :virtual, :boolean, :default => false
+    execute('update articles_categories set virtual = (1!=1)')
+    Article.find(:all).each do |article|
+      article.category_ids = article.categories.map(&:id)
+    end
+
     add_column :categories_profiles, :virtual, :boolean, :default => false
+    execute('update categories_profiles set virtual = (1!=1)')
+    Profile.find(:all).each do |profile|
+      profile.category_ids = profile.categories.map(&:id)
+    end
   end
 
   def self.down
