53f623e81af86705ebd324fe03d4c9f520d45231
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-07-03 01:41:45 +0000

    ActionItem521: removing migration
    the field was already there
    
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@2139 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- db/migrate/044_add_region_to_profile.rb
+++
@@ -1,9 +0,0 @@
-class AddRegionToProfile < ActiveRecord::Migration
-  def self.up
-    add_column :profiles, :region_id, :integer
-  end
-
-  def self.down
-    remove_column :profiles, :region_id
-  end
-end
