0ed464f70b8989f8922adab086a1a96e0e108734
MoisesMachado <MoisesMachado@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-07-10 23:25:20 +0000

    ActionItem546: printing the category name instead of dumping the object
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@2203 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -625,7 +625,7 @@ module ApplicationHelper
   def search_page_title(title, options={})
     title = "<h1>" + title + "</h1>"
     title += "<h2 align='center'>" + _("Searched for '%s'") % options[:query] + "</h2>" if !options[:query].blank?
-    title += "<h2 align='center'>" + _("In category %s") % options[:category] + "</h2>" if !options[:category].blank?
+    title += "<h2 align='center'>" + _("In category %s") % options[:category].name + "</h2>" if !options[:category].blank?
     title += "<h2 align='center'>" + _("within %d from %s") % [options[:distance], options[:region]] + "</h2>" if !options[:distance].blank? && !options[:region].blank?
     title += "<h2 align='center'>" + _("%d results found") % options[:total_results] + "</h2>" if !options[:total_results].blank?
     title
