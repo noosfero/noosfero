f7acf324052ec0447f410b3753375655bf380f6c
MoisesMachado <MoisesMachado@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-07-12 20:23:28 +0000

    ActionItem514: re added a route to make application controller and my_profile controller tests work
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@2238 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,9 @@ ActionController::Routing::Routes.draw do |map|
   ## Public controllers
   ######################################################
 
-  # You can have the root of your site routed by hooking up '' 
+ map.connect 'test/:controller/:action/:id'  , :controller => /.*test.*/ 
+ 
+ # You can have the root of your site routed by hooking up '' 
   # -- just remember to delete public/index.html.
   map.connect '', :controller => "home"
 
