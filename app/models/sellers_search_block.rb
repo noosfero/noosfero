86f4b04a61c186bfd11571cf7f39134266944590
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-07-19 19:37:34 +0000

    ActionItem559: fixing SellersSearchBlock to use the editable title
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@2289 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/models/sellers_search_block.rb
+++ app/models/sellers_search_block.rb
@@ -4,11 +4,20 @@ class SellersSearchBlock < Block
     _('A search for enterprises by products selled and local')
   end
 
+  def self.short_description
+    _('Sellers search block')
+  end
+
+  def default_title
+    _('Search for sellers')
+  end
+
   def content
+    title = self.title
     lambda do
       @categories = ProductCategory.find(:all)
       @regions = Region.find(:all).select{|r|r.lat && r.lng}
-      render :file => 'search/_sellers_form'
+      render :file => 'search/_sellers_form', :locals => { :title => title }
     end
   end
 end
--- test/unit/sellers_search_block_test.rb
+++ test/unit/sellers_search_block_test.rb
@@ -1,8 +1,19 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class SellersSearchBlockTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+
+  should 'provide description' do
+    assert_not_equal Block.description, SellersSearchBlock.description
   end
+
+  should 'provide default title' do
+    assert_not_equal Block.new.default_title, SellersSearchBlock.new.default_title
+  end
+
+  should 'not use a fixed title' do
+    block = SellersSearchBlock.new(:title => 'my custom title')
+    expects(:render).with(:file => 'search/_sellers_form', :locals => { :title => 'my custom title'})
+    instance_eval(&block.content)
+  end
+
 end
