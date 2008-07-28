57d1291a611bb9e32750024b50e1602b44a0af3f
MoisesMachado <MoisesMachado@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-07-28 15:19:56 +0000

    ActionItem597: fixed
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@2331 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- app/helpers/enterprise_homepage_helper.rb
+++ app/helpers/enterprise_homepage_helper.rb
@@ -13,7 +13,7 @@ module EnterpriseHomepageHelper
       [ _('Economic activity:'), :economic_activity ]
     ].each { | name, att |
       if profile.send( att ) and not profile.send( att ).blank?
-        data << content_tag( 'li', content_tag('strong', name) +' '+ profile.send( att ) ) +"\n"
+        data << content_tag( 'li', content_tag('strong', name) +' '+ profile.send( att ).to_s ) +"\n"
       end
     }
     if profile.respond_to?(:distance) and !profile.distance.nil?
