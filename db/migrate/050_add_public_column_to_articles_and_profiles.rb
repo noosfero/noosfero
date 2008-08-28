c0e66bcf685c1185f872de7537c15cee09ccdb2f
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-08-28 21:06:44 +0000

    ActionItem629: updating existing data in migration
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@2429 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- db/migrate/050_add_public_column_to_articles_and_profiles.rb
+++ db/migrate/050_add_public_column_to_articles_and_profiles.rb
@@ -1,6 +1,8 @@
 class AddPublicColumnToArticlesAndProfiles < ActiveRecord::Migration
   def self.up
     add_column :profiles, :public_profile, :boolean, :default => true
+    execute("update profiles set public_profile = (1>0) where data not like '%public_profile: false%'")
+
     add_column :articles, :public_article, :boolean, :default => true
     add_column :article_versions, :public_article, :boolean, :default => true
   end
--- db/migrate/051_add_edit_appearance_permission.rb
+++ db/migrate/051_add_edit_appearance_permission.rb
@@ -1,14 +1,14 @@
 class AddEditAppearancePermission < ActiveRecord::Migration
 
   def self.up
-    [ Profile::Roles.admin, Environment::Roles.admin].each do |item|
+    [ Profile::Roles.admin, Environment::Roles.admin].compact.each do |item|
       item.permissions += [ 'edit_appearance' ]
       item.save!
     end
   end
 
   def self.down
-    [ Profile::Roles.admin, Environment::Roles.admin].each do |item|
+    [ Profile::Roles.admin, Environment::Roles.admin].compact.each do |item|
       item.permissions -= [ 'edit_appearance' ]
       item.save!
     end
