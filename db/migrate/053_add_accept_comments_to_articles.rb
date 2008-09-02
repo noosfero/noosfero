7c877fc3db601682c29100416e81700314d408de
MoisesMachado <MoisesMachado@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-09-02 20:36:16 +0000

    ActionItem147: updated accept_comments column for existing articles
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@2443 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- db/migrate/053_add_accept_comments_to_articles.rb
+++ db/migrate/053_add_accept_comments_to_articles.rb
@@ -1,7 +1,9 @@
 class AddAcceptCommentsToArticles < ActiveRecord::Migration
   def self.up
     add_column :articles, :accept_comments, :boolean, :default => true
+    execute 'update articles set accept_comments = (1>0)'
     add_column :article_versions, :accept_comments, :boolean, :default => true
+    execute 'update article_versions set accept_comments = (1>0)'
   end
 
   def self.down
