64e03235c905ddec85dd3dbf135b94b8085510cc
AntonioTerceiro <AntonioTerceiro@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-09-04 22:49:31 +0000

    ActionItem642: fixing tests (fuck, Aurélio!)
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/trunk@2461 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- test/unit/lightbox_helper_test.rb
+++ test/unit/lightbox_helper_test.rb
@@ -4,6 +4,10 @@ class LightboxHelperTest < Test::Unit::TestCase
 
   include LightboxHelper
 
+  def setup
+    stubs(:_).with(anything).returns('TEXT')
+  end
+
   should 'include lightbox properly' do
     expects(:stylesheet_link_tag).with('lightbox').returns('[lightbox.css]')
     expects(:javascript_include_tag).with('lightbox').returns('[lightbox.js]')
@@ -16,36 +20,36 @@ class LightboxHelperTest < Test::Unit::TestCase
   end
 
   should 'provide lightbox_link_to helper' do
-    expects(:link_to).with('text', { :action => 'view', :id => '1' }, { :class => 'lbOn', :id => 'my-link' }).returns('[link]')
+    expects(:link_to).with('text', { :action => 'view', :id => '1' }, has_entries({ :class => 'lbOn', :id => 'my-link' })).returns('[link]')
     assert_equal '[link]', lightbox_link_to('text', { :action => 'view', :id => '1'}, { :id => 'my-link' })
   end
 
   should 'merge existing :class option in lightbox_link_to' do
-    expects(:link_to).with('text', { :action => 'view', :id => '1' }, { :class => 'lbOn my-button', :id => 'my-link' }).returns('[link]')
+    expects(:link_to).with('text', { :action => 'view', :id => '1' }, has_entries({ :class => 'lbOn my-button', :id => 'my-link' })).returns('[link]')
     assert_equal '[link]', lightbox_link_to('text', { :action => 'view', :id => '1'}, { :class => 'my-button',  :id => 'my-link' })
 
   end
 
   should 'provide link to close lightbox' do
-    expects(:button).with(:close, 'text', '#', { :class => 'lbAction', :rel => 'deactivate', :id => 'my-id' }).returns('[close-lightbox]')
+    expects(:button).with(:close, 'text', '#', has_entries({ :class => 'lbAction', :rel => 'deactivate', :id => 'my-id' })).returns('[close-lightbox]')
 
     assert_equal '[close-lightbox]', lightbox_close_button('text', :id => 'my-id')
   end
 
   should 'merge existing :class option in lightbox_close_button' do
-    expects(:button).with(:close, 'text', '#', { :class => 'lbAction my-class', :rel => 'deactivate', :id => 'my-id' }).returns('[close-lightbox]')
+    expects(:button).with(:close, 'text', '#', has_entries({ :class => 'lbAction my-class', :rel => 'deactivate', :id => 'my-id' })).returns('[close-lightbox]')
 
     assert_equal '[close-lightbox]', lightbox_close_button('text', :class => 'my-class', :id => 'my-id' )
   end
 
   should 'provide lightbox_button' do
-    expects(:button).with('type', 'label', { :action => 'popup'}, { :class => 'lbOn' }).returns('[button]')
+    expects(:button).with('type', 'label', { :action => 'popup'}, has_entries({ :class => 'lbOn' })).returns('[button]')
 
     assert_equal '[button]', lightbox_button('type', 'label', { :action => 'popup'})
   end
 
   should 'provide lightbox_icon_button' do
-    expects(:icon_button).with('type', 'label', { :action => 'popup'}, { :class => 'lbOn' }).returns('[button]')
+    expects(:icon_button).with('type', 'label', { :action => 'popup'}, has_entries({ :class => 'lbOn' })).returns('[button]')
 
     assert_equal '[button]', lightbox_icon_button('type', 'label', { :action => 'popup'})
   end
