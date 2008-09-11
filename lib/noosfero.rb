c1d3f1a04b158fd56038bb8a1485cf7d3a904a51
MoisesMachado <MoisesMachado@3f533792-8f58-4932-b0fe-aaf55b0a4547>
2008-09-11 18:26:30 +0000

    ActionItem678: bumping version
    git-svn-id: https://svn.colivre.coop.br/svn/noosfero/branches/0.11.x@2509 3f533792-8f58-4932-b0fe-aaf55b0a4547

--- lib/noosfero.rb
+++ lib/noosfero.rb
@@ -1,6 +1,6 @@
 module Noosfero
   PROJECT = 'noosfero'
-  VERSION = '0.11.2'
+  VERSION = '0.11.3'
   SVN_ROOT = 'https://svn.colivre.coop.br/svn/noosfero'
 
   def self.pattern_for_controllers_in_directory(dir)
