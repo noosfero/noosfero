7abf5f145cd0748a28a03909fa1964fe5f8666d2
Joenio Costa <joenio@colivre.coop.br>
2008-12-22 10:30:24 -0300

    ActionItem860: remove link to wikimapia

--- app/models/localization_block.rb
+++ app/models/localization_block.rb
@@ -16,7 +16,6 @@ class LocalizationBlock < Block
       block_title(title) +
       content_tag('div',
       '<img src="http://maps.google.com/staticmap?center='+profile.lat.to_s()+','+profile.lng.to_s()+'&zoom=8&size=205x250&maptype=roadmap&markers='+profile.lat.to_s()+','+profile.lng.to_s()+',green&key='+GoogleMaps::key+'&sensor=false"/>',
-      :onclick => "window.open('http://wikimapia.org/#lat=#{profile.lat.to_s()}&lon=#{profile.lng.to_s()}&z=12&l=0&m=m&v=2','_blank','width=750,height=500')",
       :class => 'the-localization-map' ) :
       content_tag('i', _('This profile has no geographical position registered.'))
     end
