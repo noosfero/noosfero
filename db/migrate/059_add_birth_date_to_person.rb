fe11a9f3b635b44140e50613f91ac9f29bb8c170
Antonio Terceiro <terceiro@softwarelivre.org>
2009-01-27 19:52:58 -0300

    ActionItem916: fixing tests

--- db/migrate/059_add_birth_date_to_person.rb
+++ db/migrate/059_add_birth_date_to_person.rb
@@ -47,62 +47,62 @@ if $PROGRAM_NAME == __FILE__
   class ConvertDatesTest <  Test::Unit::TestCase
 
     should 'convert with slash' do
-      date = ConvertDates.convert('10/01/2009')
+      date = AddBirthDateToPerson::ConvertDates.convert('10/01/2009')
       assert_equal [10, 1, 2009], [date.day, date.month, date.year]
     end
 
     should 'convert with hyphen' do
-      date = ConvertDates.convert('10-01-2009')
+      date = AddBirthDateToPerson::ConvertDates.convert('10-01-2009')
       assert_equal [10, 1, 2009], [date.day, date.month, date.year]
     end
 
     should 'convert with dot' do
-      date = ConvertDates.convert('10.01.2009')
+      date = AddBirthDateToPerson::ConvertDates.convert('10.01.2009')
       assert_equal [10, 1, 2009], [date.day, date.month, date.year]
     end
 
     should 'convert with slash and space' do
-      date = ConvertDates.convert('10/ 01/ 2009')
+      date = AddBirthDateToPerson::ConvertDates.convert('10/ 01/ 2009')
       assert_equal [10, 1, 2009], [date.day, date.month, date.year]
     end
 
     should 'convert with empty to nil' do
-      date = ConvertDates.convert('')
+      date = AddBirthDateToPerson::ConvertDates.convert('')
       assert_nil date
     end
 
     should 'convert with nil to nil' do
-      date = ConvertDates.convert(nil)
+      date = AddBirthDateToPerson::ConvertDates.convert(nil)
       assert_nil date
     end
 
     should 'convert with two digits 1900' do
-      date = ConvertDates.convert('10/01/99')
+      date = AddBirthDateToPerson::ConvertDates.convert('10/01/99')
       assert_equal [10, 1, 1999], [date.day, date.month, date.year]
     end
 
     should 'convert with two digits 2000' do
-      date = ConvertDates.convert('10/01/09')
+      date = AddBirthDateToPerson::ConvertDates.convert('10/01/09')
       assert_equal [10, 1, 2009], [date.day, date.month, date.year]
     end
 
     should 'convert with two numbers' do
-      date = ConvertDates.convert('10/01')
+      date = AddBirthDateToPerson::ConvertDates.convert('10/01')
       assert_equal [10, 1, (Date.today.year - 100)], [date.day, date.month, date.year]
     end
 
     should 'convert to nil if non-numeric date' do
-      date = ConvertDates.convert('10 de agosto de 2009')
+      date = AddBirthDateToPerson::ConvertDates.convert('10 de agosto de 2009')
       assert_nil date
     end
 
     should 'do nothing if date' do
-      date = ConvertDates.convert(Date.today)
+      date = AddBirthDateToPerson::ConvertDates.convert(Date.today)
       assert_equal Date.today, date
     end
 
     should 'return nil when not string nor date' do
-      date = ConvertDates.convert(1001)
+      date = AddBirthDateToPerson::ConvertDates.convert(1001)
       assert_nil date
     end
 
