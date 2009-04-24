fb658e907a24a82df63b51c3d7b2cf801658bea9
Moises Machado <moises@colivre.coop.br>
2009-04-24 07:26:33 -0300

    ActionItem1003: fixing sqlite extension for debian squeeze

--- lib/sqlite_extension.rb
+++ lib/sqlite_extension.rb
@@ -2,19 +2,19 @@ if ActiveRecord::Base.connection.adapter_name.downcase == 'sqlite'
 
   database = ActiveRecord::Base.connection.raw_connection
 
-  database.create_function('pow', 2, :numeric) do |func, base, exponent|
+  database.create_function('pow', 2, 1) do |func, base, exponent|
     func.set_result(base.to_f ** exponent.to_f)
   end
   
-  database.create_function('sqrt', 1, :numeric) do |func, value|
+  database.create_function('sqrt', 1, 1) do |func, value|
     func.set_result(Math.sqrt(value))
   end
 
-  database.create_function('radians', 1, :numeric) do |func, value|
+  database.create_function('radians', 1, 1) do |func, value|
     func.set_result(value.to_f * Math::PI / 180.0)
   end
 
-  database.create_function('spheric_distance', 5, :real) do |func, lat1, long1, lat2, long2, radius|
+  database.create_function('spheric_distance', 5, 1) do |func, lat1, long1, lat2, long2, radius|
     func.set_result(
       radius.to_f * Math.acos(
 	[1,
