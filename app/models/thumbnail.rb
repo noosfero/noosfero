021594280faafc1422b31f2bf85646b8812e2197
Antonio Terceiro <terceiro@colivre.coop.br>
2009-05-15 19:30:29 -0300

    ActionItem305: workaround for importing users

--- app/models/thumbnail.rb
+++ app/models/thumbnail.rb
@@ -1,5 +1,5 @@
 class Thumbnail < ActiveRecord::Base
   has_attachment :storage => :file_system,
-    :content_type => :image
+    :content_type => :image, :max_size => 5.megabytes
   validates_as_attachment
 end
