84e3a2a00b1e4034d8e7ec8129b20cf110f951a1
Moises Machado <moises@colivre.coop.br>
2009-06-06 17:36:44 -0300

    ActionItem1080: fixing association in migration

--- db/migrate/069_add_enviroment_id_to_role.rb
+++ db/migrate/069_add_enviroment_id_to_role.rb
@@ -6,7 +6,7 @@ class RoleWithEnvironment < ActiveRecord::Base
 end
 class RoleAssignment < ActiveRecord::Base
   belongs_to :accessor, :polymorphic => true
-  belongs_to :target, :polymorphic => true
+  belongs_to :resource, :polymorphic => true
 end
 
 class AddEnviromentIdToRole < ActiveRecord::Migration
@@ -19,7 +19,7 @@ class AddEnviromentIdToRole < ActiveRecord::Migration
         re = RoleWithEnvironment.new(role.attributes)
         re.environment = env
         re.save
-        RoleAssignment.find_all_by_role_id(role.id).select{|ra| (ra.target.kind_of?(Profile) ? ra.target.environment_id : ra.target.id) == env.id }.each do |ra|
+        RoleAssignment.find_all_by_role_id(role.id).select{|ra| (ra.resource.kind_of?(Profile) ? ra.resource.environment_id : ra.resource.id) == env.id }.each do |ra|
           ra.role_id = re.id
           ra.save
         end
