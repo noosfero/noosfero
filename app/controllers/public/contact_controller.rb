77861d20827f064e837054347732fb8ac1af4999
Antonio Terceiro <terceiro@colivre.coop.br>
2009-07-15 12:51:44 -0300

    ActionItem1167: hide not informed city and state

--- app/controllers/public/contact_controller.rb
+++ app/controllers/public/contact_controller.rb
@@ -10,8 +10,8 @@ class ContactController < PublicController
     if request.post? && params[self.icaptcha_field].blank?
       @contact = Contact.new(params[:contact])
       @contact.dest = profile
-      @contact.city = (!params[:city].blank? && City.exists?(params[:city])) ? City.find(params[:city]).name : _('Missing')
-      @contact.state = (!params[:state].blank? && State.exists?(params[:state])) ? State.find(params[:state]).name : _('Missing')
+      @contact.city = (!params[:city].blank? && City.exists?(params[:city])) ? City.find(params[:city]).name : nil
+      @contact.state = (!params[:state].blank? && State.exists?(params[:state])) ? State.find(params[:state]).name : nil
       if @contact.deliver
         flash[:notice] = _('Contact successfully sent')
         redirect_to :action => 'new'
