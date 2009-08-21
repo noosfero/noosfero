21196194015176edd4061a3b1381222b8daae818
Joenio Costa <joenio@colivre.coop.br>
2009-08-21 13:13:26 -0300

    ActionItem1176: (again) Links in WYSIWYG keep changing & to &amp in each edition

--- test/unit/tiny_mce_article_test.rb
+++ test/unit/tiny_mce_article_test.rb
@@ -4,7 +4,9 @@ class TinyMceArticleTest < Test::Unit::TestCase
 
   def setup
     Article.rebuild_index
+    @profile = create_user('zezinho').person
   end
+  attr_reader :profile
   
   # this test can be removed when we get real tests for TinyMceArticle 
   should 'be an article' do
@@ -20,16 +22,21 @@ class TinyMceArticleTest < Test::Unit::TestCase
   end
 
   should 'be found when searching for articles by query' do
-    ze = create_user('zezinho').person
-    tma = TinyMceArticle.create!(:name => 'test tinymce article', :body => '---', :profile => ze)
+    tma = TinyMceArticle.create!(:name => 'test tinymce article', :body => '---', :profile => profile)
     assert_includes TinyMceArticle.find_by_contents('article'), tma
     assert_includes Article.find_by_contents('article'), tma
   end
 
   should 'not sanitize target attribute' do
-    ze = create_user('zezinho').person
-    article = TinyMceArticle.create!(:name => 'open link in new window', :body => "open <a href='www.invalid.com' target='_blank'>link</a> in new window", :profile => ze)
+    article = TinyMceArticle.create!(:name => 'open link in new window', :body => "open <a href='www.invalid.com' target='_blank'>link</a> in new window", :profile => profile)
     assert_tag_in_string article.body, :tag => 'a', :attributes => {:target => '_blank'}
+  end
+
+  should 'not translate & to amp; over times' do
+    article = TinyMceArticle.create!(:name => 'link', :body => "<a href='www.invalid.com?param1=value&param2=value'>link</a>", :profile => profile)
+    assert article.save
+    assert_no_match /&amp;amp;/, article.body
+    assert_match /&amp;/, article.body
   end
 
 end
