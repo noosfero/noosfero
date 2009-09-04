6fc45cfb46eea3796ecf8094f3f7a31160a23447
Antonio Terceiro <terceiro@colivre.coop.br>
2009-09-04 15:37:50 -0300

    ActionItem1240: setting up the environment

--- config/environments/cucumber.rb
+++ config/environments/cucumber.rb
@@ -17,5 +17,5 @@ config.action_mailer.delivery_method = :test
 
 config.gem "cucumber",    :lib => false,        :version => ">=0.3.11" unless File.directory?(File.join(Rails.root, 'vendor/plugins/cucumber'))
 config.gem "webrat",      :lib => false,        :version => ">=0.4.4" unless File.directory?(File.join(Rails.root, 'vendor/plugins/webrat'))
-config.gem "rspec",       :lib => false,        :version => ">=1.2.6" unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec'))
+config.gem "rspec",       :lib => 'spec',        :version => ">=1.2.6" unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec'))
 config.gem "rspec-rails", :lib => 'spec/rails', :version => ">=1.2.6" unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
--- features/support/env.rb
+++ features/support/env.rb
@@ -22,3 +22,9 @@ end
 
 require 'cucumber/rails/rspec'
 require 'webrat/core/matchers'
+
+# set the very basic fixtures for Noosfero
+Fixtures.reset_cache
+fixtures_folder = File.join(RAILS_ROOT, 'test', 'fixtures')
+fixtures = ['environments', 'roles']
+Fixtures.create_fixtures(fixtures_folder, fixtures)
