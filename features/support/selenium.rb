7a8bf18708b0069c87d67a8f65e504da33c43fa2
Joenio Costa <joenio@colivre.coop.br>
2009-10-31 00:14:44 -0300

    Infrastructure for cucumber testing with selenium

---
+++ features/support/selenium.rb
@@ -0,0 +1,23 @@
+Webrat.configure do |config|
+  config.mode = :selenium
+end
+
+Cucumber::Rails::World.use_transactional_fixtures = false
+
+require 'database_cleaner'
+require 'database_cleaner/cucumber'
+
+DatabaseCleaner.clean_with :truncation
+DatabaseCleaner.strategy = :truncation
+
+Before do
+  Fixtures.reset_cache
+  fixtures_folder = File.join(RAILS_ROOT, 'test', 'fixtures')
+  fixtures = ['environments', 'roles']
+  Fixtures.create_fixtures(fixtures_folder, fixtures)
+  DatabaseCleaner.start
+end
+
+After do
+  DatabaseCleaner.clean
+end
