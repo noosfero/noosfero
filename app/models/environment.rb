d1933d19f2cfe0548e76d34bd68037c377c8a9df
Antonio Terceiro <terceiro@colivre.coop.br>
2009-11-05 11:40:13 -0300

    Better wording to avoid confusion.

--- app/models/environment.rb
+++ app/models/environment.rb
@@ -129,7 +129,7 @@ class Environment < ActiveRecord::Base
       'display_header_footer_explanation' => _("Display explanation about header and footer"),
       'articles_dont_accept_comments_by_default' => _("Articles don't accept comments by default"),
       'organizations_are_moderated_by_default' => _("Organizations have moderated publication by default"),
-      'enable_organization_url_change' => _("Allow organizations to change their address"),
+      'enable_organization_url_change' => _("Allow organizations to change their URL"),
       'admin_must_approve_new_communities' => _("Admin must approve creation of communities"),
     }
   end
