f88dad03825e0e8aadb17d0b902239cd3781c142
Rodrigo Souto <rodrigo@colivre.coop.br>
2009-11-26 09:57:39 -0300

    Products accepts prices in brazilian's or american's format
      * Brazilian's currency format: 123.456,78
      * American's currency format: 123,456.78
    
    ActionItem1322

--- app/models/product.rb
+++ app/models/product.rb
@@ -67,4 +67,36 @@ class Product < ActiveRecord::Base
     enterprise.public_profile
   end
 
+  def price=(value)
+    if value.is_a?(String)
+      super(currency_to_float(value))
+    else
+      super(value)
+    end
+  end
+
+  def currency_to_float( num )
+    if num.count('.') == 1 && num.count(',') == 0
+      # number like "12.34"
+      return num.to_f
+    end
+
+    if num.count('.') == 0 && num.count(',') == 1
+      # number like "12,34"
+      return num.tr(',','.').to_f
+    end
+
+    if num.count('.') > 0 && num.count(',') > 0
+      # number like "12.345.678,90" or "12,345,678.90"
+      dec_sep = num.tr('0-9','')[-1].chr
+      return num.tr('^0-9'+dec_sep,'').tr(dec_sep,'.').to_f
+    end
+
+    # if you are here is because there is only one
+    # separator and this appears 2 times or more.
+    # number like "12.345.678" or "12,345,678"
+
+    return num.tr(',.','').to_f
+  end
+
 end
