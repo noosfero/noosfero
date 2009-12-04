173c8a8759b9297b99cb536348ae549a07e2a3b9
Antonio Terceiro <terceiro@colivre.coop.br>
2009-12-04 09:27:08 -0300

    Fixing LocationBlock tests
    (ActionItem1256)

--- test/unit/location_block_test.rb
+++ test/unit/location_block_test.rb
@@ -1,36 +1,36 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-class LocalizationBlockTest < Test::Unit::TestCase
+class LocationBlockTest < Test::Unit::TestCase
 
   def setup
     @profile = create_user('lele').person
-    @block = LocalizationBlock.new
+    @block = LocationBlock.new
     @profile.boxes.first.blocks << @block
     @block.save!
   end
   attr_reader :block, :profile
 
   should 'provide description' do
-    assert_not_equal Block.description, LocalizationBlock.description
+    assert_not_equal Block.description, LocationBlock.description
   end
 
   should 'display no localization map without lat' do
-    assert_tag_in_string block.content.call, :tag => 'i'
+    assert_tag_in_string block.content, :tag => 'i'
   end
 
   should 'display localization map' do
     profile.lat = 0
     profile.lng = 0
     profile.save!
-    assert_tag_in_string block.content.call, :tag => 'img'
+    assert_tag_in_string block.content, :tag => 'img'
   end
 
   should 'be editable' do
-    assert LocalizationBlock.new.editable?
+    assert LocationBlock.new.editable?
   end
   
   should 'default title be blank by default' do
-    assert_equal '', LocalizationBlock.new.title
+    assert_equal '', LocationBlock.new.title
   end
 
 end
