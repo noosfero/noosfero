b40cea44a70c3c9703e548d4362baff1c38e6e63
Daniela Soares Feitosa <danielafeitosa@colivre.coop.br>
2010-01-15 10:55:15 -0300

    Thickbox not looping when trying to login while viewing an image
       * thickbox_inline_popup_link has url as parameter
       * thickbox_inline_popup_icon has url as parameter
    
    (ActionItem1372)

--- app/helpers/thickbox_helper.rb
+++ app/helpers/thickbox_helper.rb
@@ -1,9 +1,9 @@
 module ThickboxHelper
-  def thickbox_inline_popup_link(title, id, options = {})
-    link_to(title, "#TB_inline?height=300&width=500&inlineId=#{id}&modal=true", {:class => 'thickbox'}.merge(options))
+  def thickbox_inline_popup_link(title, url, id, options = {})
+    link_to(title, url_for(url) + "#TB_inline?height=300&width=500&inlineId=#{id}&modal=true", {:class => 'thickbox'}.merge(options))
   end
-  def thickbox_inline_popup_icon(type, title, id, options = {})
-    icon_button(type, title, "#TB_inline?height=300&width=500&inlineId=#{id}&modal=true", {:class => "thickbox"}.merge(options))
+  def thickbox_inline_popup_icon(type, title, url, id, options = {})
+    icon_button(type, title, url_for(url) + "#TB_inline?height=300&width=500&inlineId=#{id}&modal=true", {:class => "thickbox"}.merge(options))
   end
   def thickbox_close_button(title)
     button_to_function(:close, title, 'tb_remove();')
