02fb29ba5c9ad433a9eed2836cede7c5fff4afef
Antonio Terceiro <terceiro@colivre.coop.br>
2010-01-20 14:01:37 -0300

    Updating db/schema.rb after migration renumbering

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 80) do
+ActiveRecord::Schema.define(:version => 81) do
 
   create_table "article_versions", :force => true do |t|
     t.integer  "article_id"
