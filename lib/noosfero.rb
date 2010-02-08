1ea7ee77d8ddf880f167d850a6e313c889c43277
Antonio Terceiro <terceiro@colivre.coop.br>
2010-02-08 19:08:38 -0300

    0.22.0 release

--- lib/noosfero.rb
+++ lib/noosfero.rb
@@ -1,6 +1,6 @@
 module Noosfero
   PROJECT = 'noosfero'
-  VERSION = '0.21.5'
+  VERSION = '0.22.0'
 
   def self.pattern_for_controllers_in_directory(dir)
     disjunction = controllers_in_directory(dir).join('|')
