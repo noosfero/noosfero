6a5ee4498aac441b2fe4cc2d23db2f67a8a678b3
Antonio Terceiro <terceiro@colivre.coop.br>
2010-02-11 14:27:17 -0300

    Reviewing distributed tarball
    I'm wondering whether we should just generate distribute a tarball of the
    git repository instead of explicitly listing the files that rake should
    put in this tarball.

---
+++ config/initializers/default_icon_theme.rb
@@ -0,0 +1,11 @@
+# create the symlink to the default theme if it does not exist
+default = File.join(RAILS_ROOT, 'public', 'designs', 'icons', 'default')
+if !File.exists?(default)
+  File.symlink('tango', default)
+end
+
+# create a symlink to system-wide Tango icon set if it does not exist
+tango_symlink = File.join(RAILS_ROOT, 'public', 'designs', 'icons', 'tango', 'Tango')
+if !File.exist?(tango_symlink)
+  File.symlink('/usr/share/icons/Tango', tango_symlink)
+end
