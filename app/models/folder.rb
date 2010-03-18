d0f8d245c7304e20f86be76e84de59e62f30c889
Rodrigo Souto <rodrigo@colivre.coop.br>
2010-03-18 10:34:39 -0300

    Listing folder items
    * Checking if each children of the folder should be displayed to the
    	  current user by using the 'display_to?' method.
    	* Moving some test from the folder test to the folder_helper test.
    
    (ActionItem1396)

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -39,7 +39,7 @@ class Folder < Article
   end
 
   def folder
-    content_tag('div', body) + tag('hr') + (children.empty? ? content_tag('em', _('(empty folder)')) : list_articles(children))
+    content_tag('div', body) + tag('hr')
   end
 
   def image_gallery
