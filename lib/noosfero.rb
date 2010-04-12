dca8a85db4b205a0d8d6b28c79ac3a62e61b81ef
Antonio Terceiro <terceiro@colivre.coop.br>
2010-04-12 12:31:17 -0300

    Bunping version number

--- lib/noosfero.rb
+++ lib/noosfero.rb
@@ -1,6 +1,6 @@
 module Noosfero
   PROJECT = 'noosfero'
-  VERSION = '0.22.3'
+  VERSION = '0.23.0'
 
   def self.pattern_for_controllers_in_directory(dir)
     disjunction = controllers_in_directory(dir).join('|')
