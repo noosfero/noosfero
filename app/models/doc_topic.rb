0cd4c3774cab624b27d07e0a7fd74a713e19fb49
Daniela Soares Feitosa <danielafeitosa@colivre.coop.br>
2010-04-22 22:46:53 -0300

    Ordering topics of documentation
    Adding classes to h1 of topics
        Translation to pt-br in infinitive
    
    (ActionItem1487)

--- app/models/doc_topic.rb
+++ app/models/doc_topic.rb
@@ -4,15 +4,26 @@ class DocTopic < DocItem
       raise DocItem::NotFound
     end
     lines = File.readlines(file) 
-    title = _find_title(lines)
+    title_line = _find_title(lines)
     File.basename(file) =~ /(.*)\.([^\.]+)\.xhtml$/
     id = $1
     language = $2
-    new(:id => id, :title => title, :text => lines.join, :language => language)
+    new(:id => id, :title => title(title_line), :text => lines.join, :language => language, :order => order(title_line))
   end
 
   def self._find_title(lines)
-    lines.find {|line| line =~ /^<h1>(.*)<\/h1>/ }
+    lines.find {|line| line =~ /^(<h1.*>.*<\/h1>)/ }
     $1
   end
+
+  def self.title(line)
+    line =~ /<h1.*>(.*)<\/h1>/
+    $1
+  end
+
+  def self.order(line)
+    line =~ /<h1 class="order-(.*)">.*<\/h1>/
+    $1
+  end
+
 end
