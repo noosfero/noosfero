4280df894b6bac6bed15b60c29ba270e13e31c8d
Fernanda Lopes <nanda.listas+psl@gmail.com>
2010-05-08 17:51:03 -0300

    class ActsAsSearchable transformed into a module
    (ActionItem1519)

--- lib/acts_as_searchable.rb
+++ lib/acts_as_searchable.rb
@@ -1,35 +1,42 @@
-class << ActiveRecord::Base
+module ActsAsSearchable
 
-  def acts_as_searchable(options = {})
-    acts_as_ferret({ :remote => true }.merge(options))
-    def find_by_contents(query, ferret_options = {}, db_options = {})
-      pg_options = {}
-      if ferret_options[:page]
-        pg_options[:page] = ferret_options.delete(:page)
+  module ClassMethods
+    def acts_as_searchable(options = {})
+      acts_as_ferret({ :remote => true }.merge(options))
+      extend FindByContents
+    end
+
+    module FindByContents
+      def find_by_contents(query, ferret_options = {}, db_options = {})
+        pg_options = {}
+        if ferret_options[:page]
+          pg_options[:page] = ferret_options.delete(:page)
+        end
+        if ferret_options[:per_page]
+          pg_options[:per_page] = ferret_options.delete(:per_page)
+        end
+
+        ferret_options[:limit] = :all
+
+        # FIXME this is a HORRIBLE HACK
+        ids = find_ids_with_ferret(query, ferret_options)[1][0..8000].map{|r|r[:id].to_i}
+
+        if ids.empty?
+          ids << -1
+        end
+
+        if db_options[:conditions]
+          db_options[:conditions] = sanitize_sql_for_conditions(db_options[:conditions]) + " and #{table_name}.id in (#{ids.join(', ')})"
+        else
+          db_options[:conditions] = "#{table_name}.id in (#{ids.join(', ')})"
+        end
+
+        pg_options[:page] ||= 1
+        result = find(:all, db_options)
+        result.paginate(pg_options)
       end
-      if ferret_options[:per_page]
-        pg_options[:per_page] = ferret_options.delete(:per_page)
-      end
-
-      ferret_options[:limit] = :all
-      
-      # FIXME this is a HORRIBLE HACK
-      ids = find_ids_with_ferret(query, ferret_options)[1][0..8000].map{|r|r[:id].to_i}
-
-      if ids.empty?
-        ids << -1
-      end
-
-      if db_options[:conditions]
-        db_options[:conditions] = sanitize_sql_for_conditions(db_options[:conditions]) + " and #{table_name}.id in (#{ids.join(', ')})"
-      else
-        db_options[:conditions] = "#{table_name}.id in (#{ids.join(', ')})"
-      end
-
-      pg_options[:page] ||= 1
-      result = find(:all, db_options)
-      result.paginate(pg_options)
     end
   end
-
 end
+
+ActiveRecord::Base.send(:extend, ActsAsSearchable::ClassMethods)
