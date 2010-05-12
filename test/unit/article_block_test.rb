13d40bdbca43561d81c5e7729896fa7276ab3640
Daniela Soares Feitosa <danielafeitosa@colivre.coop.br>
2010-05-12 20:03:35 -0300

    Allowing uploaded files to be added on article block
    * Moved article_to_html to ApplicationHelper
    
    (ActionItem1526)

--- test/unit/article_block_test.rb
+++ test/unit/article_block_test.rb
@@ -2,6 +2,8 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class ArticleBlockTest < Test::Unit::TestCase
 
+  include ApplicationHelper
+
   should 'describe itself' do
     assert_not_equal Block.description, ArticleBlock.description
   end
@@ -12,7 +14,7 @@ class ArticleBlockTest < Test::Unit::TestCase
     article.expects(:to_html).returns("Article content")
     block.stubs(:article).returns(article)
 
-    assert_match(/Article content/, block.content)
+    assert_match(/Article content/, instance_eval(&block.content))
   end
 
   should 'refer to an article' do
@@ -58,7 +60,7 @@ class ArticleBlockTest < Test::Unit::TestCase
     block.expects(:title).returns('')
     block.stubs(:article).returns(article)
 
-    assert_equal "<h3 class=\"block-title empty\"></h3>Article content", block.content
+    assert_equal "<h3></h3>Article content", instance_eval(&block.content)
   end
 
   should "display title if defined" do
@@ -68,7 +70,45 @@ class ArticleBlockTest < Test::Unit::TestCase
     block.expects(:title).returns('Article title')
     block.stubs(:article).returns(article)
 
-    assert_equal "<h3 class=\"block-title\">Article title</h3>Article content", block.content
+    assert_equal "<h3>Article title</h3>Article content", instance_eval(&block.content)
   end
 
+  should 'display image if article is an image' do
+    profile = create_user('testuser').person
+    block = ArticleBlock.new
+    image = UploadedFile.create!(:profile => profile, :uploaded_data => fixture_file_upload('/files/rails.png', 'image/png'))
+
+    block.article = image
+    block.save!
+
+    expects(:image_tag).with(image.public_filename(:display), :class => image.css_class_name, :style => 'max-width: 100%').returns('image')
+
+    assert_match(/image/, instance_eval(&block.content))
+  end
+
+  should 'display link to archive if article is an archive' do
+    profile = create_user('testuser').person
+    block = ArticleBlock.new
+    file = UploadedFile.create!(:profile => profile, :uploaded_data => fixture_file_upload('/files/test.txt', 'text/plain'))
+
+    block.article = file
+    block.save!
+
+    assert_tag_in_string instance_eval(&block.content), :tag => 'a', :content => 'test.txt'
+  end
+
+  protected
+
+    def content_tag(tag, text, options = {})
+      "<#{tag}>#{text}</#{tag}>"
+    end
+    def image_tag(arg)
+      arg
+    end
+    def link_to(text, url, options = {})
+      "<a href='#{url.to_s}'>#{text}</a>"
+    end
+    def params
+      {}
+    end
 end
