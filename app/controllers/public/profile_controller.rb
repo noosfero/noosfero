11619aa74dc09890d5d49b65cf0c1bd9b7cfb617
Antonio Terceiro <terceiro@colivre.coop.br>
2010-06-05 14:02:26 -0300

    Apply proper ordering in the per-tag RSS feeds
    (ActionItem1552)

--- app/controllers/public/profile_controller.rb
+++ app/controllers/public/profile_controller.rb
@@ -31,7 +31,7 @@ class ProfileController < PublicController
 
   def tag_feed
     @tag = params[:id]
-    tagged = profile.find_tagged_with(@tag).paginate(:per_page => 20, :page => 1)
+    tagged = profile.articles.paginate(:per_page => 20, :page => 1, :order => 'published_at DESC', :include => :tags, :conditions => ['tags.name LIKE ?', @tag])
     feed_writer = FeedWriter.new
     data = feed_writer.write(
       tagged,
