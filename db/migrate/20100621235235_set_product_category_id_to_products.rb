50a4e204c1a42aa512b42eb10f16f3751b3769ad
Joenio Costa <joenio@colivre.coop.br>
2010-07-21 23:35:30 -0300

    Saving products without validation in migration to set category to products without category
    (ActionItem1593)

--- db/migrate/20100621235235_set_product_category_id_to_products.rb
+++ db/migrate/20100621235235_set_product_category_id_to_products.rb
@@ -2,8 +2,7 @@ class SetProductCategoryIdToProducts < ActiveRecord::Migration
   def self.up
     Product.all(:conditions => { :product_category_id => nil }).each do |product|
       next if product.enterprise.nil?
-      product.product_category = ProductCategory.top_level_for(product.enterprise.environment).first
-      product.save!
+      product.update_attribute(:product_category_id, ProductCategory.top_level_for(product.enterprise.environment).first.id)
     end
   end
 
