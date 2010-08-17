8f6c1f4410ddc407461e04648353bdfe4ef341c9
Antonio Terceiro <terceiro@colivre.coop.br>
2010-08-17 16:14:22 -0300

    Bump version number

--- lib/noosfero.rb
+++ lib/noosfero.rb
@@ -1,6 +1,6 @@
 module Noosfero
   PROJECT = 'noosfero'
-  VERSION = '0.25.2'
+  VERSION = '0.25.3'
 
   def self.pattern_for_controllers_in_directory(dir)
     disjunction = controllers_in_directory(dir).join('|')
