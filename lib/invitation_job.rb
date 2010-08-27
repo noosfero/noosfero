c68d20d71a2cff9d52204a7eabfb6afff8f2ef93
Antonio Terceiro <terceiro@colivre.coop.br>
2010-08-27 18:20:23 -0300

    Fix problem with Delayed::Job and acts_as_ferret
    For some reason when the Profile class is loaded while InvitationJob is
    being performed there is an IOError error caused by acts_as_ferret's DRB
    server. By trial and error I found out that if the class is loaded
    during delayed_job startup, then that problem does not occur.
    
    WTF?
    
    (ActionItem1640)

--- lib/invitation_job.rb
+++ lib/invitation_job.rb
@@ -1,12 +1,8 @@
 class InvitationJob < Struct.new(:person_id, :contacts_to_invite, :message, :profile_id, :contact_list_id)
   def perform
-    begin
-     person = Person.find(person_id)
-     profile = Profile.find(profile_id)
-     Invitation.invite(person, contacts_to_invite, message, profile)
-     ContactList.find(contact_list_id).destroy
-    rescue ActiveRecord::NotFound => e
-      #...
-    end
+    person = Person.find(person_id)
+    profile = Profile.find(profile_id)
+    Invitation.invite(person, contacts_to_invite, message, profile)
+    ContactList.find(contact_list_id).destroy
   end
 end
