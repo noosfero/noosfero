8f1693a84efb66598172c8949473d7071656d0eb
Antonio Terceiro <terceiro@colivre.coop.br>
2010-09-02 19:56:27 -0300

    Workaround for problem with acts_as_ferret in production mode
    (ActionItem1661)

--- lib/create_thumbnails_job.rb
+++ lib/create_thumbnails_job.rb
@@ -1,5 +1,6 @@
 class CreateThumbnailsJob < Struct.new(:class_name, :file_id)
   def perform
+    Article.disable_ferret # acts_as_ferret sucks
     file = class_name.constantize.find(file_id)
     file.create_thumbnails
   end
