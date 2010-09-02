02217397dbf60c3f7a62b2d753b9532303599226
Antonio Terceiro <terceiro@colivre.coop.br>
2010-09-02 18:19:31 -0300

    Fix migration
    The pre-existing images must have their thumbnails_processed flag set to
    true, since they indeed already had the thumbnails created!
    
    (ActionItem1661)

--- db/migrate/20100831193450_add_thumbnails_processed_to_articles.rb
+++ db/migrate/20100831193450_add_thumbnails_processed_to_articles.rb
@@ -2,6 +2,9 @@ class AddThumbnailsProcessedToArticles < ActiveRecord::Migration
   def self.up
      add_column :articles, :thumbnails_processed, :boolean, :default => false
      add_column :article_versions, :thumbnails_processed, :boolean, :default => false
+
+     # the pre-existing images already had their thumbnails created before!
+     execute("update articles set thumbnails_processed = (1>0) where type = 'UploadedFile'")
   end
 
   def self.down
--- db/migrate/20100901203836_add_thumbnails_processed_to_image.rb
+++ db/migrate/20100901203836_add_thumbnails_processed_to_image.rb
@@ -1,9 +1,11 @@
 class AddThumbnailsProcessedToImage < ActiveRecord::Migration
   def self.up
      add_column :images, :thumbnails_processed, :boolean, :default => false
+     # the pre-existing images already had their thumbnails created
+     execute('update images set thumbnails_processed = (1>0)')
   end
 
   def self.down
-     remove_column :images, :thumbnails_processed, :boolean, :default => false
+     remove_column :images, :thumbnails_processed
   end
 end
