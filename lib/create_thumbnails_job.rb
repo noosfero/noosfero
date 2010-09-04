43052ab39f458b31e69bfd17dd1ad086769a7770
Daniela Soares Feitosa <danielafeitosa@colivre.coop.br>
2010-09-04 11:56:46 -0300

    Enabling ferret after create thumbnails
    (ActionItem1661)

--- lib/create_thumbnails_job.rb
+++ lib/create_thumbnails_job.rb
@@ -3,5 +3,6 @@ class CreateThumbnailsJob < Struct.new(:class_name, :file_id)
     Article.disable_ferret # acts_as_ferret sucks
     file = class_name.constantize.find(file_id)
     file.create_thumbnails
+    Article.enable_ferret # acts_as_ferret sucks
   end
 end
