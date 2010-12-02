765d0e876383bfc8c2d5826f81824ed911dbb653
Antonio Terceiro <terceiro@colivre.coop.br>
2010-12-02 16:44:00 -0300

    Changing limit of the blocks created by default
    ActionItem1773

--- app/models/environment.rb
+++ app/models/environment.rb
@@ -132,8 +132,8 @@ class Environment < ActiveRecord::Base
     env.boxes[1].blocks << RecentDocumentsBlock.new
 
     # "right" area
-    env.boxes[2].blocks << CommunitiesBlock.new(:limit => 4)
-    env.boxes[2].blocks << PeopleBlock.new(:limit => 4)
+    env.boxes[2].blocks << CommunitiesBlock.new(:limit => 6)
+    env.boxes[2].blocks << PeopleBlock.new(:limit => 6)
   end
 
   # One Environment can be reached by many domains
