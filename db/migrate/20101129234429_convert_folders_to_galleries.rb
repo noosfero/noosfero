4c9ca01ba5903a0e4346f0a0e9e22d5fc0373b2e
Antonio Terceiro <terceiro@colivre.coop.br>
2010-12-09 19:34:30 -0300

    Now really avoiding crash when passing nil to YAML.load()
    (ActionItem1769)

--- db/migrate/20101129234429_convert_folders_to_galleries.rb
+++ db/migrate/20101129234429_convert_folders_to_galleries.rb
@@ -1,14 +1,14 @@
 class ConvertFoldersToGalleries < ActiveRecord::Migration
   def self.up
     select_all("select id, setting from articles where type = 'Folder'").each do |folder|
-      view_as = YAML.load(folder['setting'] || {})[:view_as]
+      view_as = YAML.load(folder['setting'] || {}.to_yaml)[:view_as]
       update("update articles set type = 'Gallery' where id = %d" % folder['id']) if view_as == 'image_gallery'
     end
   end
 
   def self.down
     select_all("select id, setting from articles where type = 'Gallery'").each do |folder|
-      settings = YAML.load(folder['setting'] || {})
+      settings = YAML.load(folder['setting'] || {}.to_yaml)
       settings[:view_as] = 'image_gallery'
       assignments = ActiveRecord::Base.sanitize_sql_for_assignment(:setting => settings.to_yaml)
       update("update articles set %s, type = 'Folder' where id = %d" % [assignments, folder['id']])
