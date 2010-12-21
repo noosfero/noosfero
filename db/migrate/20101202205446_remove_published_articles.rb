b73a08ddaafdcabc7b34e67ed1752cfe3fd8f851
Antonio Terceiro <terceiro@colivre.coop.br>
2010-12-21 12:37:55 -0300

    Not involving model objects in the migration
    (ActionItem1733)

--- db/migrate/20101202205446_remove_published_articles.rb
+++ db/migrate/20101202205446_remove_published_articles.rb
@@ -1,9 +1,9 @@
 class RemovePublishedArticles < ActiveRecord::Migration
   def self.up
     select_all("SELECT * from articles WHERE type = 'PublishedArticle'").each do |published|
-      reference = Article.exists?(published['reference_article_id']) ? Article.find(published['reference_article_id']) : nil
+      reference = select('select * from articles where id = %d' % published['reference_article_id']).first
       if reference
-        execute(ActiveRecord::Base.sanitize_sql(["UPDATE articles SET type = ?, abstract = ?, body = ? WHERE articles.id  = ?", reference[:type], reference[:abstract], reference[:body], published['id']]))
+        execute(ActiveRecord::Base.sanitize_sql(["UPDATE articles SET type = ?, abstract = ?, body = ? WHERE articles.id  = ?", reference['type'], reference['abstract'], reference['body'], published['id']]))
       else
         execute("DELETE from articles where articles.id  = #{published['id']}")
       end
