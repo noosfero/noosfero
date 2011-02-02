8a99ab3acca7df86e3cb92fdffa7c87d46280333
Keilla Menezes <keilla@colivre.coop.br>
2011-02-02 15:35:30 -0300

    Added date for events with nil date
    (ActionItem1809)

---
+++ db/migrate/20110202141024_set_start_date_of_reference_article.rb
@@ -0,0 +1,11 @@
+class SetStartDateOfReferenceArticle < ActiveRecord::Migration
+  def self.up
+    execute("SELECT articles.id as a_id, articles.start_date as a_start_date, reference.id as r_id, reference.start_date as r_start_date FROM articles INNER JOIN articles reference ON articles.reference_article_id = reference.id WHERE articles.Type = 'Event' AND articles.start_date IS NULL").each do |data|
+      execute("UPDATE articles SET start_date = '#{data['r_start_date']}' WHERE id = #{data['a_id']}")
+    end
+  end
+
+  def self.down
+    say "Nothing to do"
+  end
+end
