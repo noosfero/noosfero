3f21bb2012de56cbb0bda984dd305f1949cb6513
Daniela Soares Feitosa <danielafeitosa@colivre.coop.br>
2011-02-02 21:16:34 -0300

    Added new db/schema and removed unused alias from migration
    (ActionItem1809)

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110127174236) do
+ActiveRecord::Schema.define(:version => 20110202141024) do
 
   create_table "action_tracker", :force => true do |t|
     t.integer  "user_id"
