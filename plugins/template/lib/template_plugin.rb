35b2a6cde0c357e68a31a4bbde17389cd838c3de
Antonio Terceiro <terceiro@colivre.coop.br>
2011-02-22 16:55:25 -0300

    Command-line app for managing plugins
    Run it without any arguments for usage.

---
+++ plugins/template/lib/template_plugin.rb
@@ -0,0 +1,13 @@
+class TemplatePlugin < Noosfero::Plugin
+
+  def self.plugin_name
+    # FIXME
+    "TemplatePlugin"
+  end
+
+  def self.plugin_description
+    # FIXME
+    _("A plugin that does this and that.")
+  end
+
+end
