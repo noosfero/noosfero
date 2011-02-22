6ef63129bf42b9d24fb81862d9b88cef8b4bb67f
Antonio Terceiro <terceiro@colivre.coop.br>
2011-02-22 16:55:24 -0300

    Add route for plugin administration

--- lib/noosfero/plugin/routes.rb
+++ lib/noosfero/plugin/routes.rb
@@ -3,5 +3,6 @@ Dir.glob(File.join(Rails.root, 'config', 'plugins', '*', 'controllers')) do |dir
   map.connect 'plugin/' + plugin_name + '/:action/:id', :controller => plugin_name + '_plugin_environment'
   map.connect 'profile/:profile/plugins/' + plugin_name + '/:action/:id', :controller => plugin_name + '_plugin_profile'
   map.connect 'myprofile/:profile/plugin/' + plugin_name + '/:action/:id', :controller => plugin_name + '_plugin_myprofile'
+  map.connect 'admin/plugin/' + plugin_name + '/:action/:id', :controller => plugin_name + '_plugin_admin'
 end
 
