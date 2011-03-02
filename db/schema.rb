f15a7f20421b94cc5f00f42f94424df9d48247a8
Joenio Costa <joenio@colivre.coop.br>
2011-03-02 19:01:07 -0300

    Moving serialized data to setting field for some Events
    (ActionItem1914)

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110228174632) do
+ActiveRecord::Schema.define(:version => 20110302214607) do
 
   create_table "action_tracker", :force => true do |t|
     t.integer  "user_id"
