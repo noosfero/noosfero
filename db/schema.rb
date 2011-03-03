02c67027f6eb41de07162f4dd8ba83d5865d7eba
Daniela Soares Feitosa <danielafeitosa@colivre.coop.br>
2011-03-03 18:58:52 -0300

    Fixed wrong name on db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -31,8 +31,8 @@ ActiveRecord::Schema.define(:version => 20110228174632) do
     t.integer "profile_id"
   end
 
-  add_index "action_tracker_notifications", ["action_tracker_id", "profile_id"], :name => "index_action_tracker_notifications_on_profile_id_and_action_tra", :unique => true
   add_index "action_tracker_notifications", ["action_tracker_id"], :name => "index_action_tracker_notifications_on_action_tracker_id"
+  add_index "action_tracker_notifications", ["profile_id", "action_tracker_id"], :name => "index_action_tracker_notifications_on_profile_id_and_action_tracker_id", :unique => true
   add_index "action_tracker_notifications", ["profile_id"], :name => "index_action_tracker_notifications_on_profile_id"
 
   create_table "article_versions", :force => true do |t|
