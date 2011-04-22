602c589482532940539d4f5ef211f7ba7bc1ce44
Braulio Bhavamitra <brauliobo@gmail.com>
2011-04-23 02:57:18 +0400

    Set Solr index data path

--- vendor/plugins/acts_as_solr/config/solr_environment.rb
+++ vendor/plugins/acts_as_solr/config/solr_environment.rb
@@ -6,7 +6,7 @@ SOLR_PATH = "#{File.dirname(File.expand_path(__FILE__))}/../solr" unless defined
 SOLR_LOGS_PATH = "#{rails_root_dir}/log" unless defined? SOLR_LOGS_PATH
 SOLR_PIDS_PATH = "#{rails_root_dir}/tmp/pids" unless defined? SOLR_PIDS_PATH
 SOLR_PID_FILE="#{SOLR_PIDS_PATH}/solr.#{ENV['RAILS_ENV']}.pid"
-SOLR_DATA_PATH = "#{rails_root_dir}/solr/#{ENV['RAILS_ENV']}" unless defined? SOLR_DATA_PATH
+SOLR_DATA_PATH = ENV['SOLR_DATA_PATH'] || "#{rails_root_dir}/solr/#{ENV['RAILS_ENV']}" unless defined? SOLR_DATA_PATH
 
 unless defined? SOLR_PORT
   config = YAML::load_file(rails_root_dir+'/config/solr.yml')
