77496dba4c5fa56783f3aa13de8b41a7e5f8515c
Rodrigo Souto <rodrigo@colivre.coop.br>
2011-04-23 02:15:58 -0300

    Signing plugin to hot spots

---
+++ plugins/shopping_cart/lib/shopping_cart_plugin.rb
@@ -0,0 +1,35 @@
+class ShoppingCartPlugin < Noosfero::Plugin
+
+  def self.plugin_name
+    "Shopping Cart"
+  end
+
+  def self.plugin_description
+    _("A shopping cart feature for enterprises")
+  end
+
+  def add_to_cart_button(item)
+     lambda {
+       link_to(_('Add to cart'), "add:#{item.name}",
+         :class => 'cart-add-item',
+         :onclick => "Cart.addItem('#{profile.identifier}', #{item.id}, this); return false"
+       )
+     }
+  end
+
+  alias :product_info_extras :add_to_cart_button
+  alias :catalog_item_extras :add_to_cart_button
+
+  def stylesheet?
+    true
+  end
+
+  def js_files
+    ['cart.js']
+  end
+
+  def body_beginning
+    expanded_template('cart.html.erb',{:cart => context.session[:cart]})
+  end
+
+end
