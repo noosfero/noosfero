dc6e9466e7e4b23db56c89a78e06f5d8d4fcc0ae
Rodrigo Souto <rodrigo@colivre.coop.br>
2011-05-11 13:59:32 -0300

    Fixing mailer extension
    (ActionItem1996)

--- config/initializers/plugins.rb
+++ config/initializers/plugins.rb
@@ -2,4 +2,5 @@ require 'noosfero/plugin'
 require 'noosfero/plugin/manager'
 require 'noosfero/plugin/context'
 require 'noosfero/plugin/active_record'
+require 'noosfero/plugin/mailer_base'
 Noosfero::Plugin.init_system
