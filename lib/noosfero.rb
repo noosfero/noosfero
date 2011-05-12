093c1ebcb09e660d2dc7c1af8acdaede00c736b0
Daniela Soares Feitosa <danielafeitosa@colivre.coop.br>
2011-05-12 01:07:45 -0300

    Bumping version number

--- lib/noosfero.rb
+++ lib/noosfero.rb
@@ -1,6 +1,6 @@
 module Noosfero
   PROJECT = 'noosfero'
-  VERSION = '0.30.2'
+  VERSION = '0.31.0'
 
   def self.pattern_for_controllers_in_directory(dir)
     disjunction = controllers_in_directory(dir).join('|')
