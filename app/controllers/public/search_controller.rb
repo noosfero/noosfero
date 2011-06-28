e6d5780d179d61cff097447d86196875a7730409
Braulio Bhavamitra <brauliobo@gmail.com>
2011-06-28 13:49:02 +0000

    Search refactoring
    - Start search refactoring to a action based controller, like the
      BrowseController
    - Region, city and other advanced options removed in favor of facets
    - Add search contents menu Contents (or Articles) like people one.
    - Removed browse controller and views. They will unified with the
      refactored search controller and views.
    - Initial category search changes, see subsequent commits

--- app/controllers/public/search_controller.rb
+++ app/controllers/public/search_controller.rb
@@ -1,49 +1,135 @@
 class SearchController < PublicController
 
   helper TagsHelper
+  include SearchHelper
 
   before_filter :load_category
-  before_filter :prepare_filter
   before_filter :check_search_whole_site
   before_filter :load_search_assets
-  before_filter :check_valid_assets, :only => [ :assets ]
 
   no_design_blocks
 
-  protected
+  def articles
+    @asset = :articles
+    @query = params[:query] || ''
+    @order ||= [@asset]
+    @results ||= {}
+    @filter = filter
 
-  def load_search_assets
-    @search_in = where_to_search
-    @searching = {}
-    @search_in.each do |key, name|
-      @searching[key] = (params[:asset].blank? && (params[:find_in].nil? || params[:find_in].empty? || params[:find_in].include?(key.to_s))) || (params[:asset] == key.to_s)
-    end
-  end
-
-  def prepare_filter
-    if @category
-      @noosfero_finder = CategoryFinder.new(@category)
+    pg_options = paginate_options(@asset, limit, params[:per_page])
+    if !@query.blank?
+      ret = asset_class(@asset).find_by_contents(@query, pg_options, solr_options(@asset, params[:facet], params[:order_by]))
+      @results[@asset] = ret[:results]
+      @facets = ret[:facets]
     else
-      @noosfero_finder = EnvironmentFinder.new(@environment)
+      @results[@asset] = asset_class(@asset).send('paginate', :all, pg_options)
+      @facets = {}
     end
   end
 
-  def check_search_whole_site
-    if params[:search_whole_site_yes] or params[:search_whole_site] == 'yes'
-      redirect_to params.merge(:category_path => [], :search_whole_site => nil, :search_whole_site_yes => nil)
+  alias :contents :articles 
+
+  def people
+    @asset = :people
+    @query = params[:query] || ''
+    @order ||= [@asset]
+    @results ||= {}
+    @filter = filter
+    @title = self.filter_description(params[:action] + '_' + @filter )
+
+    @results[@asset] = @environment.people.visible.send(@filter)
+    if !@query.blank?
+      ret = @results[@asset].find_by_contents(@query, {}, solr_options(@asset, params[:facet], params[:order_by]))
+      @results[@asset] = ret[:results]
+      @facets = ret[:facets]
+    else
+      @facets = {}
+    end
+    @results[@asset] = @results[@asset].compact.paginate(:per_page => limit, :page => params[:page])
+  end
+
+  def products
+    @asset = :products
+    @query = params[:query] || ''
+    @order ||= [@asset]
+    @results ||= {}
+
+    pg_options = paginate_options(@asset, limit, params[:per_page])
+    if !@query.blank?
+      ret = asset_class(@asset).find_by_contents(@query, pg_options, solr_options(@asset, params[:facet], params[:order_by]))
+      @results[@asset] = ret[:results]
+      @facets = ret[:facets]
+    else
+      @results[@asset] = asset_class(@asset).send('paginate', :all, pg_options)
+      @facets = {}
     end
   end
 
-  def check_valid_assets
-    @asset = params[:asset].to_sym
-    if !where_to_search.map(&:first).include?(@asset)
-      render :text => 'go away', :status => 403
-      return
+  def enterprises
+    @asset = :enterprises
+    @query = params[:query] || ''
+    @order ||= [@asset]
+    @results ||= {}
+
+    pg_options = paginate_options(@asset, limit, params[:per_page])
+    if !@query.blank?
+      ret = asset_class(@asset).find_by_contents(@query, pg_options, solr_options(@asset, params[:facet], params[:order_by]))
+      @results[@asset] = ret[:results]
+      @facets = ret[:facets]
+    else
+      @results[@asset] = asset_class(@asset).send('paginate', :all, pg_options)
+      @facets = {}
     end
+  end
+
+  def communities
+    @asset = :communities
+    @query = params[:query] || ''
+    @order ||= [@asset]
+    @results ||= {}
+    @filter = filter
+    @title = self.filter_description(params[:action] + '_' + @filter )
+
+    @results[@asset] = @environment.communities.visible.send(@filter)
+    if !@query.blank?
+      ret = @results[@asset].find_by_contents(@query, {}, solr_options(@asset, params[:facet], params[:order_by]))
+      @results[@asset] = ret[:results]
+      @facets = ret[:facets]
+    else
+      @facets = {}
+    end
+    @results[@asset] = @results[@asset].compact.paginate(:per_page => limit, :page => params[:page])
   end
 
   def events
+    @asset = :events
+    params[:asset] |= [@asset]
+    @query = params[:query] || ''
+    @order ||= [@asset]
+    @results ||= {}
     @category_id = @category ? @category.id : nil
+
+    if params[:year] || params[:month]
+      date = Date.new(year.to_i, month.to_i, 1)
+      date_range = (date - 1.month)..(date + 1.month).at_end_of_month
+    end
+
+    if @query.blank?
+      # Ignore pagination for asset events
+      if date_range
+        @results[@asset] = Event.send('find', :all, 
+          :conditions => [
+            'start_date BETWEEN :start_day AND :end_day OR end_date BETWEEN :start_day AND :end_day',
+            {:start_day => date_range.first, :end_day => date_range.last}
+        ])
+      else
+        @results[@asset] = Event.send('find', :all)
+      end
+    else
+      pg_options = paginate_options(@asset, limit, params[:per_page])
+      solr_options = solr_options(@asset, params[:facet], params[:per_page])
+      @results[@asset] = Event.find_by_contents(@query, pg_options, solr_options)[:results]
+    end
 
     @selected_day = nil
     @events_of_the_day = []
@@ -58,80 +144,91 @@ class SearchController < PublicController
       end
     end
 
-    events = @results[:events]
-
+    events = @results[@asset]
     @calendar = populate_calendar(date, events)
     @previous_calendar = populate_calendar(date - 1.month, events)
     @next_calendar = populate_calendar(date + 1.month, events)
   end
 
-  def people
-    #nothing, just to enable
-  end
-  def enterprises
-    load_product_categories_menu(:enterprises)
-    @categories_menu = true
-  end
-  def communities
-    #nothing, just to enable
-  end
-  def articles
-    #nothins, just to enable
-  end
+  def index
+    @results = {}
+    @order = []
+    @names = {}
 
-  def products
-    load_product_categories_menu(:products)
-    @categories_menu = true
-  end
-
-  def load_product_categories_menu(asset)
-    @results[asset].uniq!
-    # REFACTOR DUPLICATED CODE inner loop doing the same thing that outter loop
-
-    if !@query.blank? || @region && !params[:radius].blank?
-      ret = @noosfero_finder.find(asset, @query, calculate_find_options(asset, nil, params[:page], @product_category, @region, params[:radius], params[:year], params[:month], params[:order_by]).merge({:limit => :all}))
-      @result_ids = ret.is_a?(Hash) ? ret[:results] : ret
+    where_to_search.select { |key,description| @searching[key]  }.each do |key, description|
+      send(key)
+      @order << key
+      @names[key] = getterm(description)
     end
+    @asset = nil
+    @facets = {}
 
-  end
-
-  def calculate_find_options(asset, limit, page, product_category, region, radius, year, month, solr_order)
-    result = { :product_category => product_category, :per_page => limit, :page => page }
-    if [:enterprises, :people, :products].include?(asset) && region
-      result.merge!(:within => radius, :region => region.id)
-    end
-
-    if month || year
-      date = Date.new(year.to_i, month.to_i, 1)
-      result[:date_range] = (date - 1.month)..(date + 1.month).at_end_of_month
-    end
-
-    if asset_class(asset).methods.include?('facets')
-      result.merge!(:facets => {:zeros => false, :sort => :count, :fields => asset_class(asset).facets.keys,
-                    :browse => params[:facet] ? params[:facet].map{ |k,v| k.to_s+':"'+v.to_s+'"'} : ''})
-    end
-
-    result[:order_by] = solr_order
-
-    result
-  end
-
-  # limit the number of results per page
-  # TODO: dont hardcore like this
-  def limit
-    searching = @searching.values.select{|v|v}
-    if params[:display] == 'map'
-      2000
-    else
-      (searching.size == 1) ? 20 : 6
+    if @results.keys.size == 1
+      specific_action = @results.keys.first
+      if respond_to?(specific_action)
+        @asset_name = getterm(@names[@results.keys.first])
+        send(specific_action)
+        render :action => specific_action
+        return
+      end
     end
   end
 
-  public
+  alias :assets :index
 
-  include SearchHelper
+  # view the summary of one category
+  def category_index
+    @results = {}
+    @order = []
+    @names = {}
+    [
+      [ :people, _('People'), recent('people') ],
+      [ :enterprises, __('Enterprises'), recent('enterprises') ],
+      [ :products, _('Products'), recent('products') ],
+      [ :events, _('Upcoming events'), upcoming_events({:per_page => limit}) ],
+      [ :communities, __('Communities'), recent('communities', limit) ],
+      [ :most_commented_articles, _('Most commented articles'), most_commented_articles(limit) ],
+      [ :articles, _('Articles'), recent('text_articles', limit) ]
+    ].each do |key, name, list|
+      @order << key
+      @results[key] = list
+      @names[key] = name
+    end
+    @facets = {}
+  end
 
-  ######################################################
+  protected
+
+  def recent(asset, limit = nil)
+    find(asset, nil, :limit => limit, :order => 'created_at DESC, id DESC')
+  end
+
+  def most_commented_articles(limit=10, options={})
+    options = {:page => 1, :per_page => limit, :order => 'comments_count DESC'}.merge(options)
+    Article.paginate(:all, options_for_find(Article, options))
+  end
+
+  def upcoming_events(options = {})
+    options.delete(:page)
+    options.delete(:per_page)
+
+    Event.find(:all, {:include => :categories, :conditions => [ 'categories.id = ? and start_date >= ?', category_id, Date.today ], :order => 'start_date' }.merge(options))
+  end
+
+
+  attr_reader :category
+  attr_reader :category_id
+
+  def load_category
+    unless params[:category_path].blank?
+      path = params[:category_path].join('/')
+      @category = environment.categories.find_by_path(path)
+      if @category.nil?
+        render_not_found(path)
+      end
+      @category_id = @category.id
+    end
+  end
 
   def where_to_search
     [
@@ -144,77 +241,7 @@ class SearchController < PublicController
     ].select {|key, name| !environment.enabled?('disable_asset_' + key.to_s) }
   end
 
-  def cities
-    @cities = City.find(:all, :order => 'name', :conditions => ['parent_id = ? and lat is not null and lng is not null', params[:state_id]])
-    render :action => 'cities', :layout => false
-  end
-  
-  def complete_region
-    # FIXME this logic should be in the model
-    @regions = Region.find(:all, :conditions => [ '(name like ? or name like ?) and lat is not null and lng is not null', '%' + params[:region][:name] + '%', '%' + params[:region][:name].capitalize + '%' ])
-    render :action => 'complete_region', :layout => false
-  end
-
-  def index
-    @query = params[:query] || ''
-    @product_category = ProductCategory.find(params[:product_category]) if params[:product_category]
-
-    @region = City.find_by_id(params[:city]) if !params[:city].blank? && params[:city] =~ /^\d+$/
-
-    # how many assets we are searching for?
-    number_of_result_assets = @searching.values.select{|v| v}.size
-
-    @results = {}
-    @facets = {}
-    @order = []
-    @names = {}
-
-    where_to_search.select { |key,description| @searching[key]  }.each do |key, description|
-      @order << key
-      find_options = calculate_find_options(key, limit, params[:page], @product_category, @region, params[:radius], params[:year], params[:month], params[:order_by]);
-      ret = @noosfero_finder.find(key, @query, find_options)
-      @results[key] = ret.is_a?(Hash) ? ret[:results] : ret
-      @facets[key] = ret.is_a?(Hash) ? ret[:facets] : {}
-      @names[key] = getterm(description)
-    end
-
-    if @results.keys.size == 1
-      specific_action = @results.keys.first
-      if respond_to?(specific_action)
-        @asset_name = getterm(@names[@results.keys.first])
-        send(specific_action)
-        render :action => specific_action
-        return
-      end
-    end
-
-    render :action => 'index'
-  end
-
-  alias :assets :index
-
   #######################################################
-
-  # view the summary of one category
-  def category_index
-    @results = {}
-    @order = []
-    @names = {}
-    [
-      [ :people, _('People'), @noosfero_finder.recent('people', limit) ],
-      [ :enterprises, __('Enterprises'), @noosfero_finder.recent('enterprises', limit) ],
-      [ :products, _('Products'), @noosfero_finder.recent('products', limit) ],
-      [ :events, _('Upcoming events'), @noosfero_finder.upcoming_events({:per_page => limit}) ],
-      [ :communities, __('Communities'), @noosfero_finder.recent('communities', limit) ],
-      [ :most_commented_articles, _('Most commented articles'), @noosfero_finder.most_commented_articles(limit) ],
-      [ :articles, _('Articles'), @noosfero_finder.recent('text_articles', limit) ]
-    ].each do |key, name, list|
-      @order << key
-      @results[key] = list
-      @names[key] = name
-    end
-  end
-  attr_reader :category
 
   def tags
     @tags_cache_key = "tags_env_#{environment.id.to_s}"
@@ -246,6 +273,92 @@ class SearchController < PublicController
       @events_of_the_day = environment.events.by_day(@selected_day)
     end
     render :partial => 'events/events_by_day'
+  end
+
+  def current_events(year, month, options={})
+    options.delete(:page)
+    options.delete(:per_page)
+
+    range = Event.date_range(year, month)
+
+    Event.find(:all, {:include => :categories, :conditions => { 'categories.id' => category_id, :start_date => range }}.merge(options))
+  end
+
+  def filter
+    if FILTERS.include?(params[:filter])
+      params[:filter]
+    else
+      'more_recent'
+    end
+  end
+
+  def filter_description(str)
+    {
+      'contents_more_recent' => _('More recent contents'),
+      'contents_more_popular' => _('More popular contents'),
+      'people_more_recent' => _('More recent people'),
+      'people_more_active' => _('More active people'),
+      'people_more_popular' => _('More popular people'),
+      'communities_more_recent' => _('More recent communities'),  
+      'communities_more_active' => _('More active communities'),  
+      'communities_more_popular' => _('More popular communities'),
+    }[str] || str
+  end
+
+  def load_search_assets
+    @search_in = where_to_search
+    @searching = {}
+    @search_in.each do |key, name|
+      @searching[key] = (params[:asset].blank? && (params[:find_in].nil? || params[:find_in].empty? || params[:find_in].include?(key.to_s))) || (params[:asset] == key.to_s)
+    end
+  end
+
+  def check_search_whole_site
+    if params[:search_whole_site_yes] or params[:search_whole_site] == 'yes'
+      redirect_to params.merge(:category_path => [], :search_whole_site => nil, :search_whole_site_yes => nil)
+    end
+  end
+
+  FILTERS = %w(
+    more_recent
+    more_active
+    more_popular
+  )
+
+  def paginate_options(asset, limit, page)
+    result = { :per_page => limit, :page => page }
+  end
+
+  def solr_options(asset, facet, solr_order)
+    result = {}
+
+    if asset_class(asset).methods.include?('facets')
+      result.merge!(:facets => {:zeros => false, :sort => :count, :fields => asset_class(asset).facets.keys,
+                    :browse => facet ? facet.map{ |k,v| k.to_s+':"'+v.to_s+'"'} : ''})
+    end
+
+    if solr_order
+      result[:order_by] = solr_order
+    end
+
+    result
+  end
+
+  def limit
+    searching = @searching.values.select{|v|v}
+    if params[:display] == 'map'
+      2000
+    else
+      (searching.size == 1) ? 20 : 6
+    end
+  end
+
+  def asset_class(asset)
+    asset.to_s.singularize.camelize.constantize
+  end
+  
+  def asset_table(asset)
+    asset_class(asset).table_name
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -572,7 +572,7 @@ module ApplicationHelper
     end
     extra_info = extra_info.nil? ? '' : content_tag( 'span', extra_info, :class => 'extra_info' )
     links = links_for_balloon(profile)
-    content_tag tag,
+    content_tag('div', content_tag(tag,
         (environment.enabled?(:show_balloon_with_profile_links_when_clicked) ? link_to( content_tag( 'span', _('Profile links')), '#', :onclick => "toggleSubmenu(this, '#{profile.short_name}', #{links.to_json}); return false", :class => "menu-submenu-trigger #{trigger_class}", :url => url) : "") +
         link_to(
             content_tag( 'span', profile_image( profile, size ), :class => 'profile-image' ) +
@@ -582,7 +582,7 @@ module ApplicationHelper
             :class => 'profile_link url',
             :help => _('Click on this icon to go to the <b>%s</b>\'s home page') % profile.name,
             :title => profile.name ),
-        :class => 'vcard'
+        :class => 'vcard'), :class => 'common-profile-list-block')
   end
 
   def gravatar_url_for(email, options = {})
@@ -887,9 +887,6 @@ module ApplicationHelper
   def search_page_title(title, options={})
     title = "<h1>" + title + "</h1>"
     title += "<h2 class='query'>" + _("Searched for '%s'") % options[:query] + "</h2>" if !options[:query].blank?
-    title += "<h2 class='query'>" + _("In category %s") % options[:category] + "</h2>" if !options[:category].blank?
-    title += "<h2 class='query'>" + _("within %d km from %s") % [options[:distance], options[:region]] + "</h2>" if !options[:distance].blank? && !options[:region].blank?
-    title += "<h2 class='query'>" + _("%d results found") % options[:total_results] + "</h2>" if !options[:total_results].blank?
     title
   end
 
@@ -1105,33 +1102,46 @@ module ApplicationHelper
     ") : '')
   end
 
-  def browse_people_menu
+  def search_contents_menu
+    links = [
+      {s_('contents|More Recent') => {:href => url_for({:controller => 'search', :action => 'contents', :filter => 'more_recent'})}},
+      #{s_('contents|More Popular') => {:href => url_for({:controller => 'search', :action => 'contents', :filter => 'more_popular'})}}
+    ]
+    if logged_in?
+      links.push(_('New Content') => lightbox_options({:href => url_for({:controller => 'cms', :action => 'new', :profile => current_user.login, :cms => true})}))
+    end
+
+    link_to(content_tag(:span, _('Contents'), :class => 'icon-menu-contents'), {:controller => "search", :action => 'contents'}, :id => 'submenu-contents') +
+    link_to(content_tag(:span, _('Contents Menu')), '#', :onclick => "toggleSubmenu(this,'',#{links.to_json}); return false", :class => 'menu-submenu-trigger up', :id => 'submenu-contents-trigger')
+  end
+
+  def search_people_menu
      links = [
-       {s_('people|More Recent') => {:href => url_for({:controller => 'browse', :action => 'people', :filter => 'more_recent'})}},
-       {s_('people|More Active') => {:href => url_for({:controller => 'browse', :action => 'people', :filter => 'more_active'})}},
-       {s_('people|More Popular') => {:href => url_for({:controller => 'browse', :action => 'people', :filter => 'more_popular'})}}
+       {s_('people|More Recent') => {:href => url_for({:controller => 'search', :action => 'people', :filter => 'more_recent'})}},
+       {s_('people|More Active') => {:href => url_for({:controller => 'search', :action => 'people', :filter => 'more_active'})}},
+       {s_('people|More Popular') => {:href => url_for({:controller => 'search', :action => 'people', :filter => 'more_popular'})}}
      ]
      if logged_in?
        links.push(_('My friends') => {:href => url_for({:profile => current_user.login, :controller => 'friends'})})
        links.push(_('Invite friends') => {:href => url_for({:profile => current_user.login, :controller => 'invite', :action => 'friends'})})
      end
 
-    link_to(content_tag(:span, _('People'), :class => 'icon-menu-people'), {:controller => "browse", :action => 'people'}, :id => 'submenu-people') +
+    link_to(content_tag(:span, _('People'), :class => 'icon-menu-people'), {:controller => "search", :action => 'people'}, :id => 'submenu-people') +
     link_to(content_tag(:span, _('People Menu')), '#', :onclick => "toggleSubmenu(this,'',#{links.to_json}); return false", :class => 'menu-submenu-trigger up', :id => 'submenu-people-trigger')
   end
 
-  def browse_communities_menu
+  def search_communities_menu
      links = [
-       {s_('communities|More Recent') => {:href => url_for({:controller => 'browse', :action => 'communities', :filter => 'more_recent'})}},
-       {s_('communities|More Active') => {:href => url_for({:controller => 'browse', :action => 'communities', :filter => 'more_active'})}},
-       {s_('communities|More Popular') => {:href => url_for({:controller => 'browse', :action => 'communities', :filter => 'more_popular'})}}
+       {s_('communities|More Recent') => {:href => url_for({:controller => 'search', :action => 'communities', :filter => 'more_recent'})}},
+       {s_('communities|More Active') => {:href => url_for({:controller => 'search', :action => 'communities', :filter => 'more_active'})}},
+       {s_('communities|More Popular') => {:href => url_for({:controller => 'search', :action => 'communities', :filter => 'more_popular'})}}
      ]
      if logged_in?
        links.push(_('My communities') => {:href => url_for({:profile => current_user.login, :controller => 'memberships'})})
        links.push(_('New community') => {:href => url_for({:profile => current_user.login, :controller => 'memberships', :action => 'new_community'})})
      end
 
-    link_to(content_tag(:span, _('Communities'), :class => 'icon-menu-community'), {:controller => "browse", :action => 'communities'}, :id => 'submenu-communities') +
+    link_to(content_tag(:span, _('Communities'), :class => 'icon-menu-community'), {:controller => "search", :action => 'communities'}, :id => 'submenu-communities') +
     link_to(content_tag(:span, _('Communities Menu')), '#', :onclick => "toggleSubmenu(this,'',#{links.to_json}); return false", :class => 'menu-submenu-trigger up', :id => 'submenu-communities-trigger')
   end
 
