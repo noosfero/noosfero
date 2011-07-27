6bebebcbc335084cf7cbd841219cafd57375a810
Braulio Bhavamitra <brauliobo@gmail.com>
2011-07-27 19:10:29 +0000

    Fix functional tests for Solr port

--- app/controllers/public/profile_search_controller.rb
+++ app/controllers/public/profile_search_controller.rb
@@ -11,7 +11,7 @@ class ProfileSearchController < PublicController
       if params[:where] == 'environment'
         redirect_to :controller => 'search', :query => @q
       else
-        @results = profile.articles.published.find_by_contents(@q)[:results].paginate(:per_page => 10, :page => params[:page])
+        @results = Article.find_by_contents(@q + "  profile_id:#{profile.id} published:true")[:results].paginate(:per_page => 10, :page => params[:page])
       end
     end
   end
