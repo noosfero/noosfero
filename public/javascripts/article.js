aba449f03a56ef43208e2cc04e83d9b748fe9ded
Antonio Terceiro <terceiro@colivre.coop.br>
2011-08-09 17:44:35 -0700

    Use <div> instead of <li> for media items
    When you drag and drop, Firefox brings the outer element together with
    the link or image. A <div> is less weird than a <li>, though.

--- public/javascripts/article.js
+++ public/javascripts/article.js
@@ -27,13 +27,13 @@ jQuery(function($) {
     var html_for_items = '';
     $.each(items, function(i, item) {
       if (item.error) {
-        html_for_items += '<li class="media-upload-error">' + item.error + '</li>';
+        html_for_items += '<div class="media-upload-error">' + item.error + '</div>';
         return;
       }
       if (item.content_type && item.content_type.match(/^image/)) {
-        html_for_items += '<li class="icon-photos"><img src="' + item.url + '"/><br/><a href="' + item.url + '">' + item.title + '</a></li>';
+        html_for_items += '<div class="icon-photos"><img src="' + item.url + '"/><br/><a href="' + item.url + '">' + item.title + '</a></div>';
       } else {
-        html_for_items += '<li class="' + item.icon + '"><a href="' + item.url + '">' + item.title + '</a></li>';
+        html_for_items += '<div class="' + item.icon + '"><a href="' + item.url + '">' + item.title + '</a></div>';
       }
     });
     $(selector).html(html_for_items);
@@ -43,7 +43,7 @@ jQuery(function($) {
     var $button = $(this);
     $('#media-search-box').toggleClass('icon-loading');
     $.get($(this).parent().attr('action'), { 'q': query }, function(data) {
-      insert_items(data, '#media-search-results ul');
+      insert_items(data, '#media-search-results .items');
       if (data.length && data.length > 0) {
         $('#media-search-results').slideDown();
       }
@@ -61,7 +61,7 @@ jQuery(function($) {
       },
     success:
       function(data) {
-        insert_items(data, '#media-upload-results ul');
+        insert_items(data, '#media-upload-results .items');
         if (data.length && data.length > 0) {
           $('#media-upload-results').slideDown();
         }
