2220fe167d588fa1ca900dcef497a875c22b989c
Daniela Soares Feitosa <danielafeitosa@colivre.coop.br>
2011-08-11 18:28:34 -0300

    Fixing method with new solr search
    (ActionItem1958)

--- app/controllers/my_profile/cms_controller.rb
+++ app/controllers/my_profile/cms_controller.rb
@@ -291,7 +291,7 @@ class CmsController < MyProfileController
 
   def search
     query = params[:q]
-    results = query.blank? ? [] : profile.articles.published.find_by_contents(query)
+    results = query.blank? ? [] : profile.articles.published.find_by_contents(query)[:results]
     render :text => article_list_to_json(results), :content_type => 'application/json'
   end
   def media_upload
