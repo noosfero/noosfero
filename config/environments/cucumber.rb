a56214cff8c7dd6f24a3fab030fabc4ece707ca1
Daniela Soares Feitosa <danielafeitosa@colivre.coop.br>
2011-08-16 18:51:39 -0300

    Revert "Fix cucumber installation and tests"
    This reverts commit ba869eb36522034b60d819fb4d83bd0b0a751033.
    
    Conflicts:
    
    	config/solr.yml.dist
    	lib/tasks/cucumber.rake
    
    There are some problems with solr
    (ActionItem1958)

--- config/environments/cucumber.rb
+++ config/environments/cucumber.rb
@@ -21,9 +21,9 @@ config.action_mailer.delivery_method = :test
 
 config.gem 'cucumber',    :lib => false,        :version => '0.4.0' unless File.directory?(File.join(Rails.root, 'vendor/plugins/cucumber'))
 config.gem 'webrat',      :lib => false,        :version => '0.5.1' unless File.directory?(File.join(Rails.root, 'vendor/plugins/webrat'))
-config.gem 'rspec',       :lib => 'spec',        :version => '=1.2.8' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec'))
-config.gem 'rspec-rails', :lib => 'spec/rails',        :version => '1.2.7.1' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
-config.gem 'Selenium',    :lib => 'selenium',      :version => '1.1.14' unless File.directory?(File.join(Rails.root, 'vendor/plugins/selenium'))
-config.gem 'selenium-client',    :lib => 'selenium/client',      :version => '1.2.17' unless File.directory?(File.join(Rails.root, 'vendor/plugins/selenium-client'))
+config.gem 'rspec',       :lib => 'spec',        :version => '>=1.2.8' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec'))
+config.gem 'rspec-rails', :lib => 'spec/rails',        :version => '>=1.2.7.1' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
+config.gem 'Selenium',    :lib => 'selenium',      :version => '>= 1.1.14' unless File.directory?(File.join(Rails.root, 'vendor/plugins/selenium'))
+config.gem 'selenium-client',    :lib => 'selenium/client',      :version => '>= 1.2.17' unless File.directory?(File.join(Rails.root, 'vendor/plugins/selenium-client'))
 config.gem 'database_cleaner', :lib => 'database_cleaner'
 
