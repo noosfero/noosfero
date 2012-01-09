09b24df6e048845be5ba44ad4ee1bff1ecd9daf1
Carlos Morais + Paulo Meirelles <carlos88morais@gmail.com>
2012-01-09 14:38:07 -0200

    Showing source tree

--- plugins/mezuro/lib/kalibro/entities/module_node.rb
+++ plugins/mezuro/lib/kalibro/entities/module_node.rb
@@ -18,4 +18,29 @@ class Kalibro::Entities::ModuleNode < Kalibro::Entities::Entity
     @child = children
   end
 
+  def print
+    cell = "<td>#{@module.name} (#{@module.granularity})</td>"
+    return "<table><tr><td width=\"1\"></td>" + cell + "</tr></table>" if @child.nil?
+
+    id = @module.name
+    "<table>" +
+      "<tr>" +
+        "<td width=\"1\">" +
+          "<img id=\"#{id}_plus\" onclick=\"toogle('#{id}')\"" +
+            "alt=\"+\" src=\"/plugins/mezuro/images/plus.png\" class=\"link\"/>" +
+          "<img id=\"#{id}_minus\" onclick=\"toogle('#{id}')\"" +
+            "alt=\"-\" src=\"/plugins/mezuro/images/minus.png\" class=\"link\"" +
+            "style=\"display: none\"/>" +
+        "</td>" +
+        cell +
+      "</tr>" +
+      "<tr id=\"#{id}_hidden\" style=\"display: none\">" +
+        "<td></td>" +
+        "<td style=\"text-align: left\">" +
+          @child.collect { |child| child.print }.to_s +
+        "</td>" +
+      "</tr>" +
+    "</table>"
+  end
+
 end
