fe245f2b7bc6c509f10ec98064f30e167882c5de
Antonio Terceiro + Carlos Morais <terceiro@colivre.coop.br>
2012-01-27 15:50:00 +0300

    partial for source tree

--- plugins/mezuro/lib/kalibro/entities/module_node.rb
+++ plugins/mezuro/lib/kalibro/entities/module_node.rb
@@ -6,6 +6,14 @@ class Kalibro::Entities::ModuleNode < Kalibro::Entities::Entity
     @module = to_entity(value, Kalibro::Entities::Module)
   end
 
+  def module_name
+    @module.name
+  end
+
+  def granularity
+    @module.granularity
+  end
+
   def child=(value)
     @child = to_entity_array(value, Kalibro::Entities::ModuleNode)
   end
@@ -16,32 +24,6 @@ class Kalibro::Entities::ModuleNode < Kalibro::Entities::Entity
 
   def children=(children)
     @child = children
-  end
-
-  def print
-    cell = "<td>#{@module.name} (#{@module.granularity})</td>"
-    return "<table><tr><td width=\"1\"></td>" + cell + "</tr></table>" if @child.nil?
-
-    id = @module.name
-    "<table>" +
-      "<tr>" +
-        "<td width=\"10%\">" +
-          "<img id=\"#{id}_plus\" onclick=\"toogle('#{id}')\"" +
-            "alt=\"+\" src=\"/plugins/mezuro/images/plus.png\" class=\"link\"" +
-            "style=\"display: none\"/>" +
-          "<img id=\"#{id}_minus\" onclick=\"toogle('#{id}')\"" +
-            "alt=\"-\" src=\"/plugins/mezuro/images/minus.png\" class=\"link\"" +
-            "/>" +
-        "</td>" +
-        cell +
-      "</tr>" +
-      "<tr id=\"#{id}_hidden\">" +
-        "<td></td>" +
-        "<td style=\"text-align: left\">" +
-          @child.collect { |child| child.print }.to_s +
-        "</td>" +
-      "</tr>" +
-    "</table>"
   end
 
 end
