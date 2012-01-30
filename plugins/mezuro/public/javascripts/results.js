ebc7e44ad5597ef30f2312457a94ba125bf353c2
João M. M. da Silva + Carlos Morais <jaodsilv@linux.ime.usp.br>
2012-01-30 12:01:54 -0200

    fix profile on ajax results

--- plugins/mezuro/public/javascripts/results.js
+++ plugins/mezuro/public/javascripts/results.js
@@ -1,6 +1,6 @@
 function results($) {
   $('.mezuro-display-metrics').click(function() {
-    var profile = 'qt-calculator'; // FIXME
+    var profile = $('#module-result').attr('data-profile');
     var project = $('#module-result').attr('data-project-id');
     var module_name = $(this).attr('data-module-name');
     var endpoint = '/profile/' + profile + '/plugins/mezuro/metrics/' + project;
