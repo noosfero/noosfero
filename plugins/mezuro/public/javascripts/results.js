c0610875f20a116155853171961635487fe95aea
João M. M. da Silva + Carlos Morais <jaodsilv@linux.ime.usp.br>
2012-01-30 11:41:29 -0200

    show loading message for ajax results

--- plugins/mezuro/public/javascripts/results.js
+++ plugins/mezuro/public/javascripts/results.js
@@ -4,11 +4,19 @@ function results($) {
     var project = $('#module-result').attr('data-project-id');
     var module_name = $(this).attr('data-module-name');
     var endpoint = '/profile/' + profile + '/plugins/mezuro/metrics/' + project;
-    // FIXME turn on the 'loading ...'
-    $.get(endpoint, { module_name: module_name }, function(content) {
+    show_loading_message();
+    $.get(endpoint, {module_name: module_name}, function(content) {
       $('#module-result').html(content);
-      // FIXME turn off the 'loading ...'
+      show_result_table();
     });
     return false;
   });
-};
+}
+function show_loading_message() {
+  jQuery('#loading-message').attr("style", "display: inline");
+  jQuery('#module-result').attr("style", "display: none");
+}
+function show_result_table(){
+  jQuery('#loading-message').attr("style", "display: none");
+  jQuery('#module-result').attr("style", "display: inline");
+}
