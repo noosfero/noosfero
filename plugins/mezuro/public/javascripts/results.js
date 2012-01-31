985de89523a098111fa6cfbd9affd340d784f7a3
Carlos Morais + Paulo Meirelles <carlos88morais@gmail.com>
2012-01-31 11:28:14 -0200

    Refactoring javascript and html for ajax-results

--- plugins/mezuro/public/javascripts/results.js
+++ plugins/mezuro/public/javascripts/results.js
@@ -1,22 +1,21 @@
 function results($) {
-  $('.mezuro-display-metrics').click(function() {
-    var profile = $('#module-result').attr('data-profile');
-    var project = $('#module-result').attr('data-project-id');
-    var module_name = $(this).attr('data-module-name');
-    var endpoint = '/profile/' + profile + '/plugins/mezuro/metrics/' + project;
-    show_loading_message();
-    $.get(endpoint, {module_name: module_name}, function(content) {
-      $('#module-result').html(content);
-      show_result_table();
-    });
-    return false;
-  });
+  $('.module-result-link').click(show_module_result);
 }
-function show_loading_message() {
-  jQuery('#loading-message').attr("style", "display: inline");
-  jQuery('#module-result').attr("style", "display: none");
+
+function show_module_result(){
+  var profile = jQuery('#module-result').attr('data-profile');
+  var project = jQuery('#module-result').attr('data-project-id');
+  var module_name = jQuery(this).attr('data-module-name');
+  var endpoint = '/profile/' + profile + '/plugins/mezuro/metrics/' + project;
+  show_loading_message(module_name);
+  jQuery.get(endpoint, {module_name: module_name}, show_result_table);
+  return false;
 }
-function show_result_table(){
-  jQuery('#loading-message').attr("style", "display: none");
-  jQuery('#module-result').attr("style", "display: inline");
+
+function show_loading_message(module_name) {
+  jQuery('#module-result').html("Loading results for " + module_name + "...");
+}
+
+function show_result_table(content){
+  jQuery('#module-result').html(content);
 }
