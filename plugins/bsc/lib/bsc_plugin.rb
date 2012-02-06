21883480f32f7bbeaf728da1d3a3b1aa6e6424a9
Rodrigo Souto <rodrigo@colivre.coop.br>
2012-02-06 21:17:16 -0200

    Including file path in the plugins extensions requirements
    (ActionItem2271)

--- plugins/bsc/lib/bsc_plugin.rb
+++ plugins/bsc/lib/bsc_plugin.rb
@@ -1,5 +1,5 @@
-require_dependency 'ext/enterprise'
-require_dependency 'ext/product'
+require_dependency File.dirname(__FILE__) + 'ext/enterprise'
+require_dependency File.dirname(__FILE__) + 'ext/product'
 
 class BscPlugin < Noosfero::Plugin
 
