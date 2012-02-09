acc5122c81b5ca8ac57794a309d0fad57e8d4a73
Carlos Morais <carlos88morais@gmail.com>
2012-02-09 20:33:39 -0200

    [Mezuro] ModuleNodeFixtures

--- plugins/mezuro/test/unit/kalibro/entities/module_node_test.rb
+++ plugins/mezuro/test/unit/kalibro/entities/module_node_test.rb
@@ -1,35 +1,12 @@
 require "test_helper"
 
+require "#{RAILS_ROOT}/plugins/mezuro/test/fixtures/module_node_fixtures"
+
 class ModuleNodeTest < ActiveSupport::TestCase
 
-  def self.qt_calculator_tree
-    node = Kalibro::Entities::ModuleNode.new
-    node.module = ModuleTest.qt_calculator
-    node.children = [new_node('Dialog', 'CLASS'), new_node('main', 'CLASS')]
-    node
-  end
-
-  def self.new_node(name, granularity)
-    the_module = Kalibro::Entities::Module.new
-    the_module.name = name
-    the_module.granularity = granularity
-    node = Kalibro::Entities::ModuleNode.new
-    node.module = the_module
-    node
-  end
-
-  def self.qt_calculator_tree_hash
-    {:module => ModuleTest.qt_calculator_hash,
-      :child => [
-        {:module => {:name => 'Dialog', :granularity => 'CLASS'}},
-        {:module => {:name => 'main', :granularity => 'CLASS'}}
-      ]
-    }
-  end
-
   def setup
-    @hash = self.class.qt_calculator_tree_hash
-    @node = self.class.qt_calculator_tree
+    @hash = ModuleNodeFixtures.qt_calculator_tree_hash
+    @node = ModuleNodeFixtures.qt_calculator_tree
   end
 
   should 'create module node from hash' do
