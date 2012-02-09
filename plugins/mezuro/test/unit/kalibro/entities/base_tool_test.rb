166c9b0725586b4c3ecfe4a71b67b36849b34a32
Carlos Morais <carlos88morais@gmail.com>
2012-02-09 18:29:46 -0200

    [Mezuro] BaseToolFixtures

--- plugins/mezuro/test/unit/kalibro/entities/base_tool_test.rb
+++ plugins/mezuro/test/unit/kalibro/entities/base_tool_test.rb
@@ -1,25 +1,12 @@
 require "test_helper"
+
+require "#{RAILS_ROOT}/plugins/mezuro/test/fixtures/base_tool_fixtures"
+
 class BaseToolTest < ActiveSupport::TestCase
 
-  def self.analizo
-    total_cof = NativeMetricTest.total_cof
-    amloc = NativeMetricTest.amloc
-    base_tool = Kalibro::Entities::BaseTool.new
-    base_tool.name = 'Analizo'
-    base_tool.supported_metrics = [total_cof, amloc]
-    base_tool
-  end
-
-  def self.analizo_hash
-    total_cof_hash = NativeMetricTest.total_cof_hash
-    amloc_hash = NativeMetricTest.amloc_hash
-    {:name => 'Analizo',
-     :supported_metric => [total_cof_hash, amloc_hash]}
-  end
-
   def setup
-    @hash = self.class.analizo_hash
-    @base_tool = self.class.analizo
+    @hash = BaseToolFixtures.analizo_hash
+    @base_tool = BaseToolFixtures.analizo
   end
 
   should 'create base tool from hash' do
