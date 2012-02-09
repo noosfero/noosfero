30519be044fe841b5029a0b3b985d8c44a425fef
Carlos Morais <carlos88morais@gmail.com>
2012-02-09 20:42:14 -0200

    [Mezuro] RepositoryFixtures

---
+++ plugins/mezuro/test/fixtures/repository_fixtures.rb
@@ -0,0 +1,14 @@
+class RepositoryFixtures
+
+  def self.qt_calculator
+    repository = Kalibro::Entities::Repository.new
+    repository.type = 'SUBVERSION'
+    repository.address = 'https://qt-calculator.svn.sourceforge.net/svnroot/qt-calculator'
+    repository
+  end
+
+  def self.qt_calculator_hash
+    {:type => 'SUBVERSION', :address => 'https://qt-calculator.svn.sourceforge.net/svnroot/qt-calculator'}
+  end
+
+end
