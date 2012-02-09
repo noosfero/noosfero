2cb8a343235bb42c5113fa9b023e2907319a4492
Carlos Morais <carlos88morais@gmail.com>
2012-02-09 18:29:46 -0200

    BaseToolFixtures

---
+++ plugins/mezuro/test/fixtures/base_tool_fixtures.rb
@@ -0,0 +1,18 @@
+class BaseToolFixtures
+    
+  def self.analizo
+    base_tool = Kalibro::Entities::BaseTool.new
+    base_tool.name = 'Analizo'
+    base_tool.supported_metrics = [
+      NativeMetricFixtures.total_cof,
+      NativeMetricFixtures.amloc]
+    base_tool
+  end
+
+  def self.analizo_hash
+    {:name => 'Analizo', :supported_metric => [
+        NativeMetricFixtures.total_cof_hash,
+        NativeMetricFixtures.amloc_hash]}
+  end
+
+end
