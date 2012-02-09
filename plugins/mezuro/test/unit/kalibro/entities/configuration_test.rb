1cda4bc759967cd5b26d83f2c3def7a470c35365
Carlos Morais <carlos88morais@gmail.com>
2012-02-09 20:18:04 -0200

    ConfigurationFixtures

--- plugins/mezuro/test/unit/kalibro/entities/configuration_test.rb
+++ plugins/mezuro/test/unit/kalibro/entities/configuration_test.rb
@@ -1,27 +1,12 @@
 require "test_helper"
+
+require "#{RAILS_ROOT}/plugins/mezuro/test/fixtures/configuration_fixtures"
+
 class ConfigurationTest < ActiveSupport::TestCase
 
-  def self.kalibro_configuration
-    amloc_configuration = MetricConfigurationTest.amloc_configuration
-    sc_configuration = MetricConfigurationTest.sc_configuration
-    configuration = Kalibro::Entities::Configuration.new
-    configuration.name = 'Kalibro for Java'
-    configuration.description = 'Kalibro configuration for Java projects.'
-    configuration.metric_configurations = [amloc_configuration, sc_configuration]
-    configuration
-  end
-
-  def self.kalibro_configuration_hash
-    amloc_hash = MetricConfigurationTest.amloc_configuration_hash
-    sc_hash = MetricConfigurationTest.sc_configuration_hash
-    {:name => 'Kalibro for Java',
-      :description => 'Kalibro configuration for Java projects.',
-      :metric_configuration => [amloc_hash, sc_hash]}
-  end
-
   def setup
-    @hash = self.class.kalibro_configuration_hash
-    @configuration = self.class.kalibro_configuration
+    @hash = ConfigurationFixtures.kalibro_configuration_hash
+    @configuration = ConfigurationFixtures.kalibro_configuration
   end
 
   should 'create configuration from hash' do
