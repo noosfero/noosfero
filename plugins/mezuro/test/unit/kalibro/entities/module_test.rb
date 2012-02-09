68ddc5b618fdf04c4fbec43ccd1296082254dde9
Carlos Morais <carlos88morais@gmail.com>
2012-02-09 20:27:17 -0200

    [Mezuro] ModuleFixtures

--- plugins/mezuro/test/unit/kalibro/entities/module_test.rb
+++ plugins/mezuro/test/unit/kalibro/entities/module_test.rb
@@ -1,21 +1,12 @@
 require "test_helper"
+
+require "#{RAILS_ROOT}/plugins/mezuro/test/fixtures/module_fixtures"
+
 class ModuleTest < ActiveSupport::TestCase
 
-  def self.qt_calculator
-    entity = Kalibro::Entities::Module.new
-    entity.name = ProjectTest.qt_calculator.name
-    entity.granularity = 'APPLICATION'
-    entity
-  end
-
-  def self.qt_calculator_hash
-    name = ProjectTest.qt_calculator.name
-    {:name => name, :granularity => 'APPLICATION'}
-  end
-
   def setup
-    @hash = self.class.qt_calculator_hash
-    @module = self.class.qt_calculator
+    @hash = ModuleFixtures.qt_calculator_hash
+    @module = ModuleFixtures.qt_calculator
   end
 
   should 'create module from hash' do
