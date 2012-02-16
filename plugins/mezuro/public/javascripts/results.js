de625053d25f94e86d3be2711d29d90f89799f21
Carlos Morais <carlos88morais@gmail.com>
2012-02-15 23:55:55 -0200

    [Mezuro] project_result back into his own partial

--- plugins/mezuro/public/javascripts/results.js
+++ plugins/mezuro/public/javascripts/results.js
@@ -3,8 +3,8 @@ function results($) {
 }
 
 function show_module_result(){
-  var profile = jQuery('#module-result').attr('data-profile');
-  var project = jQuery('#module-result').attr('data-content');
+  var profile = jQuery('#ids').attr('data-profile');
+  var project = jQuery('#ids').attr('data-content');
   var module_name = jQuery(this).attr('data-module-name');
   var endpoint = '/profile/' + profile + '/plugins/mezuro/module_result/' + project;
   show_loading_message(module_name);
