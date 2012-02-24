ebc5cf242b3af6f634100040e4a06f5e7cf2d8db
Rafael Martins <rmmartins@gmail.com>
2012-02-24 17:07:44 -0200

    GoogleMaps model updated to v3

--- app/models/google_maps.rb
+++ app/models/google_maps.rb
@@ -1,28 +1,7 @@
 class GoogleMaps
 
-  extend ActionView::Helpers::TagHelper
-
-  class << self
-
-    include ApplicationHelper
-
-    def enabled?(domain)
-      domain = Domain.find_by_name(domain)
-      domain ? !domain.google_maps_key.nil? : false
-    end
-
-    def key(domainname)
-      domain = Domain.find_by_name(domainname)
-      domain && domain.google_maps_key || ''
-    end
-
-    def initial_zoom
-      NOOSFERO_CONF['googlemaps_initial_zoom'] || 4
-    end
-
-    def api_url(domain)
-      "http://maps.google.com/maps?file=api&amp;v=2&amp;key=#{key(domain)}"
-    end
-
+  def self.initial_zoom
+    NOOSFERO_CONF['googlemaps_initial_zoom'] || 4
   end
+
 end
