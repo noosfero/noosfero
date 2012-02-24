bf6229859a0281d106223eec6c306c7c927acbed
Rafael Martins <rmmartins@gmail.com>
2012-02-24 17:23:47 -0200

    Integration tests moved to the new search actions

--- test/integration/assets_menu_test.rb
+++ test/integration/assets_menu_test.rb
@@ -3,8 +3,8 @@ require "#{File.dirname(__FILE__)}/../test_helper"
 class AssetsMenuTest < ActionController::IntegrationTest
 
   def setup
-    HomeController.any_instance.stubs(:get_layout).returns('application')
-    SearchController.any_instance.stubs(:get_layout).returns('application')
+#    HomeController.any_instance.stubs(:get_layout).returns('application')
+#    SearchController.any_instance.stubs(:get_layout).returns('application')
 
     parent = Category.create!(:name => "Parent Category", :environment => Environment.default, :display_color => 1)
     @category = Category.create!(:name => "Category A", :environment => Environment.default, :parent => parent)
@@ -12,17 +12,14 @@ class AssetsMenuTest < ActionController::IntegrationTest
   
   should 'link to uncategorized assets at site root' do
     get '/'
-    assert_tag :tag => 'a', :attributes => { :href => '/assets/articles' }
+    assert_tag :tag => 'a', :attributes => { :href => '/search/contents' }
   end
 
   should 'link to assets inside category root' do
+    ent = @category.enterprises.create! :identifier => 'ent1', :name => 'enterprise1'
+    
     get '/cat/parent-category/category-a'
-    assert_tag :tag => 'a', :attributes => { :href => '/assets/articles/parent-category/category-a' }
-  end
-
-  should 'link to other assets in same category when' do
-    get '/assets/articles/parent-category/category-a'
-    assert_tag :tag => 'a', :attributes => { :href => '/assets/products/parent-category/category-a' }
+    assert_tag :tag => 'a', :attributes => { :href => '/search/enterprises/parent-category/category-a' }
   end
 
 end
