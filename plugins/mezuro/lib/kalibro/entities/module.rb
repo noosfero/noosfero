a62342f3550843e6d66228e7f0e77d84588fcee0
Caio Salgado + Carlos Morais + Diego Araújo + Pedro Leal <diegoamc90@gmail.com>
2012-03-01 20:37:36 +0300

    Split module name

--- plugins/mezuro/lib/kalibro/entities/module.rb
+++ plugins/mezuro/lib/kalibro/entities/module.rb
@@ -2,4 +2,13 @@ class Kalibro::Entities::Module < Kalibro::Entities::Entity
 
   attr_accessor :name, :granularity
 
+  def ancestor_names
+    path = []
+    ancestors = []
+    @name.split(".").each do |token|
+       path << token
+       ancestors << path.join(".")
+     end
+     ancestors
+  end
 end
