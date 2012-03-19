bfce4eb13212734b675c4fd07f23ecca6946cb3e
Rodrigo Souto <rodrigo@colivre.coop.br>
2012-03-19 17:32:26 -0300

    [stoa] Fixing plugin routes

--- lib/noosfero/plugin/routes.rb
+++ lib/noosfero/plugin/routes.rb
@@ -1,6 +1,6 @@
 Dir.glob(File.join(Rails.root, 'config', 'plugins', '*', 'controllers')) do |dir|
   plugin_name = File.basename(File.dirname(dir))
-  map.connect 'plugin/' + plugin_name + '/:action/:id', :controller => plugin_name + '_plugin_environment'
+  map.connect 'plugin/' + plugin_name + '/:action/:id', :controller => plugin_name + '_plugin'
   map.connect 'profile/:profile/plugins/' + plugin_name + '/:action/:id', :controller => plugin_name + '_plugin_profile'
   map.connect 'myprofile/:profile/plugin/' + plugin_name + '/:action/:id', :controller => plugin_name + '_plugin_myprofile'
   map.connect 'admin/plugin/' + plugin_name + '/:action/:id', :controller => plugin_name + '_plugin_admin'
