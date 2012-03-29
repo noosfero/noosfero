1f929062725d05a0c7c72e4db19eae34c78052ab
Alessandro Palmeira + João M. M. da Silva <alessandro.palmeira@gmail.com>
2012-03-29 17:17:57 -0300

    [Mezuro] Added "new range" partials in metric configuration

--- plugins/mezuro/controllers/mezuro_plugin_profile_controller.rb
+++ plugins/mezuro/controllers/mezuro_plugin_profile_controller.rb
@@ -56,6 +56,13 @@ class MezuroPluginProfileController < ProfileController
     @metric_configuration.metric = Kalibro::Entities::NativeMetric.new
     @metric_configuration.metric.name = "Nome falso"
     @metric_configuration.metric.origin = "Origem Falsa"
+    range = Kalibro::Entities::Range.new
+    range.beginning = "0"
+    range.end = "100"
+    range.label = "fake label"
+    range.grade = "100"
+    range.color = "FFFFFF"
+    @metric_configuration.range = [range]
   end
 
   def create_metric_configuration
@@ -66,5 +73,18 @@ class MezuroPluginProfileController < ProfileController
   def update_metric_configuration
     @configuration_name = params[:configuration_name]
     redirect_to "/#{profile.identifier}/#{@configuration_name.downcase.gsub(/\s/, '-')}"
+  end
+
+  def new_range
+  end
+
+  def create_range
+    @range = Kalibro::Entities::Range.new
+    @range.beginning = params[:range][:beginning]
+    @range.end = params[:range][:end]
+    @range.label = params[:range][:label]
+    @range.grade = params[:range][:grade]
+    @range.color = params[:range][:color]
+    @range.comments = params[:range][:comments]
   end
 end
