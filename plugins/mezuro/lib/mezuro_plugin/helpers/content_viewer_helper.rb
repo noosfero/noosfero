aec7550de060f01c65f51593101138352f7c01f0
Caio Salgado + Diego Araújo <caio.csalgado@gmail.com>
2012-04-04 15:41:13 -0300

    [Mezuro] add new method in helper and made tests

--- plugins/mezuro/lib/mezuro_plugin/helpers/content_viewer_helper.rb
+++ plugins/mezuro/lib/mezuro_plugin/helpers/content_viewer_helper.rb
@@ -2,4 +2,8 @@ class MezuroPlugin::Helpers::ContentViewerHelper
   def self.format_grade(grade)
     sprintf("%.2f", grade.to_f)
   end
+
+  def self.create_periodicity_options
+   [["Not Periodically", 0], ["1 day", 1], ["2 days", 2], ["Weekly", 7], ["Biweeky", 15], ["Monthly", 30]]
+  end
 end
--- plugins/mezuro/test/unit/mezuro_plugin/helpers/content_viewer_helper_test.rb
+++ plugins/mezuro/test/unit/mezuro_plugin/helpers/content_viewer_helper_test.rb
@@ -7,4 +7,8 @@ class ContentViewerHelperTest < ActiveSupport::TestCase
     assert_equal '4.10', MezuroPlugin::Helpers::ContentViewerHelper.format_grade('4.1')
     assert_equal '4.00', MezuroPlugin::Helpers::ContentViewerHelper.format_grade('4')
   end
+
+  should 'create the periodicity options array' do
+    assert_equal [["Not Periodically", 0], ["1 day", 1], ["2 days", 2], ["Weekly", 7], ["Biweeky", 15], ["Monthly", 30]], MezuroPlugin::Helpers::ContentViewerHelper.create_periodicity_options
+  end
 end
