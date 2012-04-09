5a07068e56f10e5951051ce3b083adc34100bece
Rodrigo Souto <rodrigo@colivre.coop.br>
2012-04-09 16:43:25 -0300

    Making blocks and articles cache language sensitive
    (ActionItem2336)

--- app/sweepers/block_sweeper.rb
+++ app/sweepers/block_sweeper.rb
@@ -1,10 +1,28 @@
 class BlockSweeper < ActiveRecord::Observer
 
-  include SweeperHelper
   observe :block
 
+  class << self
+    include SweeperHelper
+
+    def cache_key_regex(block)
+      regex = '-[a-z]*$'
+      clean_ck = block.cache_key.gsub(/#{regex}/,'')
+      %r{#{clean_ck+regex}}
+    end
+
+    # Expire block's all languages cache
+    def expire_block(block)
+      expire_timeout_fragment(cache_key_regex(block))
+    end
+
+    def expire_blocks(blocks)
+      blocks.each { |block| expire_block(block) }
+    end
+  end
+
   def after_save(block)
-    expire_fragment(block.cache_key)
+    self.class.expire_block(block)
   end
 
 end
