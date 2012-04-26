06d09a45ec152093238bd0324cb2f7ff019baf0f
Diego Araújo + Paulo Meirelles <diegoamc90@gmail.com>
2012-04-26 16:39:07 -0300

    [Mezuro] Changed the table for metric history

--- plugins/mezuro/controllers/mezuro_plugin_profile_controller.rb
+++ plugins/mezuro/controllers/mezuro_plugin_profile_controller.rb
@@ -40,5 +40,13 @@ class MezuroPluginProfileController < ProfileController
     source_tree = project_result.node_of(params[:module_name])
     render :partial =>'content_viewer/source_tree', :locals => { :source_tree => source_tree, :project_name => content.project.name}
   end
-  
+
+  def module_metrics_history
+    metric_name = params[:metric_name]
+    content = profile.articles.find(params[:id])
+    module_history = content.result_history(params[:module_name])
+    date_history = module_history.collect { |x| x.date }
+    metric_history = module_history.collect { |x| (x.metric_results.select { |y| y.metric.name.delete("() ") == metric_name })[0] }
+    render :partial => 'content_viewer/metric_history', :locals => {:metric_history => metric_history, :date_history => date_history }
+  end
 end
