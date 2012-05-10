7e86bd7df84c5e987177c71a93cf2514cbbf30c4
Alessandro Palmeira + Caio Salgado <caio.csalgado@gmail.com>
2012-05-10 18:06:55 -0300

    [Mezuro] Added collect_time to project_result_fixtures

--- plugins/mezuro/test/fixtures/project_result_fixtures.rb
+++ plugins/mezuro/test/fixtures/project_result_fixtures.rb
@@ -11,12 +11,14 @@ class ProjectResultFixtures
     result.load_time = 14878
     result.analysis_time = 1022
     result.source_tree = ModuleNodeFixtures.qt_calculator_tree
+    result.collect_time = 14878
     result
   end
 
   def self.qt_calculator_hash
     {:project => ProjectFixtures.qt_calculator_hash, :date => ModuleResultFixtures.create_hash[:date],
-      :load_time => 14878, :analysis_time => 1022, :source_tree => ModuleNodeFixtures.qt_calculator_tree_hash}
+      :load_time => 14878, :analysis_time => 1022, :source_tree => ModuleNodeFixtures.qt_calculator_tree_hash, 
+      :collect_time => 14878}
   end
     
 end
