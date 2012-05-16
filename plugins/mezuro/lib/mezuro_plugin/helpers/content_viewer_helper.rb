924e1f841c1d47acee7bdb790761cd8f46640a6e
João M. M. da Silva + Rafael Manzo <jaodsilv@linux.ime.usp.br>
2012-05-16 16:53:06 -0300

    [Mezuro] Google chart helper added to content viewer.

--- plugins/mezuro/lib/mezuro_plugin/helpers/content_viewer_helper.rb
+++ plugins/mezuro/lib/mezuro_plugin/helpers/content_viewer_helper.rb
@@ -1,3 +1,5 @@
+require 'googlecharts'
+
 class MezuroPlugin::Helpers::ContentViewerHelper
   def self.format_grade(grade)
     sprintf("%.2f", grade.to_f)
@@ -5,5 +7,15 @@ class MezuroPlugin::Helpers::ContentViewerHelper
 
   def self.create_periodicity_options
    [["Not Periodically", 0], ["1 day", 1], ["2 days", 2], ["Weekly", 7], ["Biweeky", 15], ["Monthly", 30]]
+  end
+  
+  def self.generate_chart(values)
+    Gchart.line(  
+                :title_color => 'FF0000',
+                :size => '700x180', 
+                :bg => {:color => 'efefef', :type => 'stripes'},
+                :line_colors => 'c4a000',
+                :data => values,
+                :axis_with_labels => 'y')
   end
 end
