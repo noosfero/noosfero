7ba12764fe6bd60d5c6763d8183974229235ba0c
Antonio Terceiro <terceiro@colivre.coop.br>
2012-05-16 13:10:04 -0300

    Fix test: do not pass articles without a profile

--- test/unit/cms_helper_test.rb
+++ test/unit/cms_helper_test.rb
@@ -8,7 +8,7 @@ class CmsHelperTest < ActiveSupport::TestCase
 
   should 'show default options for article' do
     CmsHelperTest.any_instance.stubs(:controller).returns(ActionController::Base.new)
-    result = options_for_article(RssFeed.new)
+    result = options_for_article(RssFeed.new(:profile => Profile.new))
     assert_match /id="article_published" name="article\[published\]" type="checkbox" value="1"/, result
     assert_match /id="article_accept_comments" name="article\[accept_comments\]" type="checkbox" value="1"/, result
   end
