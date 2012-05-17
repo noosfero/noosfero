f6e2305dcd826aeedb6fc2312b413db0e345b31f
Diego Araújo + João Machini <diegoamc90@gmail.com>
2012-05-17 15:43:40 -0300

    [Mezuro] Finished validating fields for new range and metric configuration.

--- plugins/mezuro/public/javascripts/validations.js
+++ plugins/mezuro/public/javascripts/validations.js
@@ -1,10 +1,18 @@
+jQuery(function (){
+  jQuery('#range_submit').live("click", validate_new_range_configuration);
+  jQuery('#metric_configuration_submit').live("click", validate_metric_configuration);
+});
+
+
+
 function validate_metric_configuration(){
-    var code=document.forms["configuration_form"]["metric_configuration[code]"].value;
+    var code = jQuery('#metric_configuration_code').val();
     if (is_null(code))
     {
         alert("Code must be filled out");
         return false;
     }
+    return true;
 }
 
 function is_null(value){
@@ -30,14 +38,12 @@ function IsNotHexadecimal(value){
     return true;
 }
 
-function validate_new_range_configuration(){
-    var label = document.forms["new_range_form"]["range[label]"].value;
-    var beginning = document.forms["new_range_form"]["range[beginning]"].value;
-    var end = document.forms["new_range_form"]["range[end]"].value;
-    var color = document.forms["new_range_form"]["range[color]"].value;
-    var grade = document.forms["new_range_form"]["range[grade]"].value;
-    
-    return false;
+function validate_new_range_configuration(event){    
+    var label = jQuery("#range_label").val();
+    var beginning = jQuery("#range_beginning").val();
+    var end = jQuery("#range_end").val();
+    var color = jQuery("#range_color").val();
+    var grade = jQuery("#range_grade").val();
     
     if (is_null(label) || is_null(beginning) || is_null(end) || is_null(color) || is_null(grade))
     {
