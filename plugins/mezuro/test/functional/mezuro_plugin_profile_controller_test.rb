mezuro/noosfero-plugin

[Mezuro] Fixed 'get module result' test

plugins/mezuro/test/functional/mezuro_plugin_profile_controller_test.rb
@@ -68,7 +68,10 @@ class MezuroPluginProfileControllerTest < ActionController::TestCase
 
   should 'get module result' do
     create_project_content
-    Kalibro::Client::ModuleResultClient.expects(:module_result).with(@content, @name).returns(@module_result)
+	module_result_client = mock
+    Kalibro::Client::ProjectResultClient.expects(:last_result).with(@name).returns(@project_result)
+	Kalibro::Client::ModuleResultClient.expects(:new).returns(module_result_client)
+    module_result_client.expects(:module_result).with(@name, @name, @project_result.date).returns(@module_result)
     get :module_result, :profile => @profile.identifier, :id => @content.id, :module_name => @name
     assert_response 200
     assert_select('h5', 'Metric results for: Qt-Calculator (APPLICATION)')

noosfero/noosfero

plugins/mezuro/test/functional/mezuro_plugin_profile_controller_test.rb
@@ -65,9 +65,10 @@ class MezuroPluginProfileControllerTest < ActionController::TestCase
   
   should 'get project results from a specific date' do
     create_project_content
-	  mock_project_result
-  	Kalibro::Client::ProjectClient.expects(:project).with(@name).returns(@project)
-  	get :project_result, :profile => @profile.identifier, :id => @content.id, :date => @project_result.date
+    mock_project_result
+    Kalibro::Client::ProjectClient.expects(:project).with(@name).returns(@project)
+    get :project_result, :profile => @profile.identifier, :id => @content.id, :date => @project_result.date
+    module_result_client.expects(:module_result).with(@name, @name, @project_result.date).returns(@module_result)
     assert_response 200
   end
   