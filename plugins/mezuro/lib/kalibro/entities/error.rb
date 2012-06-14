c71f3d8186de9cbba43c785cc445f7e42adf40b1
Paulo Meirelles <paulo@softwarelivre.org>
2012-06-13 21:29:46 -0300

    [Mezuro] Adding cause error method

--- plugins/mezuro/lib/kalibro/entities/error.rb
+++ plugins/mezuro/lib/kalibro/entities/error.rb
@@ -1,6 +1,6 @@
 class Kalibro::Entities::Error < Kalibro::Entities::Entity
   
-  attr_accessor :error_class, :message, :stack_trace_element
+  attr_accessor :error_class, :message, :stack_trace_element, :cause
 
   def stack_trace_element=(value)
     @stack_trace_element = to_entity_array(value, Kalibro::Entities::StackTraceElement)
@@ -12,6 +12,10 @@ class Kalibro::Entities::Error < Kalibro::Entities::Entity
 
   def stack_trace=(stack_trace)
     @stack_trace_element = stack_trace
+  end
+
+  def cause=(cause_value)
+    @cause = to_entity(cause_value, Kalibro::Entities::Error)
   end
 
 end
