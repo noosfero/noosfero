dbf169eeeaada60f4da06e4c4fcd0144298631ad
Larissa Reis <larissa@colivre.coop.br>
2012-06-22 12:08:45 -0300

    Fixes crash in shopping cart reports when product is removed
      Changes what's stored in PurchaseOrder so the product name is also stored,
      that way if the product is removed its name isn't lost.
      Uses a class named LineItem to encapsulate the information about an
      ordered product for clarification.
      Also fixes the views so it shows the product name but not the product url
      if the product was removed.
    
    (ActionItem2362)

---
+++ plugins/shopping_cart/lib/shopping_cart_plugin/line_item.rb
@@ -0,0 +1,23 @@
+class ShoppingCartPlugin::LineItem
+
+  attr_accessor :product_id, :quantity
+
+  def initialize(product_id, name)
+    @product_id = product_id
+    @name = name
+    @quantity = 0
+  end
+
+  def product
+    @product ||= Product.find_by_id(product_id)
+  end
+
+  def name
+    product && product.name || @name
+  end
+
+  def ==(other)
+    self.product == other.product && self.name == other.name && self.quantity == other.quantity
+  end
+
+end
