4901ee9020df692ba9e34427fad4865c3474084e
Diego Araújo + João M. M. da Silva + Pedro Leal <diegoamc90@gmail.com>
2012-07-12 16:40:53 -0300

    [Mezuro] Refactored Compound Metric entity to model.

--- plugins/mezuro/test/fixtures/compound_metric_fixtures.rb
+++ plugins/mezuro/test/fixtures/compound_metric_fixtures.rb
@@ -1,15 +1,10 @@
 class CompoundMetricFixtures
 
-  def self.sc
-    sc = Kalibro::Entities::CompoundMetric.new
-    sc.description = 'Calculate the Structural Complexity of the Code'
-    sc.name = 'Structural Complexity'
-    sc.scope = 'CLASS'
-    sc.script = 'return 42;'
-    sc
+  def self.compound_metric
+    Kalibro::CompoundMetric.new compound_metric_hash
   end
 
-  def self.sc_hash
+  def self.compound_metric_hash
     {:name => 'Structural Complexity', :scope => 'CLASS', :script => 'return 42;', :description => 'Calculate the Structural Complexity of the Code'}
   end
 
--- plugins/mezuro/test/unit/kalibro/compound_metric_test.rb
+++ plugins/mezuro/test/unit/kalibro/compound_metric_test.rb
@@ -5,12 +5,12 @@ require "#{RAILS_ROOT}/plugins/mezuro/test/fixtures/compound_metric_fixtures"
 class CompoundMetricTest < ActiveSupport::TestCase
 
   def setup
-    @hash = CompoundMetricFixtures.sc_hash
-    @metric = CompoundMetricFixtures.sc
+    @hash = CompoundMetricFixtures.compound_metric_hash
+    @metric = CompoundMetricFixtures.compound_metric
   end
 
   should 'create compound metric from hash' do
-    assert_equal @metric, Kalibro::Entities::CompoundMetric.from_hash(@hash)
+    assert_equal @hash[:script], Kalibro::CompoundMetric.new(@hash).script
   end
 
   should 'convert compound metric to hash' do
