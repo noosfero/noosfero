01ad0fd18eb09f9310f6de5a118e823e97bbda80
Daniela Soares Feitosa <danielafeitosa@colivre.coop.br>
2012-07-18 05:40:05 -0300

    Fixed google_cse files names to match plugin routes
    Routes changed on commit bfce4eb13212734b675c4fd07f23ecca6946cb3e

--- plugins/google_cse/controllers/google_cse_plugin_controller.rb
+++ plugins/google_cse/controllers/google_cse_plugin_controller.rb
@@ -1,4 +1,4 @@
-class GoogleCsePluginEnvironmentController < ApplicationController
+class GoogleCsePluginController < ApplicationController
   append_view_path File.join(File.dirname(__FILE__) + '/../views')
   no_design_blocks
 end
--- plugins/google_cse/test/functional/google_cse_plugin_controller_test.rb
+++ plugins/google_cse/test/functional/google_cse_plugin_controller_test.rb
@@ -1,13 +1,13 @@
 require File.dirname(__FILE__) + '/../../../../test/test_helper'
-require File.dirname(__FILE__) + '/../../controllers/google_cse_plugin_environment_controller'
+require File.dirname(__FILE__) + '/../../controllers/google_cse_plugin_controller'
 
 # Re-raise errors caught by the controller.
-class GoogleCsePluginEnvironmentController; def rescue_action(e) raise e end; end
+class GoogleCsePluginController; def rescue_action(e) raise e end; end
 
-class GoogleCsePluginEnvironmentControllerTest < ActionController::TestCase
+class GoogleCsePluginControllerTest < ActionController::TestCase
 
   def setup
-    @controller = GoogleCsePluginEnvironmentController.new
+    @controller = GoogleCsePluginController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
   end
