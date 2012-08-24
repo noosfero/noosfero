26d49af9f769157f26d04d2890652e64aee14b63
Antonio Terceiro <terceiro@colivre.coop.br>
2012-08-24 15:30:33 -0300

    Remove bogus thread usage

--- test/unit/profile_test.rb
+++ test/unit/profile_test.rb
@@ -3,10 +3,6 @@ require File.dirname(__FILE__) + '/../test_helper'
 class ProfileTest < ActiveSupport::TestCase
   fixtures :profiles, :environments, :users, :roles, :domains
 
-  def teardown
-    Thread.current[:enabled_plugins] = nil
-  end
-
   def test_identifier_validation
     p = Profile.new
     p.valid?
