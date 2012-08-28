9a31421eb81f59c614059115d5b342c0705bcc0c
Antonio Terceiro <terceiro@colivre.coop.br>
2012-08-28 13:48:32 -0300

    make extra dependency for Mezuro explicit

--- plugins/mezuro/dependencies.rb
+++ plugins/mezuro/dependencies.rb
@@ -1,1 +1,2 @@
 require 'savon'
+require 'googlecharts'
--- plugins/mezuro/lib/mezuro_plugin/helpers/content_viewer_helper.rb
+++ plugins/mezuro/lib/mezuro_plugin/helpers/content_viewer_helper.rb
@@ -1,5 +1,3 @@
-require 'googlecharts'
-
 class MezuroPlugin::Helpers::ContentViewerHelper
   def self.format_grade(grade)
     sprintf("%.2f", grade.to_f)
