3844f93c462dc69a7432b4baae18fdeca797a490
Rodrigo Souto <rodrigo@colivre.coop.br>
2012-09-11 14:07:42 -0300

    Fixing typo plugins -> plugin

--- lib/noosfero/plugin/routes.rb
+++ lib/noosfero/plugin/routes.rb
@@ -20,7 +20,7 @@ Dir.glob(File.join(Rails.root, plugins_root, '*', 'controllers')) do |controller
   end
 
   map.connect 'plugin/' + plugin_name + '/:action/:id', :controller => plugin_name + '_plugin'
-  map.connect 'profile/:profile/plugins/' + plugin_name + '/:action/:id', :controller => plugin_name + '_plugin_profile'
+  map.connect 'profile/:profile/plugin/' + plugin_name + '/:action/:id', :controller => plugin_name + '_plugin_profile'
   map.connect 'myprofile/:profile/plugin/' + plugin_name + '/:action/:id', :controller => plugin_name + '_plugin_myprofile'
   map.connect 'admin/plugin/' + plugin_name + '/:action/:id', :controller => plugin_name + '_plugin_admin'
 end
