3307129f8eed7c235b282ee37004ca74345bae3c
João M. M. da Silva + Alessandro Palmeira <jaodsilv@linux.ime.usp.br>
2012-09-18 18:00:51 -0300

    [Mezuro] Draft to configuration exceptions handling.

--- plugins/mezuro/controllers/myprofile/mezuro_plugin_myprofile_controller.rb
+++ plugins/mezuro/controllers/myprofile/mezuro_plugin_myprofile_controller.rb
@@ -3,8 +3,8 @@ class MezuroPluginMyprofileController < ProfileController #MyprofileController?
   append_view_path File.join(File.dirname(__FILE__) + '/../../views')
 
   rescue_from Exception do |exception|
-    message = URI.escape(CGI.escape(exception.message),'.')
-    redirect_to_error_page message
+    @message = process_error_message exception.message
+    render :partial => "error_page"
   end
 
   def error_page
@@ -26,12 +26,20 @@ class MezuroPluginMyprofileController < ProfileController #MyprofileController?
   protected
 
   def redirect_to_error_page(message)
-    message = URI.escape(CGI.escape(message),'.')
+    message = URI.escape(CGI.escape(process_error_message(message)),'.')
     redirect_to "/myprofile/#{profile.identifier}/plugin/mezuro/error_page?message=#{message}"
   end
 
   def metric_configuration_has_errors? metric_configuration
     not metric_configuration.errors.empty?
+  end
+
+  def process_error_message message
+    if message =~ /bla/
+      message
+    else
+      message
+    end
   end
 
 end
