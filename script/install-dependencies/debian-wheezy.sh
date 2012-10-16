58aeae13605a44fdce974fd3a1711fc1e62666a9
João da Silva + Eduardo Morais + Rafael Manzo <rr.manzo@gmail.com>
2012-10-16 15:52:49 -0300

    quick-start updated

---
+++ script/install-dependencies/debian-wheezy.sh
@@ -0,0 +1,10 @@
+# needed to run noosfero
+runtime_dependencies=$(sed -e '1,/^Depends:/d; /^Recommends:/,$ d; s/([^)]*)//g; s/,\s*/\n/g' debian/control | grep -v 'memcached\|debconf\|dbconfig-common\|postgresql\|misc:Depends\|adduser\|mail-transport-agent')
+run sudo apt-get -y install $runtime_dependencies
+sudo apt-get -y install iceweasel || sudo apt-get -y install firefox
+
+# needed for development
+run sudo apt-get -y install ruby-tidy ruby-mocha imagemagick po4a xvfb libxml2-dev libxslt1-dev
+gem which bundler >/dev/null 2>&1 || gem_install bundler
+setup_rubygems_path
+run bundle install
