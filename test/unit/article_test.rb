8d5b096c970c816e8b21acb4560380b954d6963e
Rodrigo Souto <rodrigo@colivre.coop.br>
2012-10-23 22:04:16 +0000

    tUpdating path before save

--- test/unit/article_test.rb
+++ test/unit/article_test.rb
@@ -1782,4 +1782,22 @@ class ArticleTest < ActiveSupport::TestCase
     assert_equal license, article.license
   end
 
+  should 'update path if parent is changed' do
+    f1 = Folder.create!(:name => 'Folder 1', :profile => profile)
+    f2 = Folder.create!(:name => 'Folder 2', :profile => profile)
+    article = TinyMceArticle.create!(:name => 'Sample Article', :parent_id => f1.id, :profile => profile)
+    assert_equal [f1.path,article.slug].join('/'), article.path
+
+    article.parent = f2
+    article.save!
+    assert_equal [f2.path,article.slug].join('/'), article.path
+
+    article.parent = nil
+    article.save!
+    assert_equal article.slug, article.path
+
+    article.update_attributes({:parent_id => f2.id})
+    assert_equal [f2.path,article.slug].join('/'), article.path
+  end
+
 end
