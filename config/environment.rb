6bda4c8e41eaf49ddc83ff9a718aa4778515bf4a
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2012-10-23 17:41:54 -0200

    Environments updated to Rails 3 standard

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,9 @@
-# Be sure to restart your web server when you modify this file.
+# Load the rails application
+require File.expand_path('../application', __FILE__)
 
 # Uncomment below to force Rails into production mode when 
 # you don't control web/app server and can't set it the proper way
 #ENV['RAILS_ENV'] ||= 'production'
-
-# Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
-
-# Bootstrap the Rails environment, frameworks, and default configuration
-require File.join(File.dirname(__FILE__), 'boot')
 
 # extra directories for controllers organization 
 extra_controller_dirs = %w[
