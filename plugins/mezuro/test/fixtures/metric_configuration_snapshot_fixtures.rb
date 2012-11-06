4edeb9d92ecac9c01adcdb88e3591e16d89840da
João M. M. da Silva + Diego Araújo <jaodsilv@linux.ime.usp.br>
2012-11-06 18:26:44 -0200

    [Mezuro] Completed metric configuration snapshot model and tests creation.

---
+++ plugins/mezuro/test/fixtures/metric_configuration_snapshot_fixtures.rb
@@ -0,0 +1,26 @@
+require File.dirname(__FILE__) + '/metric_fixtures'
+require File.dirname(__FILE__) + '/range_snapshot_fixtures'
+
+class MetricConfigurationSnapshotFixtures
+
+  def self.metric_configuration_snapshot
+    Kalibro::MetricConfigurationSnapshot.new metric_configuration_snapshot_hash
+  end
+
+  def self.metric_configuration_snapshot_hash
+    {
+      :code => "code",
+      :weight => 1,
+      :aggregation_form => 'AVERAGE',
+      :metric => MetricFixtures.amloc_hash,
+      :base_tool_name => "Analizo",
+      :range => [RangeSnapshotFixtures.range_snapshot_hash],
+      :attributes! => {
+        :metric => {
+          'xmlns:xsi'=> 'http://www.w3.org/2001/XMLSchema-instance',
+          'xsi:type' => 'kalibro:metricXml'  }
+      }
+    }
+  end
+
+end
