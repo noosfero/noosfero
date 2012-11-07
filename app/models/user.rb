7af9fa59a99bdc295bdc95029decdcb12e44104f
Daniela Soares Feitosa <danielafeitosa@colivre.coop.br>
2012-11-07 20:57:38 -0200

    Revert "Revert "Do not send email to template users""
    This reverts commit b8920a1e95ef9e9d54527de691fbeb1b1921a27f.
    
    Conflicts:
    
    	app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -328,7 +328,7 @@ class User < ActiveRecord::Base
     end
 
     def deliver_activation_code
-      return if person.identifier =~ /person_template/
+      return if person.is_template?
       User::Mailer.deliver_activation_code(self) unless self.activation_code.blank?
     end
 
