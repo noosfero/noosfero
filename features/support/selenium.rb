b0d063303a26afc68d1840e79e0972684b4ebb5d
Daniel Cunha <daniel.ccunha@gmail.com>
2012-11-23 02:31:59 -0300

    removing duplicated code

--- features/support/selenium.rb
+++ features/support/selenium.rb
@@ -1,13 +1,3 @@
-require 'rbconfig'
-require 'cucumber/formatter/unicode'
-
-require 'capybara'
-require 'capybara/dsl'
-require "capybara/cucumber"
-
-require 'database_cleaner'
-require 'database_cleaner/cucumber'
-
 Cucumber::Rails::World.use_transactional_fixtures = false
 
 Capybara.default_driver = :selenium
@@ -19,16 +9,10 @@ DatabaseCleaner.clean_with :truncation
 DatabaseCleaner.strategy = :truncation
 
 Before do
-  Fixtures.reset_cache
-  fixtures_folder = File.join(RAILS_ROOT, 'test', 'fixtures')
-  fixtures = ['environments', 'roles']
-  Fixtures.create_fixtures(fixtures_folder, fixtures)
-  ENV['LANG'] = 'C'
   DatabaseCleaner.start
 end
 
 After do
-  sleep 2
   DatabaseCleaner.clean
 end
 
