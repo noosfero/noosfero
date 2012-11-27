21eb9a5b8b2ae07f0948b0872eae64bcb3c13440
João M. M. da Silva + Rafael Manzo <jaodsilv@linux.ime.usp.br>
2012-11-27 16:26:00 -0200

    [Mezuro] Fixed Kalibro::Project.all when it should return just 1 project and Kalibro::Configuration.all when it should return just 1 configuration

--- plugins/mezuro/lib/kalibro/project.rb
+++ plugins/mezuro/lib/kalibro/project.rb
@@ -3,8 +3,9 @@ class Kalibro::Project < Kalibro::Model
   attr_accessor :id, :name, :description
 
   def self.all
-    response = request(:all_projects)[:project].to_a
+    response = request(:all_projects)[:project]
     response = [] if response.nil?
+    response = [response] if response.is_a? (Hash) 
     response.map {|project| new project}
   end
 
