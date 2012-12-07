2d1d91bc39c6286b9f1e84145cc91b22bb8034ae
Daniel Cunha <daniel.ccunha@gmail.com>
2012-12-07 08:39:20 -0300

    databasecleaner problem

--- features/support/selenium.rb
+++ features/support/selenium.rb
@@ -8,6 +8,7 @@ Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, :browser => :firefox)
 end
 
+# FIXME: 'DELETE FROM ...' is being ran 3x - see cucumber.log
 DatabaseCleaner.clean_with :truncation
 DatabaseCleaner.strategy = :truncation
 
