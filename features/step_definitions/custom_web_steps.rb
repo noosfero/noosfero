cb8a5d9d24962835a7f080b2a6fe9b825bacd2aa
Daniel Cunha <daniel.ccunha@gmail.com>
2012-12-12 14:52:08 -0300

    events.feature

--- features/step_definitions/custom_web_steps.rb
+++ features/step_definitions/custom_web_steps.rb
@@ -4,6 +4,14 @@ Then /^"([^"]*)" should not be visible within "([^"]*)"$/ do |text, selector|
   end
 end
 
+Then /^I should see "([^"]*)" link$/ do |text|
+  page.should have_css('a', :text => text)
+end
+
+Then /^I should not see "([^"]*)" link$/ do |text|
+  page.should have_no_css('a', :text => text)
+end
+
 When /^I reload and wait for the page$/ do
   raise "Why why need this? Remove!"
   visit page.driver.browser.current_url
