f8df63547d67ae076779b24de1df0d94dca4b9b6
Antonio Terceiro <terceiro@colivre.coop.br>
2012-12-25 16:40:55 -0300

    Revert "Assets moved from the public and adapted to Rails 3 standard"
    This reverts commit 86d4d6669bb21fd220f6a2d0d0b2ad0e27954ed7.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,6 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+
 function noosfero_init() {
   // focus_first_field(); it is moving the page view when de form is down.
 }
