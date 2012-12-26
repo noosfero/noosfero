78ad0c16bb492561c98f22fdb435aa064224ee4d
Antonio Terceiro <terceiro@colivre.coop.br>
2012-12-25 22:56:05 -0300

    Remove more solr stuff

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,5 @@
 ENV["RAILS_ENV"] = "test"
 
-# Start/stop Solr
-if not $test_helper_loaded
-	abort unless system 'rake -s solr:start'
-  at_exit { system 'rake -s solr:stop' }
-  $test_helper_loaded = true
-end
-
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
 require 'mocha'
@@ -17,7 +10,6 @@ require 'noosfero/test'
 require File.dirname(__FILE__) + '/factories'
 require File.dirname(__FILE__) + '/noosfero_doc_test'
 require File.dirname(__FILE__) + '/action_tracker_test_helper'
-require File.expand_path(File.dirname(__FILE__) + "/test_solr_helper.rb")
 
 FileUtils.rm_rf(File.join(Rails.root, 'index', 'test'))
 
