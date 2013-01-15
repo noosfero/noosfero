a5586b3bc9c3f528b29fd8c6617fdabf564a9b1b
Leandro Nunes dos Santos <leandro.santos@serpro.gov.br>
2013-01-15 14:12:55 -0300

    adding DisplayContent plugin

---
+++ plugins/display_content/test/unit/display_content_plugin_test.rb
@@ -0,0 +1,23 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class DisplayContentPluginTest < ActiveSupport::TestCase
+
+  should "add the jstree javascript" do
+    plugin = DisplayContentPlugin.new
+    assert plugin.js_files.include?('/javascripts/jstree/jquery.jstree.js')
+  end
+
+  should "add new JQuery version" do
+    plugin = DisplayContentPlugin.new
+    assert plugin.js_files.include?('/javascripts/jstree/_lib/jquery-1.8.3.js')
+  end
+
+  should "return DisplayContentBlock in extra_mlocks class method" do
+    assert  DisplayContentPlugin.extra_blocks.keys.include?(DisplayContentBlock)
+  end
+
+  should "return false for class method has_admin_url?" do
+    assert  !DisplayContentPlugin.has_admin_url?
+  end
+
+end
