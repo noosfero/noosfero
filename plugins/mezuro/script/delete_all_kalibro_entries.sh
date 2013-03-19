7b8b62ef9d08fc3c7b618bb9b8c46440f815812d
Daniel Alves + Diego Araújo + Guilherme Rojas <danpaulalves@gmail.com>
2013-03-19 18:24:32 -0300

    [Mezuro] Improved database scripts.

--- plugins/mezuro/script/delete_all_kalibro_entries.sh
+++ plugins/mezuro/script/delete_all_kalibro_entries.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-DATABASE="kalibro_test"
+source plugins/mezuro/script/kalibro_scripts.conf
 
-sudo su postgres -c "export PGPASSWORD=\"kalibro\" && psql -q -t -d $DATABASE -c \"SELECT 'DELETE FROM ' || n.nspname || '.' || c.relname || ' CASCADE;' FROM pg_catalog.pg_class AS c LEFT JOIN pg_catalog.pg_namespace AS n ON n.oid = c.relnamespace WHERE relkind = 'r' AND n.nspname NOT IN ('pg_catalog', 'pg_toast') AND pg_catalog.pg_table_is_visible(c.oid)\" | sed '/sequences/d' > /tmp/query && psql -q -d $DATABASE -f /tmp/query && rm /tmp/query"
+sudo su postgres -c "export PGPASSWORD=$PASSWORD && psql -q -d $DATABASE -f $QUERYFILE"
