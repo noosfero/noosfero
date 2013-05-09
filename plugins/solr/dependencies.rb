6ff0271273650a90deaabb452d130c34dcbd5f8b
Rodrigo Souto <rodrigo@colivre.coop.br>
2013-05-09 19:03:48 -0300

    Moving acts as searchable to the plugin

--- plugins/solr/dependencies.rb
+++ plugins/solr/dependencies.rb
@@ -1,4 +1,4 @@
 require 'rubygems'
 require 'active_record'
-require 'acts_as_searchable'
+require "#{File.dirname(__FILE__)}/lib/acts_as_searchable"
 require "#{File.dirname(__FILE__)}/lib/acts_as_faceted"
