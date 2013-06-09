212e8b0b59314e43a4df748ad8976fe18f8b77fb
Antonio Terceiro <terceiro@colivre.coop.br>
2013-06-09 01:56:20 -0300

    [rails3] Adjust attr_accessible

--- app/models/mailing.rb
+++ app/models/mailing.rb
@@ -2,6 +2,7 @@ require 'mailing_job'
 
 class Mailing < ActiveRecord::Base
 
+  attr_accessible :subject, :body
   validates_presence_of :source_id, :subject, :body
   belongs_to :source, :foreign_key => :source_id, :polymorphic => true
   belongs_to :person
--- test/unit/mailing_sent_test.rb
+++ test/unit/mailing_sent_test.rb
@@ -4,8 +4,10 @@ class MailingSentTest < ActiveSupport::TestCase
 
   should 'return mailing and person' do
     person = fast_create(Person)
-    mailing = Mailing.create(:source => Environment.default, :subject => 'Hello', :body => 'We have some news')
-    sent = MailingSent.create(:mailing => mailing, :person => person)
+    environment = Environment.default
+    mailing = environment.mailings.create(:subject => 'Hello', :body => 'We have some news')
+
+    sent = mailing.mailing_sents.create(:person => person)
 
     mailing_sent = MailingSent.find(sent.id)
     assert_equal [mailing, person], [mailing_sent.mailing, mailing_sent.person]
