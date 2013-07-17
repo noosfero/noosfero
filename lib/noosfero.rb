186b2b10caac1d6888d23217333527674f5ecddd
Rodrigo Souto <rodrigo@colivre.coop.br>
2013-07-17 03:41:46 -0300

    Revert "Bumping version 0.45.0~rc20130717024842"
    This reverts commit 48899650a4e02b6b1691db23abb839d5d2117a8f.

--- lib/noosfero.rb
+++ lib/noosfero.rb
@@ -3,7 +3,7 @@ require 'fast_gettext'
 
 module Noosfero
   PROJECT = 'noosfero'
-  VERSION = '0.45.0~rc20130717024842'
+  VERSION = '0.44.0'
 
   def self.pattern_for_controllers_in_directory(dir)
     disjunction = controllers_in_directory(dir).join('|')
