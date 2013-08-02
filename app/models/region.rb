05a4e7055efdc76502df5ae82368d4c851379060
Leandro Nunes dos Santos <leandronunes@gmail.com>
2013-08-02 11:44:20 -0300

    Rails3: Fix Region unit test

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,5 +1,8 @@
 # Region is a special type of category that is related to geographical issues. 
 class Region < Category
+ 
+  attr_accessible :name 
+ 
   has_and_belongs_to_many :validators, :class_name => 'Organization', :join_table => :region_validators
 
   require_dependency 'enterprise' # enterprises can also be validators
