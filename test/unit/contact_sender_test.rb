0b4205cedfe41e6ae9058a4601c5a7d6c4bd00c7
Leandro Nunes dos Santos <leandronunes@gmail.com>
2013-08-02 12:32:38 -0300

    Rails3: Fix ContactSender unit tests partially

--- test/unit/contact_sender_test.rb
+++ test/unit/contact_sender_test.rb
@@ -12,7 +12,7 @@ class ContactSenderTest < ActiveSupport::TestCase
   end
 
   should 'be able to deliver mail' do
-    ent = Enterprise.new(:name => 'my enterprise', :identifier => 'myent', :environment => Environment.default)
+    ent = Environment.default.enterprises.new(:name => 'my enterprise', :identifier => 'myent')
     ent.contact_email = 'contact@invalid.com'
     c = build(Contact, :dest => ent)
     response = Contact::Sender.deliver_mail(c)
@@ -21,7 +21,7 @@ class ContactSenderTest < ActiveSupport::TestCase
   end
 
   should 'deliver mail to contact_email' do
-    ent = Enterprise.new(:name => 'my enterprise', :identifier => 'myent', :environment => Environment.default)
+    ent = Environment.default.enterprises.new(:name => 'my enterprise', :identifier => 'myent')
     ent.contact_email = 'contact@invalid.com'
     c = build(Contact, :dest => ent)
     response = Contact::Sender.deliver_mail(c)
@@ -30,7 +30,7 @@ class ContactSenderTest < ActiveSupport::TestCase
  
   should 'deliver mail to admins of enterprise' do
     admin = create_user('admin_test').person
-    ent = Enterprise.create!(:name => 'my enterprise', :identifier => 'myent', :environment => Environment.default)
+    ent = Environment.default.enterprises.create!(:name => 'my enterprise', :identifier => 'myent')
     ent.contact_email = 'contact@invalid.com'
     ent.add_admin(admin)
     assert ent.save!
@@ -40,14 +40,14 @@ class ContactSenderTest < ActiveSupport::TestCase
   end
 
   should 'deliver a copy of email if requester wants' do
-    ent = Enterprise.new(:name => 'my enterprise', :identifier => 'myent', :environment => Environment.default)
+    ent = Environment.default.enterprises.new(:name => 'my enterprise', :identifier => 'myent')
     c = build(Contact, :dest => ent, :email => 'requester@invalid.com', :receive_a_copy => true)
     response = Contact::Sender.deliver_mail(c)
     assert_includes response.cc, c.email
   end
 
   should 'not deliver a copy of email if requester dont wants' do
-    ent = Enterprise.new(:name => 'my enterprise', :identifier => 'myent', :environment => Environment.default)
+    ent = Environment.default.enterprises.new(:name => 'my enterprise', :identifier => 'myent')
     c = build(Contact, :dest => ent, :email => 'requester@invalid.com', :receive_a_copy => false)
     response = Contact::Sender.deliver_mail(c)
     assert_nil response.cc
