3aa7898c30715cb3288fca845b6a43a0a5f63716
Rodrigo Souto <rodrigo@colivre.coop.br>
2013-09-17 12:00:48 -0300

    user_mailer_test: remove randomness from test

--- test/unit/user_mailer_test.rb
+++ test/unit/user_mailer_test.rb
@@ -11,11 +11,9 @@ class UserMailerTest < ActiveSupport::TestCase
 
   end
 
-
   should 'deliver activation email notify' do
     assert_difference ActionMailer::Base.deliveries, :size do
-      u = Person.find(:first).user
-      u.environment = Environment.default
+      u = create_user('some_user')
       User::Mailer.deliver_activation_email_notify(u)
     end
   end
