73458512f6527b6a9f7be583a4cbc56ad7b78212
Rodrigo Souto <rodrigo@colivre.coop.br>
2013-10-01 11:33:11 -0300

    ldap: create install file

--- plugins/ldap/install.rb
+++ plugins/ldap/install.rb
@@ -1,1 +1,5 @@
-raise "Not ready yet"
+system "gem install --user-install net-ldap -v 0.3.1"
+puts "\nWARNING: This plugin is not setting up a ldap test server automatically.
+Some tests may not be running. If you want to fully test this plugin, please
+setup the ldap test server and make the proper configurations on
+fixtures/ldap.yml.\n\n"
