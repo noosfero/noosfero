27c9021e39c55a86a1d7096577789b96c384102f
Victor Costa <vfcosta@gmail.com>
2013-10-30 11:14:51 -0300

    Reload plugin at each request on development mode
    Core plugins extensions was not reloaded (development mode) between requests.
    This breaks plugins which depends on extensions.

--- config/initializers/plugins.rb
+++ config/initializers/plugins.rb
@@ -5,3 +5,9 @@ require 'noosfero/plugin/active_record'
 require 'noosfero/plugin/mailer_base'
 require 'noosfero/plugin/settings'
 Noosfero::Plugin.init_system if $NOOSFERO_LOAD_PLUGINS
+
+if Rails.env == 'development' && $NOOSFERO_LOAD_PLUGINS
+  ActionController::Base.send(:prepend_before_filter) do |controller|
+    Noosfero::Plugin.init_system
+  end
+end
