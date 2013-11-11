d1926f5494ed085f2a462bce2b1682ac44a9f433
Braulio Bhavamitra <braulio@eita.org.br>
2013-11-11 19:07:31 -0300

    Cache theme renderings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -433,19 +433,19 @@ module ApplicationHelper
   end
 
   def theme_site_title
-    theme_include('site_title')
+    @theme_site_title ||= theme_include 'site_title'
   end
 
   def theme_header
-    theme_include('header')
+    @theme_header ||= theme_include 'header'
   end
 
   def theme_footer
-    theme_include('footer')
+    @theme_footer ||= theme_include 'footer'
   end
 
   def theme_extra_navigation
-    theme_include('navigation')
+    @theme_extra_navigation ||= theme_include 'navigation'
   end
 
   def is_testing_theme
