d205407d38b643dced3d360e293db5c17b22064a
Rodrigo Souto <rodrigo@colivre.coop.br>
2013-12-11 15:37:49 -0300

    Remove plugin reload on development
    This is a known problem that will be fixed as described on
    http://noosfero.org/Development/ActionItem2932

--- config/initializers/plugins.rb
+++ config/initializers/plugins.rb
@@ -6,9 +6,3 @@ require 'noosfero/plugin/mailer_base'
 require 'noosfero/plugin/settings'
 require 'noosfero/plugin/spammable'
 Noosfero::Plugin.init_system if $NOOSFERO_LOAD_PLUGINS
-
-if Rails.env == 'development' && $NOOSFERO_LOAD_PLUGINS
-  ActionController::Base.send(:prepend_before_filter) do |controller|
-    Noosfero::Plugin.init_system
-  end
-end
