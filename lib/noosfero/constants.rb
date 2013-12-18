50d57964af1b754a4b6eb123bb123d70b8628205
Braulio Bhavamitra <braulio@eita.org.br>
2013-12-18 14:36:28 -0300

    Allow _ in emails

--- lib/noosfero/constants.rb
+++ lib/noosfero/constants.rb
@@ -1,5 +1,5 @@
 module Noosfero::Constants
-  EMAIL_FORMAT = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  EMAIL_FORMAT = /\A([^@\s]+)@((?:[-_a-z0-9]+\.)+[a-z]{2,})\Z/i
   INTEGER_FORMAT = /\A\d*\Z/i
   PROFILE_PER_PAGE = 10
 end
