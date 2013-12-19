ffecc2e37006a4d1f2517ba9d15ee3970b2f2cde
Daniela Soares Feitosa <danielafeitosa@colivre.coop.br>
2013-12-19 01:59:17 +0000

    Fix: updated db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file,
+# This file is auto-generated from the current state of the database. Instead of editing this file, 
 # please use the migrations feature of Active Record to incrementally modify your database, and
 # then regenerate this schema definition.
 #
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131011164400) do
+ActiveRecord::Schema.define(:version => 20131121162641) do
 
   create_table "abuse_reports", :force => true do |t|
     t.integer  "reporter_id"
