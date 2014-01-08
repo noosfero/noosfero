fde04c8897295d73bc83e5bf96bade41620fbfcf
Rodrigo Souto <rodrigo@colivre.coop.br>
2014-01-08 09:41:59 -0300

    custom-forms: remove unnecessary length validation of select_field

--- plugins/custom_forms/lib/custom_forms_plugin/select_field.rb
+++ plugins/custom_forms/lib/custom_forms_plugin/select_field.rb
@@ -1,8 +1,5 @@
 class CustomFormsPlugin::SelectField < CustomFormsPlugin::Field
   set_table_name :custom_forms_plugin_fields
-
-  validates_length_of :select_field_type, :minimum => 1, :allow_nil => false
   validates_inclusion_of :select_field_type, :in => %w(radio check_box select multiple_select)
-
   validates_length_of :alternatives, :minimum => 1, :message => 'can\'t be empty'
 end
