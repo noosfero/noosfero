bf559b5fee8f679ed2038bbc339339f4f5c56c53
Rodrigo Souto <rodrigo@colivre.coop.br>
2014-01-20 19:21:05 +0000

    box: pass holder class as type for extra blocks hotspot
    (ActionItem2958)

--- test/unit/box_test.rb
+++ test/unit/box_test.rb
@@ -119,4 +119,25 @@ class BoxTest < ActiveSupport::TestCase
     assert blocks.include?('box-test_plugin-block')
   end
 
+  should 'list plugin block as allowed for the right holder' do
+    class SomePlugin < Noosfero::Plugin
+      def self.extra_blocks
+        { PluginBlock => {:position => 1, :type => [Person, Enterprise]} }
+      end
+    end
+    class PluginBlock < Block
+      def self.to_s; 'plugin-block'; end
+    end
+    Noosfero::Plugin::Manager.any_instance.stubs(:enabled_plugins).returns([SomePlugin.new])
+
+    blocks = Box.new(:position => 1, :owner => Person.new).acceptable_blocks
+    assert blocks.include?('box-test_plugin-block')
+
+    blocks = Box.new(:position => 1, :owner => Enterprise.new).acceptable_blocks
+    assert blocks.include?('box-test_plugin-block')
+
+    blocks = Box.new(:position => 1, :owner => Community.new).acceptable_blocks
+    assert !blocks.include?('box-test_plugin-block')
+  end
+
 end
