171bb6e3aec1eb3ae3783cc116bc329fd6ea1064
Victor Costa <vfcosta@gmail.com>
2014-01-20 15:45:07 -0300

    Move uniqueness validation from vote_fu to vote plugin
    (ActionItem2786)

--- vendor/plugins/vote_fu/lib/models/vote.rb
+++ vendor/plugins/vote_fu/lib/models/vote.rb
@@ -12,6 +12,6 @@ class Vote < ActiveRecord::Base
   attr_accessible :vote, :voter, :voteable
 
   # Uncomment this to limit users to a single vote on each item. 
-  validates_uniqueness_of :voteable_id, :scope => [:voteable_type, :voter_type, :voter_id]
+  #validates_uniqueness_of :voteable_id, :scope => [:voteable_type, :voter_type, :voter_id]
 
 end
