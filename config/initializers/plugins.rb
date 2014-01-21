a27c733333342fabe121ae684f530e171f0cf24a
Victor Costa <vfcosta@gmail.com>
2014-01-21 16:52:04 -0300

    rails3: added plugins initialization file

---
+++ config/initializers/plugins.rb
@@ -0,0 +1,8 @@
+require 'noosfero/plugin'
+require 'noosfero/plugin/hot_spot'
+require 'noosfero/plugin/manager'
+require 'noosfero/plugin/active_record'
+require 'noosfero/plugin/mailer_base'
+require 'noosfero/plugin/settings'
+require 'noosfero/plugin/spammable'
+Noosfero::Plugin.init_system if $NOOSFERO_LOAD_PLUGINS
