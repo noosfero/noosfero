b140da91a877de5907223305cfa774eb9dac4b58
Victor Costa <vfcosta@gmail.com>
2014-01-24 11:11:16 -0300

    rails3: fix json ecape

---
+++ config/initializers/json_escape.rb
@@ -0,0 +1,9 @@
+class ActionView::Base
+  def json_escape(s)
+    #result = s.to_s.gsub('/', '\/')
+    result = CGI::escapeHTML(s)
+    s.html_safe? ? result.html_safe : result
+  end
+
+  alias j json_escape
+end
