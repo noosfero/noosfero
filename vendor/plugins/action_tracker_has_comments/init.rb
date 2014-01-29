79d66909685530f4fab929e547e3ae1af0fefeff
Rodrigo Souto <rodrigo@colivre.coop.br>
2014-01-29 20:32:15 +0000

    comment: remove comment_as_thread method
    This method was removed by ca5ebf95375cd5dbb80a047dc79f4c31e36df899
    and was mistakenly included on this merge
    4ad91064632c8c1f5483a559d158d96b93e1a3c6 due to this fix
    70c90e476a2a263639d13654564b8ea9db30a911. Also its tests weren't
    removed.

--- vendor/plugins/action_tracker_has_comments/init.rb
+++ vendor/plugins/action_tracker_has_comments/init.rb
@@ -11,23 +11,5 @@ Rails.configuration.to_prepare do
       type, id = (self.target_type == 'Article' ? ['Article', self.target_id] : [self.class.to_s, self.id])
       "source_type = '#{type}' AND source_id = '#{id}' AND spam IS NOT TRUE AND reply_of_id IS NULL"
     end
-
-    def comments_as_thread
-      result = {}
-      root = []
-      self.comments.each do |c|
-        c.replies = []
-        result[c.id] ||= c
-        if c.reply_of_id.nil?
-          root << c
-        elsif result[c.reply_of_id]
-          result[c.reply_of_id].replies << c
-        else # Comment is a reply but the reply is not being displayed - is spam, for example
-          root << c
-        end
-      end
-      root
-    end
-
   end
 end
