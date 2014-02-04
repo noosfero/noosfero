e4246f5fc71ddd43709a6568cd4e53c2f6c4b3da
Victor Costa <vfcosta@gmail.com>
2014-02-04 11:08:57 -0300

    rails3: set allowed tags for sanitization
    Also enable white list monkey patch to unescape html comments.

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,12 @@ module Noosfero
 
     require 'noosfero/plugin'
 
+    # Adds custom attributes to the Set of allowed html attributes for the #sanitize helper
+    config.action_view.sanitized_allowed_attributes = 'align', 'border', 'alt', 'vspace', 'hspace', 'width', 'heigth', 'value', 'type', 'data', 'style', 'target', 'codebase', 'archive', 'classid', 'code', 'flashvars', 'scrolling', 'frameborder', 'controls', 'autoplay'
+
+    # Adds custom tags to the Set of allowed html tags for the #sanitize helper
+    config.action_view.sanitized_allowed_tags = 'object', 'embed', 'param', 'table', 'tr', 'th', 'td', 'applet', 'comment', 'iframe', 'audio', 'video', 'source'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
