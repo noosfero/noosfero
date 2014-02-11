27ef9853f4822acef608a2908feb895e90fea4bb
Braulio Bhavamitra <braulio@eita.org.br>
2014-02-11 18:02:19 -0300

    Don't duplicate themes

--- lib/noosfero/core_ext.rb
+++ lib/noosfero/core_ext.rb
@@ -1,4 +1,5 @@
 require 'noosfero/core_ext/string'
 require 'noosfero/core_ext/integer'
+require 'noosfero/core_ext/array'
 require 'noosfero/core_ext/object'
 require 'noosfero/core_ext/active_record'
