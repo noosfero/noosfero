749448447be27587cf819a2200ad5498f585710f
Joenio Costa <joenio@colivre.coop.br>
2014-03-31 21:09:02 -0300

    Mark string for translation
    (ActionItem3031)

--- app/helpers/boxes_helper.rb
+++ app/helpers/boxes_helper.rb
@@ -224,7 +224,7 @@ module BoxesHelper
 
       html = content_tag('div',
               content_tag('h2', _('Embed block code')) +
-              content_tag('div', 'Below, you''ll see a field containing embed code for the block. Just copy the code and paste it into your website or blogging software.', :style => 'margin-bottom: 1em;') +
+              content_tag('div', _('Below, you''ll see a field containing embed code for the block. Just copy the code and paste it into your website or blogging software.'), :style => 'margin-bottom: 1em;') +
               content_tag('textarea', block.embed_code(url), :style => 'margin-bottom: 1em; width:100%; height:40%;') +
               thickbox_close_button(_('Close')), :style => 'display: none;', :id => "embed-code-box-#{block.id}")
 
