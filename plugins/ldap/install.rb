b92f86327b85802f3b0352cd819feaee3f46312a
Rodrigo Souto <rodrigo@colivre.coop.br>
2014-04-07 15:18:04 +0000

    ldap-plugin: install gem only if not installed already

--- plugins/ldap/install.rb
+++ plugins/ldap/install.rb
@@ -1,4 +1,9 @@
-system "gem install --user-install net-ldap -v 0.3.1"
+begin
+  gem "net-ldap", '=0.3.1'
+rescue Gem::LoadError => exception
+  system "gem install --user-install net-ldap -v 0.3.1"
+end
+
 puts "\nWARNING: This plugin is not setting up a ldap test server automatically.
 Some tests may not be running. If you want to fully test this plugin, please
 setup the ldap test server and make the proper configurations on
