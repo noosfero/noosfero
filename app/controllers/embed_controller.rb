fe8dbbb43f893c21c3267d21be88d9ea0eacfc2d
Joenio Costa <joenio@colivre.coop.br>
2014-04-08 22:30:19 +0000

    Test files should ends with "_test.rb"
    also, moving views from app/views/shared to app/views/embed
    
    (ActionItem3031)

--- app/controllers/embed_controller.rb
+++ app/controllers/embed_controller.rb
@@ -3,12 +3,11 @@ class EmbedController < ApplicationController
 
   def block
     @block = Block.find(params[:id])
-    @source = params[:source]
     if !@block.embedable? || !@block.visible?
-      render :template => 'shared/embed_unavailable.rhtml', :status => 403
+      render 'unavailable.rhtml', :status => 403
     end
     rescue ActiveRecord::RecordNotFound
-      render :template => 'shared/embed_not_found.rhtml', :status => 404
+      render 'not_found.rhtml', :status => 404
   end
 
 end
