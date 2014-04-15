06accd14dd6c45d053247358e37f540328d84234
Rodrigo Souto <rodrigo@colivre.coop.br>
2014-04-15 14:50:14 +0000

    [template-welcome-page] Fix use of no_design_blocks on welcome action

--- app/controllers/public/home_controller.rb
+++ app/controllers/public/home_controller.rb
@@ -19,7 +19,7 @@ class HomeController < PublicController
   end
 
   def welcome
-    self.class.no_design_blocks
+    @no_design_blocks = true
     @display_confirmation_tips = !user.present? && !environment.enabled?(:skip_new_user_email_confirmation)
     @person_template = user && user.template || params[:template_id] && Person.find(params[:template_id])
     @template_welcome_page = @person_template && @person_template.welcome_page && @person_template.welcome_page.published ? @person_template.welcome_page.body : nil
