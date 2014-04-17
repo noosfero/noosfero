8ac35bfb3c0bff15480d8db83c249f96390ff4f1
Rodrigo Souto <rodrigo@colivre.coop.br>
2014-04-17 14:30:05 +0000

    Skip enterprise activiation if the task has no enterprise

--- db/migrate/20140318233831_finish_enterprise_activation_for_enabled_enterprises.rb
+++ db/migrate/20140318233831_finish_enterprise_activation_for_enabled_enterprises.rb
@@ -2,7 +2,7 @@ class FinishEnterpriseActivationForEnabledEnterprises < ActiveRecord::Migration
   def self.up
     EnterpriseActivation.find_each do |enterprise_activation|
       enterprise = enterprise_activation.enterprise
-      next unless enterprise.enabled
+      next unless enterprise.present? && enterprise.enabled
       enterprise_activation.update_attribute :status, Task::Status::FINISHED
     end
   end
