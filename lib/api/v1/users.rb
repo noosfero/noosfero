f14df8d379298ef381f5e3682bb5ff79b1ce359d
Victor Costa <vfcosta@gmail.com>
2014-05-16 17:27:31 -0300

    Removed trailing spaces from api

--- lib/api/v1/users.rb
+++ lib/api/v1/users.rb
@@ -1,23 +1,23 @@
 module API
   module V1
     class Users < Grape::API
-   
+
       before { authenticate! }
-  
+
       resource :users do
-  
+
         #FIXME make the pagination
         #FIXME put it on environment context
         get do
           present User.all, :with => Entities::User
         end
-   
+
         get ":id" do
           present User.find(params[:id]), :with => Entities::User
         end
-  
+
       end
-   
+
     end
   end
 end
