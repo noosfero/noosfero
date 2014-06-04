ea204f7e7e2abe58438b6c2226e57c1e6bbfafbb
Braulio Bhavamitra <braulio@eita.org.br>
2014-06-04 11:29:00 -0300

    application-helper: Change name to identify other types of profiles
    ActionItem3101

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1336,7 +1336,7 @@ module ApplicationHelper
       @message = _("The content here is available to %s's friends only.") % profile.short_name
     else
       @action = :join
-      @message = _('The contents in this community is available to members only.')
+      @message = _('The contents in this profile is available to members only.')
     end
     @no_design_blocks = true
   end
