11eaf495025a11223e3804839bf356f6c458e2b3
Larissa Reis <larissa@colivre.coop.br>
2014-06-13 22:46:19 -0300

    stoa-plugin-tests: fixes person_api tests
      attachment_fu doesn't provide #current_data for filesystem types of
      attachment anymore, so I added the method Image#current_data.
    
      Also fixes the column name that contains the tags' count for a sql
      query.

--- plugins/stoa/lib/stoa_plugin/person_api.rb
+++ plugins/stoa/lib/stoa_plugin/person_api.rb
@@ -28,7 +28,7 @@ class StoaPlugin::PersonApi < Noosfero::FieldsDecorator
   end
 
   def tags
-    articles.published.tag_counts({:order => 'tags.count desc', :limit => 10}).inject({}) do |memo,tag|
+    articles.published.tag_counts({:order => 'count desc', :limit => 10}).inject({}) do |memo,tag|
       memo[tag.name] = tag.count
       memo
     end
