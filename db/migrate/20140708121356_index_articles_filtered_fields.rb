832f268702a8bc614063f27ddb7bd085b60cd04c
Rodrigo Souto <rodrigo@colivre.coop.br>
2014-07-24 15:08:35 +0000

    Remove duplicate article parent_id index

--- db/migrate/20140708121356_index_articles_filtered_fields.rb
+++ db/migrate/20140708121356_index_articles_filtered_fields.rb
@@ -1,7 +1,6 @@
 class IndexArticlesFilteredFields < ActiveRecord::Migration
   def self.up
     %w[articles article_versions].each do |table|
-      add_index table, [:parent_id]
       add_index table, [:path]
       add_index table, [:path, :profile_id]
     end
