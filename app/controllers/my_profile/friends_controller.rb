fcad091d97e7e03a9f7ea161d4e69389fdc25d21
Daniela Feitosa <danielafeitosa@colivre.coop.br>
2014-07-26 20:36:29 -0300

    profile_suggestions: friends suggestions interface
    Users can:
       * add a suggestion as friend
       * remove a suggestion from list
    
    (ActionItem3234)

--- app/controllers/my_profile/friends_controller.rb
+++ app/controllers/my_profile/friends_controller.rb
@@ -3,6 +3,7 @@ class FriendsController < MyProfileController
   protect 'manage_friends', :profile
   
   def index
+    @suggestions = profile.suggested_people.limit(per_page/2)
     if is_cache_expired?(profile.manage_friends_cache_key(params))
       @friends = profile.friends.paginate(:per_page => per_page, :page => params[:npage])
     end
@@ -13,6 +14,21 @@ class FriendsController < MyProfileController
     if request.post? && params[:confirmation]
       profile.remove_friend(@friend)
       redirect_to :action => 'index'
+    end
+  end
+
+  def suggest
+    @suggestions = profile.suggested_people.paginate(:per_page => per_page, :page => params[:npage])
+  end
+
+  def remove_suggestion
+    @person = profile.suggested_people.find_by_identifier(params[:id])
+    redirect_to :action => 'suggest' unless @person
+    if @person && request.post?
+      suggestion = profile.profile_suggestions.find_by_suggestion_id @person.id
+      suggestion.disable
+      session[:notice] = _('Suggestion removed')
+      redirect_to :action => 'suggest'
     end
   end
 
