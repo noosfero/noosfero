e9371699d8dcb40f2230beae59d3d79a514231b1
Antonio Terceiro <terceiro@colivre.coop.br>
2014-07-31 13:58:03 -0300

    Remove mezuro plugin by request of its authors
    Mezuro is now a standalone application.

--- plugins/mezuro/test/fixtures/project_content_fixtures.rb
+++
@@ -1,9 +0,0 @@
-class ProjectContentFixtures
-
-  def self.project_content
-    content = MezuroPlugin::ProjectContent.new
-    content.project_id = 42
-    content
-  end
-
-end
--- plugins/mezuro/test/fixtures/reading_group_content_fixtures.rb
+++
@@ -1,9 +0,0 @@
-class ReadingGroupContentFixtures
-
-  def self.reading_group_content
-    content = MezuroPlugin::ReadingGroupContent.new
-    content.reading_group_id = 42
-    content
-  end
-
-end
