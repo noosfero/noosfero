3dda03d2e329287cf3eb7bb3ff1b333c54b57965
Victor Costa <vfcosta@gmail.com>
2014-08-08 12:40:37 -0300

    Added body and abstract to link article

--- app/models/link_article.rb
+++ app/models/link_article.rb
@@ -7,6 +7,8 @@ class LinkArticle < Article
   end
 
   delegate :name, :to => :reference_article
+  delegate :body, :to => :reference_article
+  delegate :abstract, :to => :reference_article
   delegate :url, :to => :reference_article
 
 end
