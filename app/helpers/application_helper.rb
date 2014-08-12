397d8a89847d24f4cb658826009b0bfc3fc3645c
Rodrigo Souto <rodrigo@colivre.coop.br>
2014-08-12 17:36:27 -0300

    profile-suggestions: tags overflow ellipsis

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1433,6 +1433,6 @@ module ApplicationHelper
     end
     last_tag = tags.pop
     tags << last_tag.strip.chop if last_tag.present?
-    content_tag(:div, tags.join, :class => 'tag-connections')
+    content_tag(:div, tags.join, :class => 'tag-connections', :title => tags.join)
   end
 end
