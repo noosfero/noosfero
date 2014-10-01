18aeacba86d38748a4e834a3c9f2c83ec5aafd46
Braulio Bhavamitra <braulio@eita.org.br>
2014-10-01 09:00:17 -0300

    Update to tinymce 4

---
+++ public/javascripts/tinymce/js/tinymce/plugins/etherpadlite/langs/en_dlg.js
@@ -0,0 +1,11 @@
+tinyMCE.addI18n('en.etherpadlite_dlg',{
+    title : "Insert a collaborative text pad",
+    instr : "If you already know a pad, put its name. If not, leave the name blank.",
+    epID : "Pad name<br><i>(leave it blank if you're creating a new pad)</i>",
+    chat : "Enable chat?", 
+    alwaysChat : "Always show chat?", 
+    epW : "Width",
+    epH : "Height",
+    yes:"Yes",
+    no:"No"
+});
