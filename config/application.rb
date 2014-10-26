36442057d5bc168a5fbc841d62391b0836ca5682
Hugo Melo <hugo@riseup.net>
2014-10-26 20:32:39 -0200

    Set default timezone as needed by rails 3.2
    Without this, rails won't convert back to system timezone when reading
    timestamṕ/datatime fields from database.
    
    https://github.com/rails/rails/issues/6816

--- config/application.rb
+++ config/application.rb
@@ -115,6 +115,9 @@ module Noosfero
       :key    => '_noosfero_session',
     }
 
+    config.time_zone = File.read('/etc/timezone').split("\n").first
+    config.active_record.default_timezone = :local
+
     config.i18n.load_path += Dir.glob "#{Rails.root}/{baseplugins,config/plugins/*}/locales/*.{rb,yml}"
 
     Noosfero::Plugin.setup(config)
