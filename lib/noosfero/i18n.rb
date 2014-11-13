da3b4c3f71f0197b50524e2a617b547087251c67
Larissa Reis <larissa@colivre.coop.br>
2014-11-13 00:47:51 -0300

    Adds support for domain specific custom locales
      Support for custom locales for hosted environments using the
      multitenancy support and noosfero domains. Some environments need
      personalized translations and this hopefully offers a clean way to do
      it based on domains as well.
    
      Related to MR 195
    
    (ActionItem3113)

--- lib/noosfero/i18n.rb
+++ lib/noosfero/i18n.rb
@@ -7,6 +7,7 @@ class Object
 end
 
 
+# Adds custom locales for a whole environment
 custom_locale_dir = Rails.root.join('custom_locales', Rails.env)
 repos = []
 if File.exists?(custom_locale_dir)
@@ -22,3 +23,15 @@ end
 
 FastGettext.add_text_domain 'noosfero', :type => :chain, :chain => repos
 FastGettext.default_text_domain = 'noosfero'
+
+# Adds custom locales for specific domains; Domains are identified by the
+# sequence before the first dot, while tenants are identified by schema name
+hosted_environments = Noosfero::MultiTenancy.mapping.values
+hosted_environments += Domain.all.map { |domain| domain.name[/(.*?)\./,1] } if Domain.table_exists?
+
+hosted_environments.uniq.each do |env|
+  custom_locale_dir = Rails.root.join('custom_locales', env)
+  if File.exists?(custom_locale_dir)
+    FastGettext.add_text_domain(env, :type => :chain, :chain => [FastGettext::TranslationRepository.build('environment', :type => 'po', :path => custom_locale_dir)] + repos)
+  end
+end
