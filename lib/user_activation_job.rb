e9da52f6c0d6eeab5baac93f5bab2e08e3282975
Daniela Feitosa <danielafeitosa@colivre.coop.br>
2014-12-02 19:14:49 -0300

    Prevent templates from being destroyed
    Check if is template before removing an inactive user

--- lib/user_activation_job.rb
+++ lib/user_activation_job.rb
@@ -1,6 +1,6 @@
 class UserActivationJob < Struct.new(:user_id)
   def perform
     user = User.find(user_id)
-    user.destroy unless user.activated?
+    user.destroy unless user.activated? || user.person.is_template?
   end
 end
