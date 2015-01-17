b853d6c3eb2ec5b044b0d29a047ff65fc3368c0b
Larissa Reis <larissa@colivre.coop.br>
2015-01-16 23:13:23 -0300

    [profile-suggestions] Fixes people_block test and adds a couple more
      `instance_eval` in FriendsBlockTest chokes on `user` so we avoid this
      `people_block` test from crashing by checking if suggestions exist
      first. Therefore this unit test is only testing for a view all link,
      so I wrote a couple more tests to test the added suggestion feature
      inside the block.
    
      I wasn't sure where to put a test to actually test that the
      suggestions are being rendered so I created a ProfileControllerTest,
      but I'm open to suggestions on where it would be best :)

--- plugins/people_block/test/unit/friends_block_test.rb
+++ plugins/people_block/test/unit/friends_block_test.rb
@@ -6,71 +6,59 @@ class FriendsBlockTest < ActionView::TestCase
     assert_kind_of Block, FriendsBlock.new
   end
 
-
   should 'declare its default title' do
     FriendsBlock.any_instance.expects(:profile_count).returns(0)
     assert_not_equal Block.new.default_title, FriendsBlock.new.default_title
   end
 
-
   should 'describe itself' do
     assert_not_equal Block.description, FriendsBlock.description
   end
-
 
   should 'is editable' do
     block = FriendsBlock.new
     assert block.editable?
   end
 
-
   should 'have field limit' do
     block = FriendsBlock.new
     assert_respond_to block, :limit
   end
-
 
   should 'default value of limit' do
     block = FriendsBlock.new
     assert_equal 6, block.limit
   end
 
-
   should 'have field name' do
     block = FriendsBlock.new
     assert_respond_to block, :name
   end
-
 
   should 'default value of name' do
     block = FriendsBlock.new
     assert_equal "", block.name
   end
 
-
   should 'have field address' do
     block = FriendsBlock.new
     assert_respond_to block, :address
   end
-
 
   should 'default value of address' do
     block = FriendsBlock.new
     assert_equal "", block.address
   end
 
-
   should 'prioritize profiles with image by default' do
     assert FriendsBlock.new.prioritize_profiles_with_image
   end
-
 
   should 'accept a limit of people to be displayed' do
     block = FriendsBlock.new
     block.limit = 20
     assert_equal 20, block.limit
   end
-
 
   should 'list friends from person' do
     owner = fast_create(Person)
@@ -92,11 +80,11 @@ class FriendsBlockTest < ActionView::TestCase
     assert_match(/#{friend2.name}/, content)
   end
 
-
   should 'link to "all friends"' do
     person1 = create_user('mytestperson').person
 
     block = FriendsBlock.new
+    block.stubs(:suggestions).returns([])
     block.expects(:owner).returns(person1).at_least_once
 
     instance_eval(&block.footer)
@@ -104,7 +92,6 @@ class FriendsBlockTest < ActionView::TestCase
       assert_select '[href=/profile/mytestperson/friends]'
     end
   end
-
 
   should 'count number of owner friends' do
     owner = fast_create(Person)
@@ -121,7 +108,6 @@ class FriendsBlockTest < ActionView::TestCase
     assert_equal 3, block.profile_count
   end
 
-
   should 'count number of public and private friends' do
     owner = fast_create(Person)
     private_p = fast_create(Person, {:public_profile => false})
@@ -136,7 +122,6 @@ class FriendsBlockTest < ActionView::TestCase
     assert_equal 2, block.profile_count
   end
 
-
   should 'not count number of invisible friends' do
     owner = fast_create(Person)
     private_p = fast_create(Person, {:visible => false})
@@ -149,6 +134,17 @@ class FriendsBlockTest < ActionView::TestCase
     block.expects(:owner).returns(owner).at_least_once
 
     assert_equal 1, block.profile_count
+  end
+
+  should 'list owner\'s friends suggestions' do
+    owner = fast_create(Person)
+    suggestion1 = owner.profile_suggestions.create(:suggestion => fast_create(Person))
+    suggestion2 = owner.profile_suggestions.create(:suggestion => fast_create(Person))
+
+    block = FriendsBlock.new
+    block.stubs(:owner).returns(owner)
+
+    assert_equivalent block.suggestions, [suggestion1,suggestion2]
   end
 
   protected
