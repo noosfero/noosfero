7896182670ba1ef80e4f08873f57a199c4db54fe
Victor Costa <vfcosta@gmail.com>
2015-01-19 16:49:14 -0300

    Use default base class for plugin admin controllers
    (cherry picked from commit e47daca26c5861d09c8248855472d4e65cbda1d0)

--- plugins/anti_spam/controllers/anti_spam_plugin_admin_controller.rb
+++ plugins/anti_spam/controllers/anti_spam_plugin_admin_controller.rb
@@ -1,4 +1,4 @@
-class AntiSpamPluginAdminController < AdminController
+class AntiSpamPluginAdminController < PluginAdminController
   append_view_path File.join(File.dirname(__FILE__) + '/../views')
 
   def index
