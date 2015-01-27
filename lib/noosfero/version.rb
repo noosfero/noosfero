ba1b25b93c0e69312b1c720ee4149d7dbc3d4f7a
Antonio Terceiro <terceiro@colivre.coop.br>
2015-01-27 14:04:44 -0200

    Noosfero::VERSION: use git data when available
    For code in development, this produces version numbers like
    "1.0-119-g124a746", which is useful when running from git. Those
    running Noosfero forks can even make their own tags and get version
    numbers like "1.0+vendor1".

--- lib/noosfero/version.rb
+++ lib/noosfero/version.rb
@@ -2,3 +2,7 @@ module Noosfero
   PROJECT = 'noosfero'
   VERSION = '1.0'
 end
+
+if File.exist?(File.join(Rails.root, '.git'))
+  Noosfero::VERSION.clear << Dir.chdir(Rails.root) { `git describe --tags`.strip }
+end
