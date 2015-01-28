cd62616e98be44029abca46164f42dd4bd48353f
Antonio Terceiro <terceiro@colivre.coop.br>
2015-01-28 09:59:20 -0200

    noosfero_urls: skip standard HTTP/HTTPS ports

--- config/initializers/noosfero_urls.rb
+++ config/initializers/noosfero_urls.rb
@@ -1,5 +1,13 @@
 if Rails.env == 'development'
   ActionController::Base.send(:prepend_before_filter) do |controller|
-    Noosfero.instance_variable_set('@development_url_options', { :port => controller.request.port })
+    # XXX note that this is not thread-safe! Accessing a Noosfero instance in
+    # development mode under different ports concurrently _will_ lead to weird
+    # things happening.
+    if [80,443].include?(controller.request.port)
+      url_options = {}
+    else
+      url_options = { :port => controller.request.port }
+    end
+    Noosfero.instance_variable_set('@development_url_options', url_options)
   end
 end
