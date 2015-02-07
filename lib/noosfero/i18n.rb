1cbfc9376fbe73615e63e140bb3ca63bfd02b9e9
Daniela Feitosa <danielafeitosa@colivre.coop.br>
2015-02-07 10:22:52 -0300

    Adds support for domain specific custom locales
    Patch for commits:
    da3b4c3f71f0197b50524e2a617b547087251c67
    ece6c5630ff06d35f466c7fc0cdbb2296a23c587
    b6108f3c8d9033375aaa5c5fce0d71bc839dad21
    
    See merge request !376

--- lib/noosfero/i18n.rb
+++ lib/noosfero/i18n.rb
@@ -7,7 +7,8 @@ class Object
 end
 
 
-custom_locale_dir = Rails.root.join('custom_locales', Rails.env)
+# Adds custom locales for a whole environment
+custom_locale_dir = Rails.root.join('config', 'custom_locales', Rails.env)
 repos = []
 if File.exists?(custom_locale_dir)
   repos << FastGettext::TranslationRepository.build('environment', :type => 'po', :path => custom_locale_dir)
@@ -22,3 +23,15 @@ end
 
 FastGettext.add_text_domain 'noosfero', :type => :chain, :chain => repos
 FastGettext.default_text_domain = 'noosfero'
+
+# Adds custom locales for specific domains; Domains are identified by the
+# sequence before the first dot, while tenants are identified by schema name
+hosted_environments = Noosfero::MultiTenancy.mapping.values
+hosted_environments += Domain.all.map { |domain| domain.name[/(.*?)\./,1] } if Domain.table_exists?
+
+hosted_environments.uniq.each do |env|
+  custom_locale_dir = Rails.root.join('config', 'custom_locales', env)
+  if File.exists?(custom_locale_dir)
+    FastGettext.add_text_domain(env, :type => :chain, :chain => [FastGettext::TranslationRepository.build('environment', :type => 'po', :path => custom_locale_dir)] + repos)
+  end
+end
