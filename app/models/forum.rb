coletivoEITA/noosfero-ecosol

Drop and replace hpricot with nokogiri

app/models/forum.rb
@@ -54,7 +54,7 @@ class Forum < Folder
 
   def first_paragraph
     return '' if body.blank?
-    paragraphs = Hpricot(body).search('p')
+    paragraphs = Nokogiri::HTML.fragment(body).search('p')
     paragraphs.empty? ? '' : paragraphs.first.to_html
   end
 

plugins/comment_group/lib/ext/article.rb
@@ -9,7 +9,7 @@ class Article
   def not_empty_group_comments_removed
     if body && body_changed?
       groups_with_comments = Comment.find(:all, :select => 'distinct group_id', :conditions => {:source_id => self.id}).map(&:group_id).compact
-      groups = Hpricot(body.to_s).search('.macro').collect{|element| element['data-macro-group_id'].to_i}
+      groups = Nokogiri::HTML.fragment(body.to_s).search('.macro').collect{|element| element['data-macro-group_id'].to_i}
       errors[:base] << (N_('Not empty group comment cannot be removed')) unless (groups_with_comments-groups).empty?
     end
   end

plugins/community_track/lib/community_track_plugin/track.rb
@@ -59,7 +59,7 @@ class CommunityTrackPlugin::Track < Folder
 
   def first_paragraph
     return '' if body.blank?
-    paragraphs = Hpricot(body).search('p')
+    paragraphs = Nokogiri::HTML.fragment(body).search('p')
     paragraphs.empty? ? '' : paragraphs.first.to_html
   end
 

test/unit/comment_helper_test.rb
@@ -137,7 +137,7 @@ class CommentHelperTest < ActiveSupport::TestCase
     plugin_action = {:link => 'plugin_action', :action_bar => true}
     @plugins.stubs(:dispatch).returns([plugin_action])
     html = comment_actions(comment)
-    assert_match /plugin_action/, Hpricot(html).search('.comments-action-bar').html
+    assert_match /plugin_action/, Nokogiri::HTML.fragment(html).search('.comments-action-bar').to_html
   end
 
   def link_to_function(content, url, options = {})

test/unit/macro_test.rb
@@ -15,7 +15,7 @@ class MacroTest < ActiveSupport::TestCase
 
   def setup
     @macro = Plugin1::Macro.new
-    @macro_element = Hpricot(MACRO).search('.macro').first
+    @macro_element = Nokogiri::HTML.fragment(MACRO).search('.macro').first
   end
 
   attr_reader :macro, :macro_element

noosfero/noosfero

app/models/forum.rb
@@ -54,7 +54,7 @@ class Forum < Folder
 
   def first_paragraph
     return '' if body.blank?
-    paragraphs = Hpricot(body).search('p')
+    paragraphs = Nokogiri::HTML.fragment(body).css('p')
     paragraphs.empty? ? '' : paragraphs.first.to_html
   end
 

plugins/comment_group/lib/ext/article.rb
@@ -9,7 +9,7 @@ class Article
   def not_empty_group_comments_removed
     if body && body_changed?
       groups_with_comments = Comment.find(:all, :select => 'distinct group_id', :conditions => {:source_id => self.id}).map(&:group_id).compact
-      groups = Hpricot(body.to_s).search('.macro').collect{|element| element['data-macro-group_id'].to_i}
+      groups = Nokogiri::HTML.fragment(body.to_s).css('.macro').collect{|element| element['data-macro-group_id'].to_i}
       errors[:base] << (N_('Not empty group comment cannot be removed')) unless (groups_with_comments-groups).empty?
     end
   end

plugins/community_track/lib/community_track_plugin/track.rb
@@ -59,7 +59,7 @@ class CommunityTrackPlugin::Track < Folder
 
   def first_paragraph
     return '' if body.blank?
-    paragraphs = Hpricot(body).search('p')
+    paragraphs = Nokogiri::HTML.fragment(body).css('p')
     paragraphs.empty? ? '' : paragraphs.first.to_html
   end
 

test/unit/comment_helper_test.rb
@@ -137,7 +137,7 @@ class CommentHelperTest < ActiveSupport::TestCase
     plugin_action = {:link => 'plugin_action', :action_bar => true}
     @plugins.stubs(:dispatch).returns([plugin_action])
     html = comment_actions(comment)
-    assert_match /plugin_action/, Hpricot(html).search('.comments-action-bar').html
+    assert_match /plugin_action/, Nokogiri::HTML.fragment(html).css('.comments-action-bar').to_html
   end
 
   def link_to_function(content, url, options = {})

test/unit/macro_test.rb
@@ -15,7 +15,7 @@ class MacroTest < ActiveSupport::TestCase
 
   def setup
     @macro = Plugin1::Macro.new
-    @macro_element = Hpricot(MACRO).search('.macro').first
+    @macro_element = Nokogiri::HTML.fragment(MACRO).css('.macro').first
   end
 
   attr_reader :macro, :macro_element