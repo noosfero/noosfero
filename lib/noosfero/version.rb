b982d5e8a930693fc0edd181cbda552be5ab433c
Antonio Terceiro <terceiro@colivre.coop.br>
2015-03-09 12:19:00 -0300

    Noosfero::VERSION: normalize RC versions from git tags

--- lib/noosfero/version.rb
+++ lib/noosfero/version.rb
@@ -5,5 +5,5 @@ end
 
 root = File.expand_path(File.dirname(__FILE__) + '/../..')
 if File.exist?(File.join(root, '.git'))
-  Noosfero::VERSION.clear << Dir.chdir(root) { `git describe --tags`.strip }
+  Noosfero::VERSION.clear << Dir.chdir(root) { `git describe --tags`.strip.sub('-rc', '~rc') }
 end
