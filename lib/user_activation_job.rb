e8e5c33a860c54923249127da4c1899e9c5de5d4
Victor Costa <vfcosta@gmail.com>
2015-03-16 10:28:31 -0300

    Do not destroy an user that has a pending ModerateUserRegistration task

--- lib/user_activation_job.rb
+++ lib/user_activation_job.rb
@@ -1,6 +1,6 @@
 class UserActivationJob < Struct.new(:user_id)
   def perform
     user = User.find(user_id)
-    user.destroy unless user.activated? || user.person.is_template?
+    user.destroy unless user.activated? || user.person.is_template? || user.moderate_registration_pending?
   end
 end
