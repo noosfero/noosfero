da29b595e64d29a3a032095b7457c9ebbc972034
Braulio Bhavamitra <braulio@eita.org.br>
2015-03-18 16:38:10 -0300

    pg_search: fix down on migration

--- plugins/pg_search/db/migrate/20130320010063_create_indexes_for_search.rb
+++ plugins/pg_search/db/migrate/20130320010063_create_indexes_for_search.rb
@@ -1,15 +1,15 @@
 class CreateIndexesForSearch < ActiveRecord::Migration
+  SEARCHABLES = %w[ article comment qualifier national_region certifier profile license scrap category ]
+  KLASSES = SEARCHABLES.map {|searchable| searchable.camelize.constantize }
   def self.up
-    searchables = %w[ article comment qualifier national_region certifier profile license scrap category ]
-    klasses = searchables.map {|searchable| searchable.camelize.constantize }
-    klasses.each do |klass|
+    KLASSES.each do |klass|
       fields = klass.pg_search_plugin_fields
       execute "create index pg_search_plugin_#{klass.name.singularize.downcase} on #{klass.table_name} using gin(to_tsvector('simple', #{fields}))"
     end
   end
 
   def self.down
-    klasses.each do |klass|
+    KLASSES.each do |klass|
       execute "drop index pg_search_plugin_#{klass.name.singularize.downcase}"
     end
   end
