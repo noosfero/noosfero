8c1e8d15ad4f8b8d69ba2465a0e722722bd0c519
Leandro Nunes dos Santos <leandronunes@gmail.com>
2015-03-24 20:39:16 -0300

    api: fixing conflit on merge

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -100,12 +100,12 @@ class Profile < ActiveRecord::Base
   scope :no_templates, {:conditions => {:is_template => false}}
 
   #FIXME make this test
-  named_scope :newer_than, lambda { |reference_id|
+  scope :newer_than, lambda { |reference_id|
     {:conditions => ["profiles.id > #{reference_id}"]}
   }
 
   #FIXME make this test
-  named_scope :older_than, lambda { |reference_id|
+  scope :older_than, lambda { |reference_id|
     {:conditions => ["profiles.id < #{reference_id}"]}
   }
 
