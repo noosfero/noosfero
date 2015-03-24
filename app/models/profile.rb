59425a1882c2daa6b128957bce3692fe29fcefc6
Leandro Nunes dos Santos <leandronunes@gmail.com>
2015-03-23 23:13:16 -0300

    adding functional testes for communities endpoint

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -149,7 +149,7 @@ class Profile < ActiveRecord::Base
     joins('LEFT JOIN "role_assignments" ON "role_assignments"."resource_id" = "profiles"."id" AND "role_assignments"."resource_type" = \'Profile\'')
     .where(
       ['( ( role_assignments.accessor_type = ? AND role_assignments.accessor_id = ? ) OR 
-          (profiles.public_profile = ?) )', Profile.name, person.id,  true]
+          (profiles.public_profile = ?)) AND (profiles.visible = ?)', Profile.name, person.id,  true, true]
     ).uniq
   }
 
