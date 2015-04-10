6dc1f4d51c3384ac8c1fffb2663e8b99147e3439
Braulio Bhavamitra <braulio@eita.org.br>
2015-04-10 16:30:50 -0300

    responsive: add plugin and base theme

---
+++ plugins/responsive/lib/ext/boxes_helper.rb
@@ -0,0 +1,79 @@
+require_dependency 'boxes_helper'
+require_relative 'application_helper'
+
+module BoxesHelper
+
+  extend ActiveSupport::Concern
+  protected
+
+  module ResponsiveMethods
+    def insert_boxes(content)
+      return super unless theme_responsive?
+
+      if controller.send(:boxes_editor?) && controller.send(:uses_design_blocks?)
+        content + display_boxes_editor(controller.boxes_holder)
+      else
+        maybe_display_custom_element(controller.boxes_holder, :custom_header_expanded, id: 'profile-header') +
+          if controller.send(:uses_design_blocks?)
+            display_boxes(controller.boxes_holder, content)
+        else
+          content_tag(:div,
+                      content_tag('div',
+                                  content_tag('div',
+                                              content_tag('div', wrap_main_content(content), class: 'no-boxes-inner-2'),
+                                              class: 'no-boxes-inner-1'
+                                             ),
+                                             class: 'no-boxes col-lg-12 col-md-12 col-sm-12'
+                                 ),
+                                 class: 'row',
+                                 id: 'content')
+        end +
+        content_tag('div',
+          maybe_display_custom_element(controller.boxes_holder, :custom_footer_expanded, id: 'profile-footer'),
+          :class => 'row')
+      end
+    end
+
+    def display_boxes holder, main_content
+      return super unless theme_responsive?
+
+      boxes = holder.boxes.with_position.order('boxes.position ASC').first(boxes_limit(holder))
+
+      template = profile.nil? ? environment.layout_template : profile.layout_template
+      template = 'default' if template.blank?
+
+      return main_content unless boxes.present?
+      render partial: "templates/boxes_#{template}", locals: {boxes: boxes, main_content: main_content}, use_cache: use_cache?
+    end
+  end
+
+  def display_topbox_content(box, main_content)
+    context = {article: @page, request_path: request.path, locale: locale, params: request.params, controller: controller}
+    box_decorator.select_blocks(box, box.blocks.includes(:box), context).map do |item|
+      if item.class.name == 'LinkListBlock' and request.params[:controller] != 'profile_design'
+        render_linklist_navbar(item)
+      else
+        display_block item, main_content
+      end
+    end.join("\n") + box_decorator.block_target(box)
+  end
+
+  def render_linklist_navbar link_list
+    list = link_list.links.select{ |i| i[:name].present? and i[:address].present? }
+    render file: 'blocks/link_list_navbar', locals: {block: link_list, links: list}
+  end
+
+  include ResponsiveChecks
+  extend ActiveSupport::Concern
+  included do
+    include ResponsiveMethods
+  end
+
+end
+
+module ApplicationHelper
+
+  include BoxesHelper::ResponsiveMethods
+
+end
+
---
+++ plugins/responsive/public/javascripts/sign_up_password_rate.js
@@ -0,0 +1,121 @@
+// This jQuery plugin is written by firas kassem [2007.04.05] and was modified to fit noosfero
+// Firas Kassem  phiras.wordpress.com || phiras at gmail {dot} com
+// for more information : http://phiras.wordpress.com/2007/04/08/password-strength-meter-a-jquery-plugin/
+
+var blankPass    = -1
+var shortPass   = 0
+var badPass     = 1
+var goodPass    = 2
+var strongPass  = 3
+
+
+function passwordStrength(password,username)
+{
+  score = 0
+
+  if(password.length == 0) return blankPass
+
+  //password < 4
+  if (password.length < 4 ) { return shortPass }
+
+  //password == username
+  if (password.toLowerCase()==username.toLowerCase()) badPass
+
+  //password length
+  score += password.length * 4
+  score += ( checkRepetition(1,password).length - password.length ) * 1
+  score += ( checkRepetition(2,password).length - password.length ) * 1
+  score += ( checkRepetition(3,password).length - password.length ) * 1
+  score += ( checkRepetition(4,password).length - password.length ) * 1
+
+  //password has 3 numbers
+  if (password.match(/(.*[0-9].*[0-9].*[0-9])/))  score += 5
+
+  //password has 2 sybols
+  if (password.match(/(.*[!,@,#,$,%,^,&,*,?,_,~].*[!,@,#,$,%,^,&,*,?,_,~])/)) score += 5
+
+  //password has Upper and Lower chars
+  if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/))  score += 10
+
+  //password has number and chars
+  if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/))  score += 15
+  //
+  //password has number and symbol
+  if (password.match(/([!,@,#,$,%,^,&,*,?,_,~])/) && password.match(/([0-9])/))  score += 15
+
+  //password has char and symbol
+  if (password.match(/([!,@,#,$,%,^,&,*,?,_,~])/) && password.match(/([a-zA-Z])/))  score += 15
+
+  //password is just a nubers or chars
+  if (password.match(/^\w+$/) || password.match(/^\d+$/) )  score -= 10
+
+  //verifing 0 < score < 100
+  if ( score < 0 )  score = 0
+  if ( score > 100 )  score = 100
+
+  if (score < 34 )  return badPass
+  if (score < 68 )  return goodPass
+  return strongPass
+}
+
+function checkRepetition(pLen,str)
+{
+  res = ""
+  for ( i=0; i<str.length ; i++ )
+  {
+      repeated=true
+      for (j=0;j < pLen && (j+i+pLen) < str.length;j++)
+          repeated=repeated && (str.charAt(j+i)==str.charAt(j+i+pLen))
+      if (j<pLen) repeated=false
+      if (repeated)
+      {
+          i+=pLen-1
+          repeated=false
+      }
+      else
+      {
+          res+=str.charAt(i)
+      }
+  }
+  return res
+}
+
+jQuery(document).ready(function() {
+    jQuery('#user_pw').keyup(function()
+    {
+      var result = passwordStrength(jQuery('#user_pw').val(),jQuery('#user_login').val())
+      if(result == blankPass)
+      {
+        showRateField('#result-blank')
+      } else
+      if(result == shortPass)
+      {
+        showRateField('#result-short')
+      } else
+      if( result == badPass )
+      {
+        showRateField('#result-bad')
+      } else
+      if( result == goodPass )
+      {
+        showRateField('#result-good')
+      } else
+      if( result == strongPass )
+      {
+        showRateField('#result-strong')
+      }
+
+    })
+})
+
+function showRateField(validation)
+{
+  jQuery('#result-blank').addClass('hidden')
+  jQuery('#result-short').addClass('hidden')
+  jQuery('#result-bad').addClass('hidden')
+  jQuery('#result-good').addClass('hidden')
+  jQuery('#result-strong').addClass('hidden')
+
+  jQuery(validation).removeClass('hidden')
+
+}
