9fd165ce461f45fe9e4d33fb7a05e5c27ca7df27
Daniela Feitosa <danielafeitosa@colivre.coop.br>
2015-04-13 14:45:43 -0300

    fixes for google_analytics_plugin
    - allow mass-assignment
    - Replaced .rhtml by .html.erb
    - updated script code

--- plugins/google_analytics/lib/ext/profile.rb
+++ plugins/google_analytics/lib/ext/profile.rb
@@ -2,4 +2,9 @@ require_dependency 'profile'
 
 class Profile
   settings_items :google_analytics_profile_id
+  attr_accessible :google_analytics_profile_id
+
+  descendants.each do |descendant|
+    descendant.attr_accessible :google_analytics_profile_id
+  end
 end
