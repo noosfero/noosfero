a9b65794ba867b09a1b2c5453076da03b5ea49c2
Victor Costa <vfcosta@gmail.com>
2015-04-13 13:41:02 -0300

    vote_plugin: allow anonymous votes

--- lib/noosfero/vote_ext.rb
+++ lib/noosfero/vote_ext.rb
@@ -2,6 +2,10 @@ require_dependency 'models/vote'
 
 class Vote
 
-  validates_uniqueness_of :voteable_id, :scope => [:voteable_type, :voter_type, :voter_id]
+  validates_uniqueness_of :voteable_id, :scope => [:voteable_type, :voter_type, :voter_id], :if => :allow_duplicated_vote?
+
+  def allow_duplicated_vote?
+    voter.present?
+  end
 
 end
