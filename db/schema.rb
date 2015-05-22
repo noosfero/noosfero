db6ca267b0488e03226b48fe489c626c454c30b2
Daniela Feitosa <danielafeitosa@colivre.coop.br>
2015-05-22 10:39:54 -0300

    Updated db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150423144533) do
+ActiveRecord::Schema.define(:version => 20150513213939) do
 
   create_table "abuse_reports", :force => true do |t|
     t.integer  "reporter_id"
