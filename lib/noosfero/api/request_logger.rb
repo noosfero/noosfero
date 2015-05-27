4ddf407d646d952f6bc3501c92327a68a861fb8c
Victor Costa <vfcosta@gmail.com>
2015-05-27 11:44:03 -0300

    api: fix logger

--- lib/noosfero/api/request_logger.rb
+++ lib/noosfero/api/request_logger.rb
@@ -7,10 +7,10 @@ module Noosfero
       def parameters(response, duration)
         {
           path: request.path,
-          params: request.params.except('password'),
+          params: request.params.to_hash.except('password'),
           method: request.request_method,
-          total: (duration * 1000).round(2),
-          db: request.env[:db_duration].round(2),
+          total: total_runtime,
+          db: @db_duration.round(2),
         }
       end
     end
