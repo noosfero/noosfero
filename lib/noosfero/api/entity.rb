6b50511ede002bf8bead925786826dfa0b1ab53d
Aurélio A. Heckert <aurelio@colivre.coop.br>
2015-06-13 01:29:40 +0000

    API response with error block

--- lib/noosfero/api/entity.rb
+++ lib/noosfero/api/entity.rb
@@ -1,5 +1,26 @@
 class Noosfero::API::Entity < Grape::Entity
 
+  def initialize(object, options = {})
+    object = nil if object.is_a? Exception
+    super object, options
+  end
+
+  def self.represent(objects, options = {})
+    if options[:is_inner_data]
+      super objects, options
+    else
+      data = super objects, options.merge(is_inner_data: true)
+      if objects.is_a? Exception
+        data.merge ok: false, error: {
+          type: objects.class.name,
+          message: objects.message
+        }
+      else
+        data.merge ok: true, error: { type: 'Success', message: '' }
+      end
+    end
+  end
+
   def self.fields_condition(fields)
     lambda do |object, options|
       return true if options[:fields].blank?
