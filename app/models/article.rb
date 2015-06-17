53c76729fced2e974abfafb065ac2397d7ef6faa
Rodrigo Souto <rodrigo@colivre.coop.br>
2015-06-17 15:14:53 -0300

    content-viewer: use lead instead of straight abstract

--- app/models/article.rb
+++ app/models/article.rb
@@ -724,8 +724,9 @@ class Article < ActiveRecord::Base
     paragraphs.empty? ? '' : paragraphs.first.to_html
   end
 
-  def lead
-    abstract.blank? ? first_paragraph.html_safe : abstract.html_safe
+  def lead(length = nil)
+    content = abstract.blank? ? first_paragraph.html_safe : abstract.html_safe
+    length.present? ? content.truncate(length) : content
   end
 
   def short_lead
