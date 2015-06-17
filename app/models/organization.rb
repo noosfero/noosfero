coletivoEITA/noosfero-ecosol

organizations: use display_name on short_name

app/models/organization.rb
@@ -151,6 +151,12 @@ class Organization < Profile
     ]
   end
 
+  def short_name chars = 40
+    s = self.display_name
+    s = super(chars) if s.empty?
+    s
+  end
+
   def notification_emails
     emails = [contact_email].select(&:present?) + admins.map(&:email)
     if emails.empty?

noosfero/noosfero

app/models/organization.rb
@@ -147,6 +147,12 @@ class Organization < Profile
     ]
   end
 
+  def short_name chars = 40
+    s = self.display_name
+    s = super(chars) if s.blank?
+    s
+  end
+
   def notification_emails
     emails = [contact_email].select(&:present?) + admins.map(&:email)
     if emails.empty?