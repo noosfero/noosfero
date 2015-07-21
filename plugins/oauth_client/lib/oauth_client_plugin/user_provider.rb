e6c893a24a99d562188cbc6ad57f363b5bf58da6
Braulio Bhavamitra <braulio@eita.org.br>
2015-07-20 21:21:30 -0300

    rails4: changed missed set_table_name

--- plugins/oauth_client/lib/oauth_client_plugin/user_provider.rb
+++ plugins/oauth_client/lib/oauth_client_plugin/user_provider.rb
@@ -3,7 +3,7 @@ class OauthClientPlugin::UserProvider < ActiveRecord::Base
    belongs_to :user, :class_name => 'User'
    belongs_to :provider, :class_name => 'OauthClientPlugin::Provider'
 
-   set_table_name :oauth_client_plugin_user_providers
+   self.table_name = :oauth_client_plugin_user_providers
 
    attr_accessible :user, :provider, :enabled
 
