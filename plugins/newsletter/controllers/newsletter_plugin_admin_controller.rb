4df54eb8b390b627c677c6969f18af2e39b49fa2
Joenio Costa <joenio@colivre.coop.br>
2015-09-11 16:49:47 -0300

    new plugin: newsletter
    a plugin that periodically sends newsletter via email to network users
    
    signed-off-by: Larissa Reis <larissa@colivre.coop.br>
    signed-off-by: Melissa Wen <melissa@colivre.coop.br>

---
+++ plugins/newsletter/controllers/newsletter_plugin_admin_controller.rb
@@ -0,0 +1,48 @@
+class NewsletterPluginAdminController < PluginAdminController
+
+  def index
+    @newsletter = NewsletterPlugin::Newsletter.where(environment_id: environment.id).first_or_initialize
+
+    if request.post?
+      # token input gives the param as a comma separated string
+      params[:newsletter][:blog_ids] = (params[:newsletter][:blog_ids] || '').split(',')
+
+      params[:newsletter][:person_id] = user.id
+
+      file = params[:file]
+      if file && file[:recipients].present?
+        @newsletter.import_recipients(file[:recipients], file[:name], file[:email], file[:headers].present?)
+      end
+
+      if !@newsletter.errors.any? && @newsletter.update_attributes(params[:newsletter])
+        if params['visualize']
+          @message = @newsletter.body
+          render :file => 'mailing/sender/notification', :layout => false
+        else
+          session[:notice] = _('Newsletter updated.')
+        end
+      else
+        session[:notice] = _('Newsletter could not be saved.')
+      end
+    end
+
+    @blogs = Blog.includes(:profile).find_all_by_id(@newsletter.blog_ids)
+  end
+
+  #TODO: Make this query faster
+  def search_communities
+    communities = environment.communities
+    blogs = Blog.joins(:profile).where(profiles: {environment_id: environment.id})
+
+    found_communities = find_by_contents(:communities, environment, communities, params['q'], {:page => 1})[:results]
+    found_blogs = find_by_contents(:blogs, environment, blogs, params['q'], {:page => 1})[:results]
+
+    results = (found_blogs + found_communities.map(&:blogs).flatten).uniq
+    render :text => results.map { |blog| {:id => blog.id, :name => _("%s in %s") % [blog.name, blog.profile.name]} }.to_json
+  end
+
+  def recipients
+    @additional_recipients = NewsletterPlugin::Newsletter.where(environment_id: environment.id).first_or_initialize.additional_recipients
+  end
+
+end
---
+++ plugins/newsletter/test/functional/newsletter_plugin_controller_test.rb
@@ -0,0 +1,37 @@
+require File.dirname(__FILE__) + '/../../../../test/test_helper'
+
+class NewsletterPluginControllerTest < ActionController::TestCase
+
+  def setup
+    @controller = NewsletterPluginController.new
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+    environment = fast_create(Environment)
+    environment.enable_plugin(NewsletterPlugin)
+    @controller.stubs(:environment).returns(environment)
+  end
+
+  should 'require login to confirm unsubscription' do
+    post :confirm_unsubscription
+    assert_response 302
+  end
+
+  should 'open unsubscription page for anonymous' do
+    get :unsubscribe
+    assert_response :success
+  end
+
+  should 'add user email from unsubscribers list' do
+    NewsletterPlugin::Newsletter.create!(
+      :environment => @controller.environment,
+      :person => fast_create(Person)
+    )
+    maria = create_user("maria").person
+    login_as("maria")
+    post :confirm_unsubscription
+    assert_response :redirect
+    assert_redirected_to :controller => 'home'
+    assert_includes assigns(:newsletter).unsubscribers, maria.email
+  end
+
+end
