0d0d8a31c1c4b09496d7c408c0474fc91e3431c6
Tallys Martins <tallysmartins@yahoo.com.br>
2015-09-13 14:06:54 -0300

    Fix mass-assignment on CreateCommunityTask
    Conflicts:
    	app/models/create_community.rb

--- app/models/create_community.rb
+++ app/models/create_community.rb
@@ -14,9 +14,10 @@ class CreateCommunity < Task
 
   acts_as_having_image
 
-  DATA_FIELDS = Community.fields + ['name', 'closed']
+  DATA_FIELDS = Community.fields + ['name', 'closed', 'description']
   DATA_FIELDS.each do |field|
     settings_items field.to_sym
+    attr_accessible field.to_sym
   end
 
   def validate
