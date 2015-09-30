ddf8ec9d671c8bf7efd22307b93e17252d305c49
Braulio Bhavamitra <braulio@eita.org.br>
2015-09-29 23:02:41 -0300

    orders: move features to shopping_cart

--- plugins/shopping_cart/features/step_definitions/delivery_steps.rb
+++ plugins/shopping_cart/features/step_definitions/delivery_steps.rb
@@ -1,10 +1,3 @@
-Given /^the shopping basket is (enabled|disabled) on "([^""]*)"$/ do |status, name|
-  status = status == 'enabled'
-  enterprise = Enterprise.find_by_name(name) || Enterprise[name]
-  settings = enterprise.shopping_cart_settings({:enabled => status})
-  settings.save!
-end
-
 Given /^"([^""]*)" has the following delivery methods$/ do |name, table|
   enterprise = Enterprise.find_by_name(name) || Enterprise[name]
   table.hashes.map{|item| item.dup}.each do |item|
