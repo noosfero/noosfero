64b98a9ea0ee1ed54adbf3456730101bca04ef43
Daniela Feitosa <danielafeitosa@colivre.coop.br>
2015-10-01 21:47:03 +0000

    Improve pagination default
    Also %s/will_paginate/pagination_links to use the default on forum, blog
    and cms
    
    Signed-off-by: Tallys Martins <tallysmartins@yahoo.com.br>
    Signed-off-by: Melissa Wen <melissa@colivre.coop.br>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1131,7 +1131,7 @@ module ApplicationHelper
   alias :browse_communities_menu :search_communities_menu
 
   def pagination_links(collection, options={})
-    options = {:previous_label => '&laquo; ' + _('Previous'), :next_label => _('Next') + ' &raquo;'}.merge(options)
+    options = {:previous_label => content_tag(:span, '&laquo; ', :class => 'previous-arrow') + _('Previous'), :next_label => _('Next') + content_tag(:span, ' &raquo;', :class => 'next-arrow'), :inner_window => 1, :outer_window => 0 }.merge(options)
     will_paginate(collection, options)
   end
 
