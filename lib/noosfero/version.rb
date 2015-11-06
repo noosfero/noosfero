d877a56dd9ad293683a28ff3be347994ea1bcf3a
Antonio Terceiro <terceiro@colivre.coop.br>
2015-11-06 11:03:36 -0200

    Noosfero::VERSION: fix detecting git version for good

--- lib/noosfero/version.rb
+++ lib/noosfero/version.rb
@@ -6,7 +6,13 @@ end
 root = File.expand_path(File.dirname(__FILE__) + '/../..')
 if File.exist?(File.join(root, '.git')) && system('which git >/dev/null')
   git_version = Dir.chdir(root) { `git describe --tags`.to_s.strip.sub('-rc', '~rc') }
-  if git_version > Noosfero::VERSION
+  version_sort = IO.popen(['sort', '--version-sort'], 'w+')
+  version_sort.puts(Noosfero::VERSION)
+  version_sort.puts(git_version)
+  version_sort.close_write
+  new_version = version_sort.readlines.last.strip
+  if new_version != Noosfero::VERSION
     Noosfero::VERSION.clear << git_version
   end
+  version_sort.close
 end
