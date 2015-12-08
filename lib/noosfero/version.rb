71178d9a6a3b4162ab6db68b5aeb8353f2474f8e
Antonio Terceiro <terceiro@colivre.coop.br>
2015-12-08 18:29:12 -0200

    Noosfero::VERSION: avoid git warning on shallow clones
    `git describe` will not be able to describe the HEAD commit in shallow
    clones, which will most probably not contain any tags.

--- lib/noosfero/version.rb
+++ lib/noosfero/version.rb
@@ -5,14 +5,16 @@ end
 
 root = File.expand_path(File.dirname(__FILE__) + '/../..')
 if File.exist?(File.join(root, '.git')) && system('which git >/dev/null')
-  git_version = Dir.chdir(root) { `git describe --tags`.to_s.strip.sub('-rc', '~rc') }
-  version_sort = IO.popen(['sort', '--version-sort'], 'w+')
-  version_sort.puts(Noosfero::VERSION)
-  version_sort.puts(git_version)
-  version_sort.close_write
-  new_version = version_sort.readlines.last.strip
-  if new_version != Noosfero::VERSION
-    Noosfero::VERSION.clear << git_version
+  git_version = Dir.chdir(root) { `git describe --tags 2>/dev/null`.to_s.strip.sub('-rc', '~rc') }
+  if git_version != ''
+    version_sort = IO.popen(['sort', '--version-sort'], 'w+')
+    version_sort.puts(Noosfero::VERSION)
+    version_sort.puts(git_version)
+    version_sort.close_write
+    new_version = version_sort.readlines.last.strip
+    if new_version != Noosfero::VERSION
+      Noosfero::VERSION.clear << git_version
+    end
+    version_sort.close
   end
-  version_sort.close
 end
