632d06f901cd284c7b4dd5d8482696a8657f0c0c
Braulio Bhavamitra <braulio@eita.org.br>
2015-12-12 13:48:55 -0300

    product_category: filter distinct (fix test)

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -7,7 +7,7 @@ class ProductCategory < Category
 
   scope :unique, :select => 'DISTINCT ON (path) categories.*'
   scope :by_enterprise, -> enterprise {
-    joins(:products).
+    distinct.joins(:products).
     where('products.profile_id = ?', enterprise.id)
   }
   scope :by_environment, -> environment {
