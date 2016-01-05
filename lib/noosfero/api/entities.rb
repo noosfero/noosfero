80fe18a60900f493298cbce39ce79671d05b9677
Thiago Ribeiro <thiagitosouza@gmail.com>
2016-01-05 16:08:50 -0200

    Add attribute admin username in community via api
    (cherry picked from commit f3085abc80ff6485b6b17908da2acaa3d2377942)

--- lib/noosfero/api/entities.rb
+++ lib/noosfero/api/entities.rb
@@ -106,7 +106,7 @@ module Noosfero
         root 'communities', 'community'
         expose :description
         expose :admins do |community, options|
-          community.admins.map{|admin| {"name"=>admin.name, "id"=>admin.id}}
+          community.admins.map{|admin| {"name"=>admin.name, "id"=>admin.id, "id"=>admin.identifier}}
         end
         expose :categories, :using => Category
         expose :members, :using => Person
