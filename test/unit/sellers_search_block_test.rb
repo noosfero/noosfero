c90084dcda21a752b7553f7bfdd79fca1579230b
Joenio Costa <joenio@colivre.coop.br>
2016-01-11 16:58:03 -0300

    removing deprecated test for SellersSearchBlock

--- test/unit/sellers_search_block_test.rb
+++ test/unit/sellers_search_block_test.rb
@@ -10,10 +10,4 @@ class SellersSearchBlockTest < ActiveSupport::TestCase
     assert_not_equal Block.new.default_title, SellersSearchBlock.new.default_title
   end
 
-  should 'not use a fixed title' do
-    block = SellersSearchBlock.new(:title => 'my custom title')
-    expects(:render).with(:file => 'search/_sellers_form', :locals => { :title => 'my custom title'})
-    instance_eval(&block.content)
-  end
-
 end
