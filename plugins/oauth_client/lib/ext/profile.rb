f2aaabd6dd85b2e436d734c6420ae91e4599c347
Braulio Bhavamitra <braulio@eita.org.br>
2016-03-01 20:10:14 -0300

    oauth_client: fix association declaration

--- plugins/oauth_client/lib/ext/profile.rb
+++ plugins/oauth_client/lib/ext/profile.rb
@@ -1,10 +1,8 @@
 require_dependency 'profile'
 
-Profile.descendants.each do |subclass|
-  subclass.class_eval do
+class Profile
 
-    has_many :oauth_auths, foreign_key: :profile_id, class_name: 'OauthClientPlugin::Auth', dependent: :destroy
-    has_many :oauth_providers, through: :oauth_auths, source: :provider
+  has_many :oauth_auths, foreign_key: :profile_id, class_name: 'OauthClientPlugin::Auth', dependent: :destroy
+  has_many :oauth_providers, through: :oauth_auths, source: :provider
 
-  end
 end
