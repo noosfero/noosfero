2942e278d2df7bd14a2bb165fd9c4f63cf618ab8
Rafael Reggiani Manzo <rr.manzo@protonmail.com>
2016-03-17 10:54:29 -0300

    Extract methods from ApplicationHelper
    The method for build profile_image_link was part of ApplicationHelper
    but will also be necessary for building proper tests on a upcoming
    refactor for PeopleBlockPlugin.
    
    As including ApplicationHelper on unit tests has already caused issues
    with unwanted block behaviour regarding their visibility, this is a move
    in the direction of avoiding such issues before they happen and allow
    reliable future tests.

--- test/unit/application_helper_test.rb
+++ test/unit/application_helper_test.rb
@@ -145,35 +145,6 @@ class ApplicationHelperTest < ActionView::TestCase
     assert_tag_in_string rolename_for(member2, community), :tag => 'span', :content => 'Profile Administrator'
   end
 
-  should 'get theme from environment by default' do
-    @environment = mock
-    @environment.stubs(:theme).returns('my-environment-theme')
-    stubs(:profile).returns(nil)
-    assert_equal 'my-environment-theme', current_theme
-  end
-
-  should 'get theme from profile when profile is present' do
-    profile = mock
-    profile.stubs(:theme).returns('my-profile-theme')
-    stubs(:profile).returns(profile)
-    assert_equal 'my-profile-theme', current_theme
-  end
-
-  should 'override theme with testing theme from session' do
-    stubs(:session).returns(:theme => 'theme-under-test')
-    assert_equal 'theme-under-test', current_theme
-  end
-
-  should 'point to system theme path by default' do
-    expects(:current_theme).returns('my-system-theme')
-    assert_equal '/designs/themes/my-system-theme', theme_path
-  end
-
-  should 'point to user theme path when testing theme' do
-    stubs(:session).returns({:theme => 'theme-under-test'})
-    assert_equal '/user_themes/theme-under-test', theme_path
-  end
-
   should 'render theme footer' do
     stubs(:theme_path).returns('/user_themes/mytheme')
     footer_path = Rails.root.join('public', 'user_themes', 'mytheme', 'footer.html.erb')
@@ -293,38 +264,6 @@ class ApplicationHelperTest < ActionView::TestCase
     env.enable(:disable_categories)
     assert env.enabled?(:disable_categories)
     assert_nil select_categories(mock)
-  end
-
-  should 'provide sex icon for males' do
-    stubs(:environment).returns(Environment.default)
-    expects(:content_tag).with(anything, 'male').returns('MALE!!')
-    expects(:content_tag).with(anything, 'MALE!!', is_a(Hash)).returns("FINAL")
-    assert_equal "FINAL", profile_sex_icon(build(Person, :sex => 'male'))
-  end
-
-  should 'provide sex icon for females' do
-    stubs(:environment).returns(Environment.default)
-    expects(:content_tag).with(anything, 'female').returns('FEMALE!!')
-    expects(:content_tag).with(anything, 'FEMALE!!', is_a(Hash)).returns("FINAL")
-    assert_equal "FINAL", profile_sex_icon(build(Person, :sex => 'female'))
-  end
-
-  should 'provide undef sex icon' do
-    stubs(:environment).returns(Environment.default)
-    expects(:content_tag).with(anything, 'undef').returns('UNDEF!!')
-    expects(:content_tag).with(anything, 'UNDEF!!', is_a(Hash)).returns("FINAL")
-    assert_equal "FINAL", profile_sex_icon(build(Person, :sex => nil))
-  end
-
-  should 'not draw sex icon for non-person profiles' do
-    assert_equal '', profile_sex_icon(Community.new)
-  end
-
-  should 'not draw sex icon when disabled in the environment' do
-    env = fast_create(Environment, :name => 'env test')
-    env.expects(:enabled?).with('disable_gender_icon').returns(true)
-    stubs(:environment).returns(env)
-    assert_equal '', profile_sex_icon(build(Person, :sex => 'male'))
   end
 
   should 'display field on person signup' do
@@ -524,19 +463,6 @@ class ApplicationHelperTest < ActionView::TestCase
     assert_equal Environment.default.name, page_title
   end
 
-  should 'gravatar default parameter' do
-    profile = mock
-    profile.stubs(:theme).returns('some-theme')
-    stubs(:profile).returns(profile)
-
-    NOOSFERO_CONF.stubs(:[]).with('gravatar').returns('crazyvatar')
-    assert_equal gravatar_default, 'crazyvatar'
-
-    stubs(:theme_option).returns('gravatar' => 'nicevatar')
-    NOOSFERO_CONF.stubs(:[]).with('gravatar').returns('nicevatar')
-    assert_equal gravatar_default, 'nicevatar'
-  end
-
   should 'use theme passed via param when in development mode' do
     stubs(:environment).returns(build(Environment, :theme => 'environment-theme'))
     Rails.env.stubs(:development?).returns(true)
@@ -556,48 +482,6 @@ class ApplicationHelperTest < ActionView::TestCase
     stubs(:environment).returns(fast_create(Environment, :theme => 'new-theme'))
     stubs(:profile).returns(fast_create(Profile))
     assert_equal environment.theme, current_theme
-  end
-
-  should 'return nil when :show_balloon_with_profile_links_when_clicked is not enabled in environment' do
-    env = Environment.default
-    env.stubs(:enabled?).with(:show_balloon_with_profile_links_when_clicked).returns(false)
-    stubs(:environment).returns(env)
-    profile = Profile.new
-    assert_nil links_for_balloon(profile)
-  end
-
-  should 'return ordered list of links to balloon to Person' do
-    env = Environment.default
-    env.stubs(:enabled?).with(:show_balloon_with_profile_links_when_clicked).returns(true)
-    stubs(:environment).returns(env)
-    person = Person.new identifier: 'person'
-    person.stubs(:url).returns('url for person')
-    person.stubs(:public_profile_url).returns('url for person')
-    links = links_for_balloon(person)
-    assert_equal ['Wall', 'Friends', 'Communities', 'Send an e-mail', 'Add'], links.map{|i| i.keys.first}
-  end
-
-  should 'return ordered list of links to balloon to Community' do
-    env = Environment.default
-    env.stubs(:enabled?).with(:show_balloon_with_profile_links_when_clicked).returns(true)
-    stubs(:environment).returns(env)
-    community = Community.new identifier: 'comm'
-    community.stubs(:url).returns('url for community')
-    community.stubs(:public_profile_url).returns('url for community')
-    links = links_for_balloon(community)
-    assert_equal ['Wall', 'Members', 'Agenda', 'Join', 'Leave community', 'Send an e-mail'], links.map{|i| i.keys.first}
-  end
-
-  should 'return ordered list of links to balloon to Enterprise' do
-    env = Environment.default
-    env.stubs(:enabled?).with(:show_balloon_with_profile_links_when_clicked).returns(true)
-    stubs(:environment).returns(env)
-    enterprise = Enterprise.new identifier: 'coop'
-    enterprise.stubs(:url).returns('url for enterprise')
-    enterprise.stubs(:public_profile_url).returns('url for enterprise')
-    stubs(:catalog_path)
-    links = links_for_balloon(enterprise)
-    assert_equal ['Products', 'Members', 'Agenda', 'Send an e-mail'], links.map{|i| i.keys.first}
   end
 
   should 'use favicon from environment theme if does not have profile' do
@@ -649,24 +533,6 @@ class ApplicationHelperTest < ActionView::TestCase
     user.stubs(:is_admin?).with(environment).returns(true)
     stubs(:user).returns(user)
     assert admin_link.present?
-  end
-
-  should 'not return mime type of profile icon if not requested' do
-    stubs(:profile).returns(Person.new)
-    stubs(:current_theme).returns('default')
-
-    filename, mime = profile_icon(Person.new, :thumb)
-    assert_not_nil filename
-    assert_nil mime
-  end
-
-  should 'return mime type of profile icon' do
-    stubs(:profile).returns(Person.new)
-    stubs(:current_theme).returns('default')
-
-    filename, mime = profile_icon(Person.new, :thumb, true)
-    assert_not_nil filename
-    assert_not_nil mime
   end
 
   should 'pluralize without count' do
@@ -1035,31 +901,6 @@ class ApplicationHelperTest < ActionView::TestCase
     stubs(:environment).returns(env)
     stubs(:profile).returns(c)
     assert_equal c.top_url, top_url
-  end
-
-  should "Extra info with hash" do
-    @plugins = mock
-    @plugins.stubs(:dispatch_first).returns(false)
-    env = Environment.default
-    stubs(:environment).returns(env)
-    stubs(:profile).returns(profile)
-    profile = fast_create(Person, :environment_id => env.id)
-    info = {:value =>_('New'), :class => 'new-profile'}
-    html = profile_image_link(profile, size=:portrait, tag='li', extra_info = info)
-    assert_tag_in_string html, :tag => 'span', :attributes => { :class => 'profile-image new-profile' }
-    assert_tag_in_string html, :tag => 'span', :attributes => { :class => 'extra_info new-profile' }, :content => 'New'
-  end
-
-  should "Extra info without hash" do
-    @plugins = mock
-    @plugins.stubs(:dispatch_first).returns(false)
-    env = Environment.default
-    stubs(:environment).returns(env)
-    stubs(:profile).returns(profile)
-    profile = fast_create(Person, :environment_id => env.id)
-    info = 'new'
-    html = profile_image_link(profile, size=:portrait, tag='li', extra_info = info)
-    assert_tag_in_string html, :tag => 'span', :attributes => { :class => 'extra_info' }, :content => 'new'
   end
 
   protected
