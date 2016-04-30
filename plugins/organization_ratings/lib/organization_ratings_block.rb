66eabb2c1a84442a50add99e3aa35c6828815ca9
Rafael Reggiani Manzo <rr.manzo@protonmail.com>
2016-04-30 09:23:06 -0300

    Remove OrganizationRatings#content
    This method is no longer necessary (and violates MVC) with the new
    structure at BoxesHelper being enough for such job.
    
    The view just required a rename to get into the standards.

--- plugins/organization_ratings/lib/organization_ratings_block.rb
+++ plugins/organization_ratings/lib/organization_ratings_block.rb
@@ -9,17 +9,6 @@ class OrganizationRatingsBlock < Block
     _('This block displays the organization ratings.')
   end
 
-  def content(args = {})
-    block = self
-
-    proc do
-      render(
-        :file => 'blocks/organization_ratings_block',
-        :locals => {:block => block}
-      )
-    end
-  end
-
   def limit_number_of_ratings
     env_organization_ratings_config.per_page
   end
