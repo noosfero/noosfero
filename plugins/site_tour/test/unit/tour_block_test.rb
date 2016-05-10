ffc2b613ac1e2ed6447a262434807e7ec5ff8271
Rafael Reggiani Manzo <rr.manzo@protonmail.com>
2016-05-10 09:42:31 -0300

    Remove TourBlock#content
    This method was in violation of MVC principles and its logic is now
    handled by the BoxesHelper.

--- plugins/site_tour/test/unit/tour_block_test.rb
+++ plugins/site_tour/test/unit/tour_block_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class TrackListBlockTest < ActionView::TestCase
 
   ActionView::Base.send :include, ApplicationHelper
+  include BoxesHelper
 
   def setup
     @block = fast_create(SiteTourPlugin::TourBlock)
@@ -18,24 +19,24 @@ class TrackListBlockTest < ActionView::TestCase
 
   should 'render script tag in visualization mode' do
     controller.expects(:boxes_editor?).returns(false)
-    assert_tag_in_string instance_eval(&block.content), :tag => 'script'
+    assert_tag_in_string render_block_content(block), :tag => 'script'
   end
 
   should 'do not render script tag when editing' do
     controller.expects(:boxes_editor?).returns(true)
     controller.expects(:uses_design_blocks?).returns(true)
-    assert_no_tag_in_string instance_eval(&block.content), :tag => 'script'
+    assert_no_tag_in_string render_block_content(block), :tag => 'script'
   end
 
   should 'display help button' do
     controller.expects(:boxes_editor?).returns(false)
-    assert_tag_in_string instance_eval(&block.content), :tag => 'a', :attributes => {:class => 'button icon-help with-text tour-button'}
+    assert_tag_in_string render_block_content(block), :tag => 'a', :attributes => {:class => 'button icon-help with-text tour-button'}
   end
 
   should 'do not display help button when display_button is false' do
     block.display_button = false
     controller.expects(:boxes_editor?).returns(false)
-    assert_no_tag_in_string instance_eval(&block.content), :tag => 'a', :attributes => {:class => 'button icon-help with-text tour-button'}
+    assert_no_tag_in_string render_block_content(block), :tag => 'a', :attributes => {:class => 'button icon-help with-text tour-button'}
   end
 
 end
