8898935c1b2438a939525942de0c31338f36a13e
Caio SBA <caiosba@gmail.com>
2016-05-16 09:59:22 -0300

    Ticket #45: Adding APi endpoint to get environment tags

--- app/api/v1/tags.rb
+++ app/api/v1/tags.rb
@@ -1,12 +1,8 @@
 module Api
   module V1
     class Tags < Grape::API
-      before { authenticate! }
-
       resource :articles do
-
         resource ':id/tags' do
-
           get do
             article = find_article(environment.articles, params[:id])
             present article.tag_list
@@ -14,11 +10,19 @@ module Api
 
           desc "Add a tag to an article"
           post do
+            authenticate!
             article = find_article(environment.articles, params[:id])
             article.tag_list=params[:tags]
             article.save
             present article.tag_list
           end
+        end
+      end
+
+      resource :environment do
+        desc 'Return the tag counts for this environment'
+        get '/tags' do
+          present environment.tag_counts
         end
       end
     end
