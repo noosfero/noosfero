18bda87e8783fd80cc0ceddaa06b741a8aee5862
Braulio Bhavamitra <braulio@eita.org.br>
2016-05-20 22:10:15 -0300

    responsive: fix missed conflicts

--- plugins/responsive/lib/ext/forms_helper.rb
+++ plugins/responsive/lib/ext/forms_helper.rb
@@ -11,10 +11,6 @@ module FormsHelper
       return super unless theme_responsive?
 
       options[:id] ||= 'radio-' + FormsHelper.next_id_number
-<<<<<<< HEAD
-      content_tag( 'label', radio_button_tag( name, value, checked, options ) + '  ' +
- human_name, for: options[:id], class: 'radio-inline' )
-=======
       content_tag :div, class:'radio-inline' do
         content_tag :label, for: options[:id] do
           [
@@ -24,7 +20,6 @@ module FormsHelper
           ].safe_join
         end
       end
->>>>>>> 2ef3a43... responsive: fix html_safe issues
     end
 
     # add -inline class
@@ -32,10 +27,6 @@ module FormsHelper
       return super unless theme_responsive?
 
       options[:id] ||= 'checkbox-' + FormsHelper.next_id_number
-<<<<<<< HEAD
-      hidden_field_tag(name, '0') +
-        content_tag( 'label', check_box_tag( name, value, checked, options ) + '  ' + human_name, for: options[:id], class: 'checkbox-inline')
-=======
       [
         hidden_field_tag(name, '0'),
         content_tag(:div, class:'checkbox-inline') do
@@ -48,7 +39,6 @@ module FormsHelper
           end
         end
       ].safe_join
->>>>>>> 2ef3a43... responsive: fix html_safe issues
     end
 
     def submit_button(type, label, html_options = {})
@@ -69,29 +59,13 @@ module FormsHelper
 
       html_options.delete(:cancel)
       bt_submit = button_tag(label, html_options.merge(class: the_class))
-<<<<<<< HEAD
-=======
 
       [bt_submit + bt_cancel].safe_join
     end
->>>>>>> 2ef3a43... responsive: fix html_safe issues
 
       bt_submit + bt_cancel
     end
 
-<<<<<<< HEAD
-    %w[select select_tag text_field_tag number_field_tag password_field_tag].each do |method|
-      define_method method do |*args, &block|
-        #return super(*args, &block) unless theme_responsive?
-
-        options = args.extract_options!
-        if options['class']
-          options['class'] = "#{options['class']} form-control"
-        else
-          options[:class] = "#{options[:class]} form-control"
-        end
-        super(*(args << options), &block)
-=======
     %w[
       select_tag
       text_field_tag text_area_tag
@@ -101,22 +75,12 @@ module FormsHelper
       define_method method do |name, value=nil, options={}, &block|
         responsive_add_field_class! options
         super(name, value, options, &block).html_safe
->>>>>>> 2ef3a43... responsive: fix html_safe issues
       end
     end
     %w[select_month select_year].each do |method|
       define_method method do |date, options={}, html_options={}|
-<<<<<<< HEAD
-        if html_options['class']
-          html_options['class'] = "#{html_options['class']} form-control"
-        else
-          html_options[:class] = "#{html_options[:class]} form-control"
-        end
-        super date, options, html_options
-=======
         responsive_add_field_class! html_options
         super(date, options, html_options).html_safe
->>>>>>> 2ef3a43... responsive: fix html_safe issues
       end
     end
 
--- plugins/responsive/lib/ext/input_helper.rb
+++ plugins/responsive/lib/ext/input_helper.rb
@@ -4,29 +4,15 @@ module InputHelper
   protected
 
   def input_group_addon addon, options = {}, &block
-<<<<<<< HEAD
-    content_tag :div,
-      content_tag(:span, addon, class: 'input-group-addon') + yield,
-    class: 'input-group'
-=======
     content_tag :div, class: 'input-group' do
       [
         content_tag(:span, addon, class: 'input-group-addon'),
         capture(&block),
       ].safe_join
     end
->>>>>>> 2ef3a43... responsive: fix html_safe issues
   end
 
 end
 
-<<<<<<< HEAD
-module ApplicationHelper
-
-  include InputHelper
-
-end
-=======
 ApplicationHelper.include InputHelper
->>>>>>> 2ef3a43... responsive: fix html_safe issues
 
