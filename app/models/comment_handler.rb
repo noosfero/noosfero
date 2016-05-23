7fdd7fbf762707d35291b1ab6651399cbeb95887
Gabriel Silva <gabriel93.silva@gmail.com>
2016-05-23 20:22:02 +0000

    CommentHandler delayed job saves the context locale
    Signed-off-by: Gabriel Silva <gabriel93.silva@gmail.com>

--- app/models/comment_handler.rb
+++ app/models/comment_handler.rb
@@ -1,8 +1,16 @@
-class CommentHandler < Struct.new(:comment_id, :method)
+class CommentHandler < Struct.new(:comment_id, :method, :locale)
+  def initialize(*args)
+    super
+    self.locale ||= FastGettext.locale
+  end
 
   def perform
+    saved_locale = FastGettext.locale
+    FastGettext.locale = locale
+
     comment = Comment.find(comment_id)
     comment.send(method)
+    FastGettext.locale = saved_locale
   rescue ActiveRecord::RecordNotFound
     # just ignore non-existing comments
   end
