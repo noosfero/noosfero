2af0cb9b45316a28662d90032c5101ad7518e44f
Victor Costa <vfcosta@gmail.com>
2016-06-06 11:42:05 -0300

    api: endpoint to edit blocks

--- app/api/v1/blocks.rb
+++ app/api/v1/blocks.rb
@@ -8,6 +8,13 @@ module Api
           return forbidden! unless block.visible_to_user?(current_person)
           present block, :with => Entities::Block, display_api_content: true
         end
+
+        post ':id' do
+          block = Block.find(params["id"])
+          return forbidden! unless block.allow_edit?(current_person)
+          block.update_attributes!(params[:block])
+          present block, :with => Entities::Block, display_api_content: true
+        end
       end
     end
 
