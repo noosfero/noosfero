fbdee132ab393bc8a3afe4f9abb746171f36d803
Hugo Melo <hugo@riseup.net>
2016-06-29 18:28:39 -0300

    Move requires_email to organization so that enterprise doesn't crash

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,6 +2,10 @@
 class Organization < Profile
 
   attr_accessible :moderated_articles, :foundation_year, :contact_person, :acronym, :legal_form, :economic_activity, :management_information, :cnpj, :display_name, :enable_contact_us
+  attr_accessible :requires_email
+
+  settings_items :requires_email, type: :boolean
+  alias_method :requires_email?, :requires_email
 
   SEARCH_FILTERS = {
     :order => %w[more_recent more_popular more_active],
