64e7e54397805a7b4374e883a1d042b0b7bdbf7d
Rodrigo Souto <rodrigo@colivre.coop.br>
2016-08-16 17:37:28 -0300

    chat: ensure user logged in before register chat_status offline

--- app/controllers/public/account_controller.rb
+++ app/controllers/public/account_controller.rb
@@ -156,8 +156,8 @@ class AccountController < ApplicationController
   def logout
     if logged_in?
       self.current_user.forget_me
+      current_user.update({:chat_status_at => DateTime.now}.merge({:last_chat_status => current_user.chat_status, :chat_status => 'offline'}))
     end
-    current_user.update({:chat_status_at => DateTime.now}.merge({:last_chat_status => current_user.chat_status, :chat_status => 'offline'}))
     reset_session
     session[:notice] = _("You have been logged out.")
     redirect_to :controller => 'home', :action => 'index'
