00c502c314aac4b6e3edc76d0b9cbb86737a3d80
Victor Costa <vfcosta@gmail.com>
2016-10-26 15:34:31 -0300

    Use session theme in current_theme
    Signed-off-by: Evandro Jr <evandrojr@gmail.com>

--- app/helpers/theme_loader_helper.rb
+++ app/helpers/theme_loader_helper.rb
@@ -2,32 +2,31 @@ module ThemeLoaderHelper
   def current_theme
     @current_theme ||=
       begin
-        if session[:user_theme]
-          session[:user_theme]
-        else
-          # utility for developers: set the theme to 'random' in development mode and
-          # you will get a different theme every request. This is interesting for
-          # testing
-          if Rails.env.development? && environment.theme == 'random'
-            @random_theme ||= Dir.glob('public/designs/themes/*').map { |f| File.basename(f) }.rand
-            @random_theme
-          elsif Rails.env.development? && respond_to?(:params) && params[:user_theme] && File.exists?(Rails.root.join('public/designs/themes', params[:user_theme]))
-            params[:user_theme]
-          else
-            if profile && !profile.theme.nil?
-              profile.theme
-            elsif environment
-              environment.theme
-            else
-              if logger
-                logger.warn("No environment found. This is weird.")
-                logger.warn("Request environment: %s" % request.env.inspect)
-                logger.warn("Request parameters: %s" % params.inspect)
-              end
+        return session[:theme] if session[:theme]
+        return session[:user_theme] if session[:user_theme]
 
-              # could not determine the theme, so return the default one
-              'default'
+        # utility for developers: set the theme to 'random' in development mode and
+        # you will get a different theme every request. This is interesting for
+        # testing
+        if Rails.env.development? && environment.theme == 'random'
+          @random_theme ||= Dir.glob('public/designs/themes/*').map { |f| File.basename(f) }.rand
+          @random_theme
+        elsif Rails.env.development? && respond_to?(:params) && params[:user_theme] && File.exists?(Rails.root.join('public/designs/themes', params[:user_theme]))
+          params[:user_theme]
+        else
+          if profile && !profile.theme.nil?
+            profile.theme
+          elsif environment
+            environment.theme
+          else
+            if logger
+              logger.warn("No environment found. This is weird.")
+              logger.warn("Request environment: %s" % request.env.inspect)
+              logger.warn("Request parameters: %s" % params.inspect)
             end
+
+            # could not determine the theme, so return the default one
+            'default'
           end
         end
       end
