0d673be8451a9c81f2a0b4e7a4684fbcb8e0d561
Leandro Nunes dos Santos <leandronunes@gmail.com>
2016-12-09 14:22:37 -0300

    save block_images values on block edition

--- app/helpers/block_helper.rb
+++ app/helpers/block_helper.rb
@@ -16,15 +16,15 @@ module BlockHelper
     "
     <tr class=\"image-data-line\" data-row-number='#{row_number}'>
       <td>
-        #{select_tag 'block[images][][image_id]', content_tag(:option) + option_groups_from_collection_for_select(block.folder_choices, :images, :name, :id, :name, image[:image_id].to_i).html_safe}
+        #{select_tag 'block[block_images][][image_id]', content_tag(:option) + option_groups_from_collection_for_select(block.folder_choices, :images, :name, :id, :name, image[:image_id].to_i).html_safe}
       </td>
-      <td>#{text_field_tag 'block[images][][address]', image[:address], :class => 'highlight-address', :size => 20}</td>
-      <td>#{text_field_tag 'block[images][][position]', image[:position], :class => 'highlight-position', :size => 1}</td>
-      <td>#{check_box_tag 'block[images][][new_window]', '1', image[:new_window], :class => 'highlight-new_window', :size => 1}</td>
+      <td>#{text_field_tag 'block[block_images][][address]', image[:address], :class => 'highlight-address', :size => 20}</td>
+      <td>#{text_field_tag 'block[block_images][][position]', image[:position], :class => 'highlight-position', :size => 1}</td>
+      <td>#{check_box_tag 'block[block_images][][new_window]', '1', image[:new_window], :class => 'highlight-new_window', :size => 1}</td>
     </tr><tr class=\"image-title\" data-row-number='#{row_number}'>
       <td colspan=\"3\"><label>#{
         content_tag('span', _('Title')) +
-        text_field_tag('block[images][][title]', image[:title], :class => 'highlight-title', :size => 45)
+        text_field_tag('block[block_images][][title]', image[:title], :class => 'highlight-title', :size => 45)
       }</label></td>
       <td>#{button_without_text(:delete, _('Remove'), '#', class: 'delete-highlight', data: {confirm: _('Are you sure you want to remove this highlight')})}</td>
     </tr>
