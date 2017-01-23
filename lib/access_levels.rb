c2295bbb96270095ed8bfd6ccfeee2da02ab5b7e
Rodrigo Souto <rodrigo@colivre.coop.br>
2017-01-23 17:51:22 +0000

    Merge branch 'show_person_wall' into 'master'
    Refactors AccessLevel to use integer flags
    
    Closes #241
    
    See merge request !1089

--- lib/access_levels.rb
+++ lib/access_levels.rb
@@ -1,14 +1,25 @@
 class AccessLevels
-  OPTIONS = ['visitors', 'users', 'related', 'self']
 
-  LABELS = {'visitors' => _('Visitors'), 'users' => _('Logged users')}
-  PERSON_LABELS = LABELS.merge({'self' => _('Me'), 'related' => _('Friends')})
-  GROUP_LABELS = LABELS.merge({'self' => _('Administrators'), 'related' => _('Members')})
+  LEVELS = {
+    # The profile will always be visible, even to anonymous users
+    visitors: 0,
 
-  VALUES = {'self' => 3, 'related' => 2, 'users' => 1, 'visitors' => 0}
+    # Any logged user will be able to view the profile
+    users: 1,
+
+    # The profile will be visible for friends and members
+    related: 2,
+
+    # Only owners or admins can view the profile
+    self: 3
+  }
+
+  LABELS = { visitors: _('Visitors'), users: _('Logged users')}
+  PERSON_LABELS = LABELS.merge({self: _('Me'), related: _('Friends')})
+  GROUP_LABELS = LABELS.merge({self: _('Administrators'), related: _('Members')})
 
   def self.options(base_level=0)
-    OPTIONS[base_level..-1]
+    LEVELS.keys[base_level..-1]
   end
 
   def self.labels(profile)
@@ -24,11 +35,11 @@ class AccessLevels
   def self.can_access?(permission, user, profile)
     return true if user == profile || profile.admins.include?(user) || profile.environment.admins.include?(user)
     case permission
-    when 'related'
+    when LEVELS[:related]
       profile.person? ? profile.friends.include?(user) : profile.members.include?(user)
-    when 'users'
+    when LEVELS[:users]
       user.present?
-    when 'visitors'
+    when LEVELS[:visitors]
       true
     end
   end
