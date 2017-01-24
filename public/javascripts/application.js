59d214d22ce8fff5054aed347bbb7d54ce8d7db2
Rodrigo Souto <rodrigo@colivre.coop.br>
2017-01-24 14:45:38 -0300

    notification: make permanent notification throught requireInteraction property
    Already works on Chromium and will be available on Firefox on version
    52.
    
    https://developer.mozilla.org/en-US/Firefox/Releases/52#DOM_HTML_DOM

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1135,6 +1135,9 @@ function notifyMe(title, options) {
      return null;
    }
 
+  if(PERMANENT_NOTIFICATIONS)
+    options.requireInteraction = true
+
   // Let's check if the user is okay to get some notification
   var notification = null;
   if (Notification.permission === "granted") {
@@ -1159,15 +1162,11 @@ function notifyMe(title, options) {
     });
   }
 
-  if(!PERMANENT_NOTIFICATIONS)
-    setTimeout(function() {notification.close()}, 5000);
-
   notification.onclick = function(){
     notification.close();
     // Chromium tweak
     window.open().close();
     window.focus();
-    this.cancel();
   };
 
   return notification;
