942b6088cce6ff0fd5191daae12cf3278af05542
Leandro Nunes dos Santos <leandronunes@gmail.com>
2017-02-09 14:35:36 -0300

    adding status to api

---
+++ app/api/status.rb
@@ -0,0 +1,17 @@
+module Api
+  module Status
+ 
+    # HTTP Status
+    BAD_REQUEST = 400
+    UNAUTHORIZED = 401
+    FORBIDDEN = 403
+    NOT_FOUND = 404
+    METHOD_NOT_ALLOWED = 405
+    OK = 200
+    CREATED = 201
+
+    # Noosfero API Status
+    DEPRECATED = 299
+
+  end
+end
