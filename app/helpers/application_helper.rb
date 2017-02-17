d41ca60940bd99648c565322787d242dbb4de395
Rodrigo Souto <rodrigo@colivre.coop.br>
2017-02-17 16:44:48 -0300

    html-safe: article source

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -958,7 +958,7 @@ module ApplicationHelper
     elsif page.reference_article
       source_url = link_to(page.reference_article.profile.name, page.reference_article.url)
     end
-    content_tag(:div, _('Source: %s') % source_url.html_safe, :id => 'article-source') unless source_url.nil?
+    content_tag(:div, _('Source: %s').html_safe % source_url.html_safe, :id => 'article-source') unless source_url.nil?
   end
 
   def task_information(task, params = {})
