0afb95d14273302c9f4beff7971d01011afe9734
Braulio Bhavamitra <braulio@eita.org.br>
2017-03-15 16:14:59 -0300

    stoa: temp file for db ins't working well

--- plugins/stoa/test/setup_db.rb
+++ plugins/stoa/test/setup_db.rb
@@ -1,14 +1,20 @@
+db_path = 'plugins/stoa/test/test.db'
 ActiveRecord::Base.configurations['stoa'] = {
   adapter:  'sqlite3',
-  database: Tempfile.new('stoa-test').path,
+  database: db_path,
 }
 
 ActiveRecord::Base.establish_connection :stoa
+
 ActiveRecord::Schema.verbose = false
 ActiveRecord::Schema.create_table 'pessoa' do |t|
   t.integer  'codpes'
   t.text     'numcpf'
   t.date     'dtanas'
-end
+end unless 'pessoa'.in? ActiveRecord::Base.connection.tables
+
 ActiveRecord::Base.establish_connection :test
 
+at_exit do
+  File.unlink db_path
+end
