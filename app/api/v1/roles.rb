a74e19769f0c1a903a59a9b6783f3b6f0c0ac42d
Victor Costa <vfcosta@gmail.com>
2017-03-22 08:25:12 -0300

    api: expose assigned roles of a person in a profile

--- app/api/v1/roles.rb
+++ app/api/v1/roles.rb
@@ -14,7 +14,12 @@ module Api
               profile = environment.profiles.find(params[:profile_id])
               return forbidden! unless profile.kind_of?(Organization)
               roles = Profile::Roles.organization_roles(profile.environment.id, profile.id)
-              present_partial paginate(roles), with: Entities::Role
+              person_roles = []
+              if params[:person_id].present?
+                person = environment.people.find(params[:person_id])
+                person_roles = person.role_assignments.where(resource: profile).joins(:role).map(&:role)
+              end
+              present_partial paginate(roles), with: Entities::Role, person_roles: person_roles
             end
             
           end
