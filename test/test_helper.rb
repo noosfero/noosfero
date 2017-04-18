55314da3f7a6be44b2f9c6e85f3c931b447600bf
Leandro Nunes dos Santos <leandronunes@gmail.com>
2017-04-18 08:22:58 -0300

    change simplecov configuration

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,13 @@
 ENV["RAILS_ENV"] = "test"
 require 'simplecov'
-SimpleCov.start
+
+SimpleCov.start 'rails' do
+  add_filter 'vendor'
+  add_group 'Api', 'app/api'
+  add_group 'Plugins', 'plugins'
+  add_group "Models", "app/models"
+  add_group "Controllers", "app/controllers"
+end
 SimpleCov.command_name 'test'
 
 require_relative '../config/environment'
