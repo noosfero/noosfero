ba4490fdf9caff4ab728bae38015553ac9cb8b64
Braulio Bhavamitra <braulio@prout.io>
2017-04-19 14:51:51 -0300

    rollbar: ignore bots' errors

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,8 +1,15 @@
 if ENV['ROLLBAR_TOKEN'].present?
   Rollbar.configure do |config|
     config.access_token = ENV['ROLLBAR_TOKEN']
+
     config.exception_level_filters.merge!(
       'ActionController::InvalidCrossOriginRequest' => 'ignore',
     )
+
+    config.before_process << proc do |options|
+      agent = options[:scope][:request][:headers]['User-Agent']
+      raise Rollbar::Ignore if Browser.new(agent).bot?
+    end if defined? Browser
   end
 end
+
