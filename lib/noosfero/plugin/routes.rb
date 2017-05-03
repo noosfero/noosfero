69d08abc922520976c452fa7c0ada7be43bb2463
Braulio Bhavamitra <braulio@prout.io>
2017-05-03 20:32:29 -0300

    plugins: refactor routes to don't generate for uniexistant controllers

--- lib/noosfero/plugin/routes.rb
+++ lib/noosfero/plugin/routes.rb
@@ -1,41 +1,73 @@
-plugins_root = Rails.env.test? ? 'plugins' : '{baseplugins,config/plugins}'
-prefixes_by_folder = {public: 'plugin',
-                      profile: 'profile/:profile/plugin',
-                      myprofile: 'myprofile/:profile/plugin',
-                      admin: 'admin/plugin'}
+plugins_root       = if Rails.env.test? then 'plugins' else '{baseplugins,config/plugins}' end
+prefixes_by_folder = {
+  public:    'plugin',
+  profile:   'profile/:profile/plugin',
+  myprofile: 'myprofile/:profile/plugin',
+  admin:     'admin/plugin',
+}
 
-Dir.glob(Rails.root.join(plugins_root, '*', 'controllers')) do |controllers_dir|
-  plugin_name = File.basename(File.dirname(controllers_dir))
+Dir.glob Rails.root.join plugins_root, '*', 'controllers' do |controllers_dir|
+  plugin_name = File.basename File.dirname controllers_dir
 
   controllers_by_folder = prefixes_by_folder.keys.inject({}) do |hash, folder|
     path = "#{controllers_dir}/#{folder}/"
     hash[folder] = Dir.glob("#{path}{*.rb,#{plugin_name}_plugin/*.rb}").map do |filename|
-      filename.gsub(path, '').gsub /[_\/]controller.rb$/, ''
+      filename.gsub(path, '').gsub(/[_\/]controller.rb$/, '')
     end
     hash
   end
 
+  paths          = {}
+  profile_format = /#{Noosfero.identifier_format}/i
+
   controllers_by_folder.each do |folder, controllers|
     controllers.each do |controller|
-      controller_name = controller.gsub /#{plugin_name}_plugin[_\/]?/, ''
+      controller_name = controller.gsub(/#{plugin_name}_plugin[_\/]?/, '')
       controller_path = if controller_name.present? then "/#{controller_name}" else '' end
-      as = controller.tr '/','_'
-      if %w[profile myprofile].include?(folder.to_s)
-        match "#{prefixes_by_folder[folder]}/#{plugin_name}#{controller_path}(/:action(/:id))",
-          controller: controller, profile: /#{Noosfero.identifier_format}/i, via: :all, as: as
-      else
-        match "#{prefixes_by_folder[folder]}/#{plugin_name}#{controller_path}(/:action(/:id))",
-          controller: controller, via: :all
-      end
+
+      as  = controller.tr '/','_'
+      url = "#{prefixes_by_folder[folder]}/#{plugin_name}#{controller_path}(/:action(/:id))"
+
+      paths[url] = {
+        controller: controller,
+        via:        :all,
+        as:         as,
+      }
+      paths[url][:profile] = profile_format if folder.to_s.in? %w[profile myprofile]
     end
   end
 
-  match 'plugin/' + plugin_name + '(/:action(/:id))', controller: plugin_name + '_plugin', via: :all
-  match "profile/:profile/plugin/#{plugin_name}(/:action(/:id))", controller: "#{plugin_name}_plugin_profile", profile: /#{Noosfero.identifier_format}/i, via: :all
-  match "myprofile/:profile/plugin/#{plugin_name}(/:action(/:id))", controller: "#{plugin_name}_plugin_myprofile", profile: /#{Noosfero.identifier_format}/i, via: :all
-  match 'admin/plugin/' + plugin_name + '(/:action(/:id))', controller: plugin_name + '_plugin_admin', via: :all
+  # DEPRECATED default controllers
+  paths.merge!(
+    "plugin/#{plugin_name}(/:action(/:id))": {
+      controller: "#{plugin_name}_plugin",
+      via:        :all,
+    },
+    "admin/plugin/#{plugin_name}(/:action(/:id))": {
+      controller: "#{plugin_name}_plugin_admin",
+      via:        :all,
+    },
+
+    "profile/:profile/plugin/#{plugin_name}(/:action(/:id))": {
+      controller: "#{plugin_name}_plugin_profile",
+      via:        :all,
+      profile:    profile_format,
+    },
+    "myprofile/:profile/plugin/#{plugin_name}(/:action(/:id))": {
+      controller: "#{plugin_name}_plugin_myprofile",
+      via:        :all,
+      profile:    profile_format,
+    },
+  )
+
+  paths.each do |url, opts|
+    controller_klass = "#{opts[:controller]}_controller".camelize.constantize rescue nil
+    next unless controller_klass
+
+    match url.to_s, opts
+  end
 end
 
-Dir.glob(Rails.root.join(plugins_root, '*', 'config', 'routes.rb')) do |route|
-  eval(IO.read(route), binding, route)
+Dir.glob Rails.root.join plugins_root, '*', 'config', 'routes.rb' do |route|
+  eval IO.read(route), binding, route
 end
