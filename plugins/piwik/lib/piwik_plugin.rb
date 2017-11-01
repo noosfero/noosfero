61e4d8b12b816f509e70b89a69da9199eaa483c7
Gabriel Silva <gabriel93.silva@gmail.com>
2017-11-01 15:49:11 -0300

    PiwikPulgin: Adds piwik link to user menu
    Signed-off-by: Gabriel Silva <gabriel93.silva@gmail.com>

--- plugins/piwik/lib/piwik_plugin.rb
+++ plugins/piwik/lib/piwik_plugin.rb
@@ -13,6 +13,10 @@ class PiwikPlugin < Noosfero::Plugin
     _("Tracking and web analytics to your Noosfero's environment")
   end
 
+  def stylesheet?
+    true
+  end
+
   def body_ending
     domain = context.environment.piwik_domain
     site_id = context.environment.piwik_site_id
@@ -20,6 +24,21 @@ class PiwikPlugin < Noosfero::Plugin
       piwik_url = "#{domain}/#{context.environment.piwik_path}"
       piwik_url = "#{piwik_url}/" unless piwik_url.end_with?('/')
       expanded_template('tracking-code.html.erb', {:site_id => site_id, :piwik_url => piwik_url})
+    end
+  end
+
+  def user_menu_items(user)
+    domain = context.environment.piwik_domain
+    if user.is_admin? && domain.present?
+      path = context.environment.piwik_path
+      id_site = context.environment.piwik_site_id
+      proc do
+        protocol = request.ssl? ? 'https' : 'http'
+        link_to "#{protocol}://#{domain}/#{path}?idSite=#{id_site}" do
+          content_tag('i', nil, class: 'icon-piwik') +
+          content_tag('strong', _('Piwik'))
+        end
+      end
     end
   end
 
