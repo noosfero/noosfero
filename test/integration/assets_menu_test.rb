c2473ac38f94b1fbb05d8ac344541f6fb59708af
Arthur Assis <arthur120496@gmail.com>
2017-12-14 15:33:23 -0200

    Remove a useless test at assets_menu_test.rb

--- test/integration/assets_menu_test.rb
+++ test/integration/assets_menu_test.rb
@@ -10,11 +10,6 @@ class AssetsMenuTest < ActionDispatch::IntegrationTest
     @category = Category.create!(:name => "Category A", :environment => Environment.default, :parent => parent)
   end
 
-  should 'link to uncategorized assets at site root' do
-    get '/'
-    assert_tag :tag => 'a', :attributes => { :href => '/search/contents' }
-  end
-
   should 'link to assets inside category root' do
     (1..SearchController::MULTIPLE_SEARCH_LIMIT+1).each do |i|
       enterprise = Enterprise.create! :identifier => "ent#{i}", :name => "enterprise#{i}"
