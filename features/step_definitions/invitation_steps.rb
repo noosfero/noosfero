e80b0a18f59cda7a9faddcbb47fe3599fa536fae
Matheus Richard <matheusrichardt@gmail.com>
2018-01-09 17:47:13 -0200

    Fix friend invitation method at invitation_steps.rb
    Signed-off-by: Arthur Assis <arthur120496@gmail.com>

--- features/step_definitions/invitation_steps.rb
+++ features/step_definitions/invitation_steps.rb
@@ -3,20 +3,20 @@ Given /^I invite email "(.+)" to join community "(.+)"$/ do |email, community|
   visit("/myprofile/#{identifier}/profile_members")
   first(:link, "Invite people to join").click
   choose("Email")
-  click_button('Next')
+  click_link('Next')
   fill_in('manual_import_addresses', :with => "#{email}")
   click_link('Personalize invitation mail')
   fill_in('mail_template', :with => 'Follow this link <url>')
-  click_button("Invite!")
+  click_link("Invite!")
 end
 
 Given /^I invite email "(.+)" to be my friend$/ do |email|
   click_link('Manage friends')
   click_link('Invite people')
   choose("Email")
-  click_button('Next')
+  click_link('Next')
   fill_in('manual_import_addresses', :with => "#{email}")
   click_link('Personalize invitation mail')
   fill_in('mail_template', :with => 'Follow this link <url>')
-  click_button("Invite!")
+  click_link("Invite!")
 end
