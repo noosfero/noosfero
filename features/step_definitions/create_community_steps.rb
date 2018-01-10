ea3a8e87f7d3ff4e0993d62472b4a799971b7fa3
Arthur Assis <arthur120496@gmail.com>
2018-01-10 18:31:19 -0200

    Fix tests at create_community.feature
    Signed-off-by: ThalissonMelo <tallisonmelo46@gmail.com>

--- features/step_definitions/create_community_steps.rb
+++ features/step_definitions/create_community_steps.rb
@@ -13,23 +13,23 @@ Given /^"(.+)" creates the community "(.+)"$/ do |username, community|
   click_link('Manage my groups')
   click_link('Create a new community')
   fill_in("Name", :with => community)
-  click_button("Create")
+  click_link("Create")
 end
 
 Given /^I approve community "(.+)"$/ do |community|
   task = CreateCommunity.all.select {|c| c.name == community}.first
   step %{I go to admin_user's control panel}
-  click_link('Process requests')
-  choose("decision-finish-#{task.id}")
-  first(:button, 'Apply!').click
+  click_link('Tasks')
+  click_link("Accept")
+  first(:link, 'Apply!').click
 end
 
 Given /^I reject community "(.+)"$/ do |community|
   task = CreateCommunity.all.select {|c| c.name == community}.first
   step %{I go to admin_user's control panel}
-  click_link('Process requests')
-  choose("decision-cancel-#{task.id}")
-  first(:button, 'Apply!').click
+  click_link('Tasks')
+  click_link("Reject")
+  first(:link, 'Apply!').click
 end
 
 Then /^I should see "([^\"]*)"'s creation date$/ do |community|
