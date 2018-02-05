5aac98b6cf2972069f47076e8ccdb290f3c26724
mendesiasmin <mendesiasmin96@gmail.com>
2018-02-05 09:54:56 -0200

    Revert fix dependencies in fb_app

--- plugins/fb_app/install.rb
+++ plugins/fb_app/install.rb
@@ -1,8 +1,3 @@
 system "script/noosfero-plugins -q enable oauth_client open_graph products"
-
-system 'gem install fb_graph'
-system 'gem install fb_graph2'
-system 'gem install facebook-signed-request'
-
 exit $?.exitstatus
 
