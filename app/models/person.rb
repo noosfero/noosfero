2f4f91dde6af1911f1e9a5c724e0ea5a4e4aea01
Gabriel Silva <gabriel93.silva@gmail.com>
2018-05-09 10:23:33 -0300

    Splits addres field into several fields
    Signed-off-by: Gabriel Silva <gabriel93.silva@gmail.com>

--- app/models/person.rb
+++ app/models/person.rb
@@ -265,9 +265,8 @@ class Person < Profile
   organization_website
   contact_phone
   contact_information
-  full_address
   location
-  ]
+  ] + LOCATION_FIELDS
 
   validates_multiparameter_assignments
 
