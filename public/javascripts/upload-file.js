cafc523476d6947fd61ccfe536cd1da873584ffc
Matheus Richard <matheusrichardt@gmail.com>
2018-05-11 13:24:42 -0300

    Removes hardcoded profile

--- public/javascripts/upload-file.js
+++ public/javascripts/upload-file.js
@@ -1,17 +1,17 @@
 function loadMoreComments() {
   let commentList = $('#article-comments-list');
   let pageId = commentList.data('page');
+  let profile = commentList.data('profile');
   let nextCommentPage = commentList.data('comment-page') + 1;
 
-  $.get(`/save-free-software/${pageId}/view_more_comments`, {
+  $.get(`/${profile}/${pageId}/view_more_comments`, {
     comment_page: nextCommentPage,
     id: pageId, 
     dataType: 'json'
-  }).done(function() {
+  })
+  .done(function() {
     commentList.data('comment-page', nextCommentPage);
-    setTimeout(function () {
-      $(window).bind('scroll', bindScroll);
-    }, 800)
+    $(window).bind('scroll', bindScroll);
   });
 }
 
