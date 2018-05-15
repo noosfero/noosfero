ba7ca2fc6f737827adde3174680d126efcb67b38
Gabriel Silva <gabriel93.silva@gmail.com>
2018-05-15 17:03:52 -0300

    Fixes OauthClient plugin, adds signup message
    Signed-off-by: Gabriel Silva <gabriel93.silva@gmail.com>

--- plugins/oauth_client/test/functional/oauth_client_plugin_account_controller_test.rb
+++ plugins/oauth_client/test/functional/oauth_client_plugin_account_controller_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 class AccountControllerTest < ActionController::TestCase
-
-  should 'render signup page' do
-    get :signup
+  def setup
+    Environment.default.enable_plugin OauthClientPlugin
   end
 
+  should 'not render custom div if session does not contain auuth' do
+    session[:oauth_data] = nil
+    get :signup
+    assert_no_tag :div, attributes: { id: 'oauth-signup' }
+  end
+
+  should 'render custom div if session contains auuth' do
+    session[:oauth_data] = { something: 'something' }
+    get :signup
+    assert_tag :div, attributes: { id: 'oauth-signup' }
+  end
 end
