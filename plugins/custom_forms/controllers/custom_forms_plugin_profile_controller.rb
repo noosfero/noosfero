a51025f089da9e1c72b76211766fe46f52b99fa1
Gabriel Silva <gabriel93.silva@gmail.com>
2018-06-14 18:23:56 -0300

    [CustomFormsPlugin] Adds confirmation page
    Signed-off-by: Gabriel Silva <gabriel93.silva@gmail.com>

--- plugins/custom_forms/controllers/custom_forms_plugin_profile_controller.rb
+++ plugins/custom_forms/controllers/custom_forms_plugin_profile_controller.rb
@@ -30,12 +30,17 @@ class CustomFormsPluginProfileController < ProfileController
         if not @submission.save
           raise 'Submission failed: answers not valid'
         end
-        session[:notice] = _('Submission saved')
-        redirect_to :action => 'show'
+
+        redirect_to action: :confirmation, submission_id: @submission.id
       rescue
         session[:notice] = _('Submission could not be saved')
       end
     end
+  end
+
+  def confirmation
+    @submission = CustomFormsPlugin::Submission.find_by(id: params[:submission_id])
+    render_not_found unless @submission.present?
   end
 
   def review
