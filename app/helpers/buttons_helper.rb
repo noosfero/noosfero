9710adc49d4d6aba4cbc67dd95abcda637d2a3a3
Alax Alves <alaxallves@gmail.com>
2018-06-29 16:03:21 -0300

    Creating ajax handling methods

--- app/helpers/buttons_helper.rb
+++ app/helpers/buttons_helper.rb
@@ -60,13 +60,6 @@ module ButtonsHelper
     end
   end
 
-  def link_to_function(name, function, html_options = {})
-    onclick = "#{"#{html_options[:onclick]}; " if html_options[:onclick]}#{function}; return false;"
-    href = html_options[:href] || '#'
-
-    content_tag(:a, name, html_options.merge(:href => href, :onclick => onclick))
-  end
-
   #def button(type, label, url, html_options = {})
   #  klass = 'with-text'
   #  if html_options.has_key?(:class)
