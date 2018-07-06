c6bb6bff748433689ba788f0e6c2455f18ffa2e6
David Carlos <ddavidcarlos1392@gmail.com>
2018-07-06 12:18:34 -0300

    Adapt sql method to use new models structure.

--- plugins/custom_forms/lib/custom_forms_plugin/graph.rb
+++ plugins/custom_forms/lib/custom_forms_plugin/graph.rb
@@ -1,21 +1,13 @@
 class CustomFormsPlugin::Graph
   # @query_results have the format
   # [
-  #   { "data" => { "foo"=> 5, "bla" => 7, "show_as" => "radio"} },
-  #   { "data" => { "foo"=> 5, "test" => 7, "show_as" => "check_box"} }
+  #   {
+  #     "summary"=>{}, "data"=>{"foo"=>1, "bar"=>1, "teste"=>1},
+  #     "show_as"=>"radio", "field"=>"some form field"
+  #   }
   # ]
   # Each 'data' key on @query_results represents the data that will be used by
   # chartkick lib, to render a graph based on the show_as value.
-  #
-  # @answers_with_alternative_label have the format
-  # {
-  #   123 => { "1" => {"foo" : 5}, "2" => {"bla": 7}, "show_as" => "radio"},
-  #   124 => { "21" => {"foo" : 2}, "2" => {"test": 15}, "show_as" => "check_box"}
-  # }
-  #
-  # The keys 123 and 124 are the fields ids. In the hash "1" => {"foo" : 5} "1"
-  # is the alternative id, "foo" is the alternative label and 5 is the number
-  # of users that chose this alternative as answer to it respective field.
 
   AVAILABLE_FIELDS = %w(check_box radio select multiple_select text)
 
@@ -27,16 +19,19 @@ class CustomFormsPlugin::Graph
   def compute_results
     results = report()
     field = results[0]["field"]
-    graph_data = {"summary" => {}, "data" => {}, "show_as" => "radio", "field" => field}
+    show_as = chart_to_show_data(results[0]["show_as"])
+    graph_data = {"summary" => {}, "data" => {},
+                  "show_as" => show_as , "field" => field}
     results.each do |r|
       r.delete("field")
+      r.delete("show_as")
       graph_data["data"].merge! r
     end
     @query_results = [graph_data]
   end
 
 
-  def exibition_method(show_as)
+  def chart_to_show_data(show_as)
     return 'pizza' if ["radio", "select"].include?(show_as)
     return 'column' if ["check_box", "multiple_select"].include?(show_as)
     return 'text' if show_as == 'text'
@@ -51,14 +46,16 @@ class CustomFormsPlugin::Graph
   def report()
     @form.submissions
       .joins(answers: :field)
-      .joins('INNER JOIN custom_forms_plugin_alternatives ON '\
-             'custom_forms_plugin_alternatives.id::text = custom_forms_plugin_answers.value '\
-             'AND custom_forms_plugin_alternatives.field_id = custom_forms_plugin_fields.id')
-      .where(custom_forms_plugin_fields: { type: "CustomFormsPlugin::SelectField" })
-      .group('custom_forms_plugin_alternatives.label, custom_forms_plugin_fields.name')
-      .select('COUNT(custom_forms_plugin_answers.value) as vote_count, '\
+      .joins('inner join custom_forms_plugin_alternatives ON '\
+             'custom_forms_plugin_alternatives.field_id=custom_forms_plugin_fields.id')
+      .joins('inner join custom_forms_plugin_form_answers on '\
+             'custom_forms_plugin_form_answers.alternative_id=custom_forms_plugin_alternatives.id')
+      .group('custom_forms_plugin_alternatives.label, custom_forms_plugin_form_answers.answer_id, custom_forms_plugin_fields.name, custom_forms_plugin_fields.show_as')
+      .select('COUNT(custom_forms_plugin_form_answers.answer_id) as answer_count, '\
               'custom_forms_plugin_alternatives.label as label, '\
-              'custom_forms_plugin_fields.name as field_name')
-      .map { |r| { r.label => r.vote_count, "field" => r.field_name } }
+              'custom_forms_plugin_fields.name as field_name,
+              custom_forms_plugin_fields.show_as as show_as')
+      .map { |r| { r.label => r.answer_count,
+                   "field" => r.field_name, "show_as" => r.show_as } }
   end
 end
