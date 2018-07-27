a103ff2835938d5950507211bccdad0cea0dde1c
Alax Alves <alaxallves@gmail.com>
2018-07-27 16:27:46 -0300

    Fixing unfound packages

--- db/migrate/20180720180749_set_articles_positions.rb
+++ db/migrate/20180720180749_set_articles_positions.rb
@@ -1,9 +1,9 @@
-class SetArticlesPositions < ActiveRecord::Migration
+class SetArticlesPositions < ActiveRecord::Migration[5.1]
   def up
     Article.where(position: nil).update_all(position: 0)
   end
 
   def down
-    say 'this migraiton cannot be reverted'
+    say 'this migration cannot be reverted'
   end
 end
