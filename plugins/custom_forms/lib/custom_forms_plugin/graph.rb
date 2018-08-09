daf416fede6cfc8486bba4f3af3b128d7bb3a243
alaxalves <alaxallves@gmail.com>
2018-08-09 14:57:39 -0300

    Spliting format_data method
    Signed-off-by: MatheusRich <matheusrichardt@gmail.com>

--- plugins/custom_forms/lib/custom_forms_plugin/graph.rb
+++ plugins/custom_forms/lib/custom_forms_plugin/graph.rb
@@ -38,32 +38,39 @@ class CustomFormsPlugin::Graph
   end
 
   private
-  def get_data
+  def get_select_fields
     # TODO: Use left_joins after Rails 5 upgrade
-    non_text_fields = @form.fields
-                      .joins(:alternatives)
-                      .joins('LEFT JOIN custom_forms_plugin_form_answers
-                              ON custom_forms_plugin_alternatives.id = custom_forms_plugin_form_answers.alternative_id')
-                      .group('custom_forms_plugin_fields.id, custom_forms_plugin_alternatives.id')
-                      .order('custom_forms_plugin_alternatives.id')
-                      .select('custom_forms_plugin_fields.name AS field_name,
-                               custom_forms_plugin_fields.show_as AS show_as,
-                               custom_forms_plugin_alternatives.id AS alternative,
-                               custom_forms_plugin_alternatives.label AS label,
-                               COUNT(custom_forms_plugin_form_answers.alternative_id) AS answer_count')
+    @form.fields
+         .joins(:alternatives)
+         .joins('LEFT JOIN custom_forms_plugin_form_answers
+                 ON custom_forms_plugin_alternatives.id = custom_forms_plugin_form_answers.alternative_id')
+         .group('custom_forms_plugin_fields.id, custom_forms_plugin_alternatives.id')
+         .order('custom_forms_plugin_alternatives.id')
+         .select('custom_forms_plugin_fields.name AS field_name,
+                   custom_forms_plugin_fields.show_as AS show_as,
+                   custom_forms_plugin_alternatives.id AS alternative,
+                   custom_forms_plugin_alternatives.label AS label,
+                   COUNT(custom_forms_plugin_form_answers.alternative_id) AS answer_count')
+  end
 
-    text_fields = @form.fields
-                  .joins("INNER JOIN custom_forms_plugin_answers as ans ON ans.field_id = custom_forms_plugin_fields.id
-                          INNER JOIN custom_forms_plugin_submissions as s ON s.id = ans.submission_id")
-                  .select("custom_forms_plugin_fields.id as id,
-                           custom_forms_plugin_fields.name as field_name,
-                           ans.value as answers,
-                           s.author_name as users,
-                           custom_forms_plugin_fields.show_as as show_as,
-                           ans.imported as imported")
-                  .where("custom_forms_plugin_fields.type = 'CustomFormsPlugin::TextField'")
+  def get_text_fields
+    @form.fields
+         .joins("INNER JOIN custom_forms_plugin_answers as ans ON ans.field_id = custom_forms_plugin_fields.id
+                 INNER JOIN custom_forms_plugin_submissions as s ON s.id = ans.submission_id")
+         .select("custom_forms_plugin_fields.id as id,
+                   custom_forms_plugin_fields.name as field_name,
+                   ans.value as answers,
+                   s.author_name as users,
+                   custom_forms_plugin_fields.show_as as show_as,
+                   ans.imported as imported")
+         .where("custom_forms_plugin_fields.type = 'CustomFormsPlugin::TextField'")
+  end
+
+  def get_data
+    select_fields = get_select_fields
+    text_fields = get_text_fields
     
-    { text_fields: text_fields, non_text_fields: non_text_fields }
+    { text_fields: text_fields, select_fields: select_fields }
   end
 
   def format_data(data)
@@ -87,8 +94,8 @@ class CustomFormsPlugin::Graph
       end
     end
     
-    unless data[:non_text_fields] == []
-      data[:non_text_fields].map do |r|
+    unless data[:select_fields] == []
+      data[:select_fields].map do |r|
         if computed_fields.include?(r.field_name)
           computed_data.each do |c|
             if c["field"] == r.field_name
@@ -99,7 +106,7 @@ class CustomFormsPlugin::Graph
         else
           computed_fields << r.field_name
           data = { 
-            "data" => { r.label => r.answer_count},
+            "data" => {r.label => r.answer_count},
             "field" => r.field_name,
             "show_as" => chart_to_show_data(r.show_as),
             "summary" => {}
