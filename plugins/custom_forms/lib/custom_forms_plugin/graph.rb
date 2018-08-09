add098f7637a32ab09562daef6ad29ef5fb5b094
MatheusRich <matheusrichardt@gmail.com>
2018-08-09 11:18:40 -0300

    Adds query to non-text fields
    Signed-off-by: alaxalves <alaxallves@gmail.com>

--- plugins/custom_forms/lib/custom_forms_plugin/graph.rb
+++ plugins/custom_forms/lib/custom_forms_plugin/graph.rb
@@ -40,43 +40,75 @@ class CustomFormsPlugin::Graph
   private
   def get_data
     # TODO: Use left_joins after Rails 5 upgrade
-    data = @form.fields
-          .joins(:alternatives)
-          .joins('LEFT JOIN custom_forms_plugin_form_answers '\
-                 'ON custom_forms_plugin_alternatives.id = custom_forms_plugin_form_answers.alternative_id')
-          .group('custom_forms_plugin_fields.id, custom_forms_plugin_alternatives.id')
-          .order('custom_forms_plugin_alternatives.id')
-          .select('custom_forms_plugin_fields.name AS field_name,'\
-                  'custom_forms_plugin_fields.show_as AS show_as,'\
-                  'custom_forms_plugin_alternatives.id AS alternative,'\
-                  'custom_forms_plugin_alternatives.label AS label,'\
-                  'COUNT(custom_forms_plugin_form_answers.alternative_id) AS answer_count')
+    non_text_fields = @form.fields
+                      .joins(:alternatives)
+                      .joins('LEFT JOIN custom_forms_plugin_form_answers
+                              ON custom_forms_plugin_alternatives.id = custom_forms_plugin_form_answers.alternative_id')
+                      .group('custom_forms_plugin_fields.id, custom_forms_plugin_alternatives.id')
+                      .order('custom_forms_plugin_alternatives.id')
+                      .select('custom_forms_plugin_fields.name AS field_name,
+                               custom_forms_plugin_fields.show_as AS show_as,
+                               custom_forms_plugin_alternatives.id AS alternative,
+                               custom_forms_plugin_alternatives.label AS label,
+                               COUNT(custom_forms_plugin_form_answers.alternative_id) AS answer_count')
+
+    text_fields = @form.fields
+                  .joins("INNER JOIN custom_forms_plugin_answers as ans ON ans.field_id = custom_forms_plugin_fields.id
+                          INNER JOIN custom_forms_plugin_submissions as s ON s.id = ans.submission_id")
+                  .select("custom_forms_plugin_fields.id as id,
+                           custom_forms_plugin_fields.name as field_name,
+                           ans.value as answers,
+                           s.author_name as users,
+                           custom_forms_plugin_fields.show_as as show_as,
+                           ans.imported as imported")
+                  .where("custom_forms_plugin_fields.type = 'CustomFormsPlugin::TextField'")
     
-    data
+    { text_fields: text_fields, non_text_fields: non_text_fields }
   end
 
   def format_data(data)
     computed_fields = []
     computed_data = []
+    text_fields = []
+    
+    unless data[:text_fields] == []
+      data[:text_fields].each do |f|
+        text_answers = {
+          "data" => {
+            "answers" => f.answers.map(&:value),
+            "users" => [f.users],
+            "imported" => [f.imported]
+          },
+          "show_as" => 'text',
+          "field" => f.field_name
+        }
 
-    data.map do |r|
-      if computed_fields.include?(r.field_name)
-        computed_data.each do |c|
-          if c["field"] == r.field_name
-            c["data"][r.label] = r.answer_count
-            break
+        text_fields << text_answers
+      end
+    end
+    
+    unless data[:non_text_fields] == []
+      data[:non_text_fields].map do |r|
+        if computed_fields.include?(r.field_name)
+          computed_data.each do |c|
+            if c["field"] == r.field_name
+              c["data"][r.label] = r.answer_count
+              break
+            end
           end
+        else
+          computed_fields << r.field_name
+          data = { 
+            "data" => { r.label => r.answer_count},
+            "field" => r.field_name,
+            "show_as" => chart_to_show_data(r.show_as),
+            "summary" => {}
+          }
+          computed_data << data
         end
-      else
-        computed_fields << r.field_name
-        data = { "data" => { r.label => r.answer_count},
-                 "field" => r.field_name,
-                 "show_as" => chart_to_show_data(r.show_as),
-                 "summary" => {}}
-        computed_data << data
       end
     end
 
-    computed_data
+    computed_data + text_fields
   end 
 end
