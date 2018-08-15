313cec1b9d8abcb56a256b0863ecf2344aa602fb
MatheusRich <matheusrichardt@gmail.com>
2018-08-14 23:57:06 -0300

    Running chart customizations only when charts exist

--- plugins/custom_forms/public/javascripts/graph.js
+++ plugins/custom_forms/public/javascripts/graph.js
@@ -30,38 +30,39 @@
   }
 
   $( document ).ready(function() {
-    Chart.defaults.global.plugins.datalabels.color = "white";
-    Chart.defaults.global.plugins.datalabels.formatter = function(value, context) {
-      var total = 0;
-      for (var i in context.dataset.data) {
-        total += context.dataset.data[i];
+    if(Chart.defaults.global.plugins.datalabels) {
+      Chart.defaults.global.plugins.datalabels.color = "white";
+      Chart.defaults.global.plugins.datalabels.formatter = function(value, context) {
+        var total = 0;
+        for (var i in context.dataset.data) {
+          total += context.dataset.data[i];
+        }
+        var tooltipPercentage = Math.round((value / total) * 100);
+  
+        return `${tooltipPercentage}%`;
+      };
+  
+      Chart.defaults.global.plugins.datalabels.display = function (context) {
+        return context.dataset.data[context.dataIndex] !== 0;  
       }
-      var tooltipPercentage = Math.round((value / total) * 100);
-
-      return `${tooltipPercentage}%`;
-    };
-
-    Chart.defaults.global.plugins.datalabels.display = function (context) {
-      return context.dataset.data[context.dataIndex] !== 0;  
-    }
-
-    const charts = Chartkick.charts;
-    const chartPrefix = "chart-";
-    var chartIndex = 1;
-
-    while (true){
-      var chart = charts[chartPrefix + chartIndex];
-      legend(chart, chartIndex);
-      if (typeof chart == "undefined")
-        break;
-
-      var graph = chart.getChartObject();
-      if (graph.config.type == 'pie' || graph.config.type == 'bar') {
-        chart.redraw();
+  
+      const charts = Chartkick.charts;
+      const chartPrefix = "chart-";
+      var chartIndex = 1;
+  
+      while (true){
+        var chart = charts[chartPrefix + chartIndex];
+        legend(chart, chartIndex);
+        if (typeof chart == "undefined")
+          break;
+  
+        var graph = chart.getChartObject();
+        if (graph.config.type == 'pie' || graph.config.type == 'bar') {
+          chart.redraw();
+        }
+        chartIndex += 1;
       }
-      chartIndex += 1;
     }
-
   });
 
 })();
