2e43988953ef8a9276f1f1f68d9d812613379324
MatheusRich <matheusrichardt@gmail.com>
2018-08-17 11:48:33 -0300

    Fixes migration to add form_answers on old answers

--- plugins/custom_forms/db/migrate/20180815152844_add_form_answers_on_old_answers.rb
+++ plugins/custom_forms/db/migrate/20180815152844_add_form_answers_on_old_answers.rb
@@ -5,8 +5,10 @@ class AddFormAnswersOnOldAnswers < ActiveRecord::Migration
     select_answers_without_form_answer = answers_without_form_answer.select {|a| a.field.type == "CustomFormsPlugin::SelectField"}
 
     select_answers_without_form_answer.each do |answer|
-      answer.alternatives.each do |alternative|
-        form_answer = CustomFormsPlugin::FormAnswer.create(answer_id: answer.id, alternative_id: alternative.id)
+      alternatives_ids = answer.attributes['value']
+      alternatives = CustomFormsPlugin::Alternative.where("id IN (?)", alternatives_ids)
+      alternatives.each do |alternative|
+        form_answer = CustomFormsPlugin::FormAnswer.create!(answer_id: answer.id, alternative_id: alternative.id)
         answer.form_answers << form_answer
         answer.save!
       end
@@ -18,6 +20,7 @@ class AddFormAnswersOnOldAnswers < ActiveRecord::Migration
     answers_ids = CustomFormsPlugin::FormAnswer.all.map(&:answer_id)
     answers = CustomFormsPlugin::Answer.where("id IN (?)", answers_ids)
 
+    CustomFormsPlugin::FormAnswer.delete_all
     answers.each do |answer|
       answer.form_answers = []
       answer.save!
