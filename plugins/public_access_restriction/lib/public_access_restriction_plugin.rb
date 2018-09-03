b0b7febc5ca914b27216449fafabd17d38dd1ffc
MatheusRich <matheusrichardt@gmail.com>
2018-09-03 11:40:02 -0300

    Allowes newsletter access on public_access_restriction plugin
    Signed-off-by: alaxalves <alaxallves@gmail.com>

--- plugins/public_access_restriction/lib/public_access_restriction_plugin.rb
+++ plugins/public_access_restriction/lib/public_access_restriction_plugin.rb
@@ -23,7 +23,7 @@ class PublicAccessRestrictionPlugin < Noosfero::Plugin
       params['controller'] == 'national_regions' ||
       params['controller'] == 'public_access_restriction_plugin_public_page' ||
       linked_on_portal_news(environment, params, profile) ||
-      show_newsletter(environment, profile)
+      show_newsletter?(environment, params)
     )
   end
 
@@ -67,11 +67,12 @@ class PublicAccessRestrictionPlugin < Noosfero::Plugin
       where(reference_article_id: article.id).first.present?
   end
 
-  def show_newsletter environment, profile
+  def show_newsletter? environment, params
     if environment.enabled_plugins.include?("NewsletterPlugin")
-      newsletter = NewsletterPlugin::Newsletter.find_by(environment: environment.id)
-      newsletter.blogs.find_by(profile: profile)
+      params['controller'] == 'newsletter_plugin' && params['action'] == 'mailing'
     end
+
+    false
   end
 
 end
