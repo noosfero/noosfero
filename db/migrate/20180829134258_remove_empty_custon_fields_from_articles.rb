ba6dd51b0bbbc418f2de8854743879d7e7c73fd8
Rodrigo Souto <rodrigo@colivre.coop.br>
2018-09-05 12:15:59 +0000

    Merge branch 'custom-fields-fail-validate' into 'master'
    Fix validation failure in custom fields
    
    See merge request noosfero/noosfero!1606
    
    (cherry picked from commit 902a980a1a5da36e4e389db072e81f2228df5d0b)
    
    bf889066 Fix different treatment for custom fields in new and edit methods
    19697ea4 Check if article has metadata custom_field before validate
    adb36712 Add migrate to remove invalid custom_fields in article
    84677144 Fix tratament to edit custom fields

---
+++ db/migrate/20180829134258_remove_empty_custon_fields_from_articles.rb
@@ -0,0 +1,10 @@
+class RemoveEmptyCustonFieldsFromArticles < ActiveRecord::Migration
+  def change
+    Article.find_each do |article|
+        if article.metadata.has_key?('custom_fields') && article.metadata['custom_fields'].empty?
+            article.metadata.delete('custom_fields')
+            article.save
+        end
+    end
+  end
+end
