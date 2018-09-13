30feac03616ddb273118799969f28d054c109f43
Matheus Richard <matheusrichardt@gmail.com>
2018-09-12 21:52:12 -0300

    Fixes default environment plugins

--- plugins/public_access_restriction/test/unit/public_access_restriction_test.rb
+++ plugins/public_access_restriction/test/unit/public_access_restriction_test.rb
@@ -13,10 +13,10 @@ class PublicAccessRestrictionPluginTest < ActiveSupport::TestCase
   should 'not block a common authenticated user' do
     user = fast_create Person
     profile = fast_create Community
-    assert ! @plugin.should_block?(user, @env, {}, nil)
-    assert ! @plugin.should_block?(user, @env, {controller:'any'}, profile)
-    assert ! @plugin.should_block?(user, @env, {controller:'account'}, nil)
-    assert ! @plugin.should_block?(user, @env, {controller:'home'}, nil)
+    assert_not @plugin.should_block?(user, @env, {}, nil)
+    assert_not @plugin.should_block?(user, @env, {controller:'any'}, profile)
+    assert_not @plugin.should_block?(user, @env, {controller:'account'}, nil)
+    assert_not @plugin.should_block?(user, @env, {controller:'home'}, nil)
   end
 
   should 'block an unauthenticated user on most controllers' do
@@ -28,21 +28,20 @@ class PublicAccessRestrictionPluginTest < ActiveSupport::TestCase
 
   should 'not block an unauthenticated user on home controller' do
     user = nil
-    assert ! @plugin.should_block?(user, @env, {controller:'home'}, nil)
+    assert_not @plugin.should_block?(user, @env, {controller:'home'}, nil)
   end
 
   should 'not block an unauthenticated user on portal profile' do
     user = nil
     profile = fast_create Community
     @env.stubs(:is_portal_community?).returns(profile)
-    assert ! @plugin.should_block?(user, @env, {controller:'some'}, profile)
-    assert ! @plugin.should_block?(user, @env, {controller:'content_viewer',
+    assert_not @plugin.should_block?(user, @env, {controller:'some'}, profile)
+    assert_not @plugin.should_block?(user, @env, {controller:'content_viewer',
               action:'view_page', profile:profile.identifier, page:'some'}, nil)
   end
   
   should 'not block an unauthenticated user on newsletter' do
     user, profile = nil
-    @env.stubs(:enabled_plugins).returns(["NewsletterPlugin"])
     params = { "controller" => "newsletter_plugin", "action" => "mailing" }
 
     assert @plugin.send(:show_newsletter?, @env, params, profile)
@@ -51,12 +50,12 @@ class PublicAccessRestrictionPluginTest < ActiveSupport::TestCase
 
   should 'not block an unauthenticated user on account controller' do
     user = nil
-    assert ! @plugin.should_block?(user, @env, {controller:'account'}, nil)
+    assert_not @plugin.should_block?(user, @env, {controller:'account'}, nil)
   end
 
   should 'not block an unauthenticated user on public_access_restriction plugin public_page controller' do
     user = nil
-    assert ! @plugin.should_block?(user, @env, {controller:'public_access_restriction_plugin_public_page'}, nil)
+    assert_not @plugin.should_block?(user, @env, {controller:'public_access_restriction_plugin_public_page'}, nil)
   end
 
   should 'display public page if profile says so' do
