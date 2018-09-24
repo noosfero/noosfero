70794a362c25aabc2b9d0f2857cd499f423817f8
Rodrigo Souto <rodrigo@colivre.coop.br>
2018-09-24 17:30:40 -0300

    comments: load comments on scroll only if there are comments on the page

--- public/javascripts/upload-file.js
+++ public/javascripts/upload-file.js
@@ -1,18 +1,20 @@
 function loadMoreComments() {
   var commentList = $('#article-comments-list');
-  var pageId = commentList.data('page');
-  var profile = commentList.data('profile');
-  var nextCommentPage = commentList.data('comment-page') + 1;
+  if (commentList.length > 0) {
+    var pageId = commentList.data('page');
+    var profile = commentList.data('profile');
+    var nextCommentPage = commentList.data('comment-page') + 1;
 
-  $.get('/' + profile + '/' + pageId + '/view_more_comments', {
-    comment_page: nextCommentPage,
-    id: pageId, 
-    dataType: 'json'
-  })
-  .done(function() {
-    commentList.data('comment-page', nextCommentPage);
-    $(window).bind('scroll', bindScroll);
-  });
+    $.get('/' + profile + '/' + pageId + '/view_more_comments', {
+      comment_page: nextCommentPage,
+      id: pageId,
+      dataType: 'json'
+    })
+    .done(function() {
+      commentList.data('comment-page', nextCommentPage);
+      $(window).bind('scroll', bindScroll);
+    });
+  }
 }
 
 function bindScroll() {
