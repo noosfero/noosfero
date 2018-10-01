5d58a79644318b3663de700ed752eaf1ee2830fe
Rodrigo Souto <rodrigo@colivre.coop.br>
2018-10-01 18:25:45 -0300

    public-access-restriction: add params to show_newsletter method

--- plugins/public_access_restriction/lib/public_access_restriction_plugin.rb
+++ plugins/public_access_restriction/lib/public_access_restriction_plugin.rb
@@ -23,7 +23,7 @@ class PublicAccessRestrictionPlugin < Noosfero::Plugin
       params['controller'] == 'national_regions' ||
       params['controller'] == 'public_access_restriction_plugin_public_page' ||
       linked_on_portal_news(environment, params, profile) ||
-      show_newsletter?(environment, profile) ||
+      show_newsletter?(environment, params, profile) ||
       newsletter_mail?(environment, params)
     )
   end
@@ -68,7 +68,7 @@ class PublicAccessRestrictionPlugin < Noosfero::Plugin
       where(reference_article_id: article.id).first.present?
   end
 
-  def show_newsletter? environment, profile
+  def show_newsletter? environment, params, profile
     if environment.enabled_plugins.include?("NewsletterPlugin")
       newsletter = NewsletterPlugin::Newsletter.find_by(environment: environment.id)
       newsletter.blogs.find_by(profile: profile)
