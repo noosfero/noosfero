ff6c323c41e0a1d31986be4442a5f14b045ffe9b
Gabriel Silva <gabriel93.silva@gmail.com>
2018-10-04 11:25:56 -0300

    [HTML5VideoPlugin] Fixes bug that displayed error for ongoing videos
    Signed-off-by: Gabriel Silva <gabriel93.silva@gmail.com>

--- plugins/html5_video/lib/html5_video_plugin/pool_helper.rb
+++ plugins/html5_video/lib/html5_video_plugin/pool_helper.rb
@@ -1,8 +1,11 @@
 module Html5VideoPlugin::PoolHelper
-
   def position_for(video)
     pool = VideoProcessor::PoolManager.new(Rails.root.to_s)
     pool.queue_position(video.environment_id, video.id)
   end
 
+  def is_ongoing?(video)
+    pool = VideoProcessor::PoolManager.new(Rails.root.to_s)
+    pool.is_ongoing?(video.environment_id, video.id)
+  end
 end
