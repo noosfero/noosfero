8ceaf304842457dfe314eb0089979e5c17643ca8
Alax Alves <alaxallves@gmail.com>
2018-12-07 13:01:52 -0200

    Upgrading capybara and creating proper selenium driver

--- features/support/selenium.rb
+++ features/support/selenium.rb
@@ -5,14 +5,14 @@ Capybara.register_driver :selenium do |app|
   case ENV['SELENIUM_DRIVER']
   when 'chrome'
     options = Selenium::WebDriver::Chrome::Options.new
-    driver = Selenium::WebDriver.for :chrome, options: options
+    driver = Capybara::Selenium::Driver.new app, browser: :chrome, options: options
   else
     profile = Selenium::WebDriver::Firefox::Profile.new
     profile.native_events = true
     profile["intl.accept_languages"] = "en"
     options = Selenium::WebDriver::Firefox::Options.new
     options.profile = profile
-    driver = Selenium::WebDriver.for :firefox, options: options
+    driver = Capybara::Selenium::Driver.new app, browser: :firefox, options: options
   end
 end
 
