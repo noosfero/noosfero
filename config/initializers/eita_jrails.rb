6430d46c05509639db4d7b52633467d1484335c8
Gabriel Silva <gabriel93.silva@gmail.com>
2019-01-11 16:09:29 -0200

    Fixes eita-jrails loading
    Signed-off-by: Gabriel Silva <gabriel93.silva@gmail.com>

--- config/initializers/eita_jrails.rb
+++ config/initializers/eita_jrails.rb
@@ -6,11 +6,13 @@ require 'action_view/helpers/jquery_helper'
 require 'action_view/helpers/jquery_ui_helper'
 require 'jrails/javascript_helper'
 
-ActionView::Base.class_eval do
-  include ActionView::Helpers::JqueryHelper
-  include ActionView::Helpers::JqueryUiHelper
-  include ActionView::Helpers::JavaScriptHelper
+ActiveSupport::Reloader.to_prepare do
+  ActionView::Base.class_eval do
+    include ActionView::Helpers::JqueryHelper
+    include ActionView::Helpers::JqueryUiHelper
+    include ActionView::Helpers::JavaScriptHelper
 
-  cattr_accessor :debug_rjs
-  self.debug_rjs = false
+    cattr_accessor :debug_rjs
+    self.debug_rjs = false
+  end
 end
