220005bf92ee1abaed13c78ef68dcd41524263d5
Leandro Nunes dos Santos <leandro.santos@serpro.gov.br>
2019-02-18 17:24:04 -0300

    adding content viewer routing file

---
+++ config/routes/cms/61_cms.rb
@@ -0,0 +1,13 @@
+#Noosfero::Application.routes.draw do
+#  scope ':profile', profile: /[^\/]+/ do
+##    resources :content_viewer, only: [] do
+##      collection do
+#    get '*pages', to: 'content_viewer#view_page'
+##      end
+#    
+##      member do 
+##        post :update
+##      end
+##    end
+#  end
+#end
