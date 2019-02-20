8f5ab193b44e0a899919e95e9bc43e64f815739b
Leandro Nunes dos Santos <leandro.santos@serpro.gov.br>
2019-02-20 13:51:14 -0300

    fix tests of plugin container_block

--- config/routes/admin/40_environment_design.rb
+++ config/routes/admin/40_environment_design.rb
@@ -4,7 +4,7 @@ Noosfero::Application.routes.draw do
       resources :environment_design, only: [:index] do
         collection do
           # FIXME this route should be on member
-          match 'move_block', to: 'profile_design#move_block', via: :all
+          match 'move_block', to: 'environment_design#move_block', via: :all
     
           get 'show_block_type_info'
           get 'search_autocomplete'
