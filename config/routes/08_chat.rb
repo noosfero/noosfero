da4b2dedd5eea0fde56829b18ea8fe3f3e2f13ed
Leandro Nunes dos Santos <leandronunes@gmail.com>
2019-02-19 23:20:22 -0300

    fix chat routes

--- config/routes/08_chat.rb
+++ config/routes/08_chat.rb
@@ -2,13 +2,14 @@ Noosfero::Application.routes.draw do
 
   resources :chat, only: [] do
     collection do 
+      match '', to: 'chat#index', via: :get, as: :index
+      match 'avatar/(:id)', to: 'chat#avatar', via: :get, as: :avatar
       get :start_session
       get :toggle
       post :tab
       post :join
       post :leave
       get :my_session
-      get :avatar
       get :avatars
       get :update_presence_status
       post :save_message
--- config/routes/cms/61_page.rb
+++ config/routes/cms/61_page.rb
@@ -1,5 +1,6 @@
 Noosfero::Application.routes.draw do
   scope ':profile', profile: /[^\/]+/ do
+    get '/', to: 'content_viewer#view_page', as: :view_page
     get '/*page/versions', to: 'content_viewer#article_versions', as: :versions
     get '/*page/versions_diff', to: 'content_viewer#versions_diff', as: :versions_diff
     get '/*page/view_more_comments', to: 'content_viewer#view_more_comments', as: :page_view_more_comments
