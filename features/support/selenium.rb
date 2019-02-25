677c31151e8f772a1ed90a3f700cae755bf9f025
Leandro Nunes dos Santos <leandro.santos@serpro.gov.br>
2019-02-25 16:31:53 -0300

    update selenium chrome config

--- features/support/selenium.rb
+++ features/support/selenium.rb
@@ -21,8 +21,9 @@ Capybara.register_driver :selenium do |app|
     options = ::Selenium::WebDriver::Chrome::Options.new
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--window-size=1400,1400')
+#    options.add_argument('--window-size=1400,1400')
 #    options.headless!
     driver = Capybara::Selenium::Driver.new app, browser: :chrome, options: options
   else
