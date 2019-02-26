28c002caeaae86b287b5a50c9b0db003fd913012
Leandro Nunes dos Santos <leandro.santos@serpro.gov.br>
2019-02-26 13:01:17 -0300

    activate angular-theme test

--- test/integration/angular_theme_test.rb
+++ test/integration/angular_theme_test.rb
@@ -2,13 +2,12 @@ require_relative "../test_helper"
 
 class AngularThemeTest < ActionDispatch::IntegrationTest
 
-#FIXME rails5 see a way to activate this test	
-#  should 'render index page for angular theme when access an inexistent route' do
-#    Theme.stubs(:angular_theme?).returns(true)
-#    profile = fast_create(Community)
-#    get "/myprofile/#{profile.identifier}/members"
-#    assert_response 200
-#  end
+  should 'render index page for angular theme when access an inexistent route' do
+    Theme.stubs(:angular_theme?).returns(true)
+    profile = fast_create(Community)
+    get "/myprofile/#{profile.identifier}/members"
+    assert_response 200
+  end
 
   should 'render not found when access an inexistent route' do
     Theme.stubs(:angular_theme?).returns(false)
@@ -24,13 +23,12 @@ class AngularThemeTest < ActionDispatch::IntegrationTest
     assert_response 404
   end
 
-#FIXME rails5 see a way to activate this test	
-#  should 'render index page for angular theme when access myprofile route of an angular profile' do
-#    Theme.stubs(:angular_theme?).with('noosfero').returns(false)
-#    Theme.stubs(:angular_theme?).with('angular').returns(true)
-#    profile = fast_create(Community)
-#    profile.update_attribute(:theme, 'angular')
-#    get "/myprofile/#{profile.identifier}/members"
-#    assert_response 200
-#  end
+  should 'render index page for angular theme when access myprofile route of an angular profile' do
+    Theme.stubs(:angular_theme?).with('noosfero').returns(false)
+    Theme.stubs(:angular_theme?).with('angular').returns(true)
+    profile = fast_create(Community)
+    profile.update_attribute(:theme, 'angular')
+    get "/myprofile/#{profile.identifier}/members"
+    assert_response 200
+  end
 end
