ad06a5dac74cc6d44abd759aa93d02491d8cdb8b
Leandro Nunes dos Santos <leandro.santos@serpro.gov.br>
2019-02-26 09:25:25 -0300

    fix gitlab-ci runner

--- features/support/selenium.rb
+++ features/support/selenium.rb
@@ -12,19 +12,11 @@ Capybara.register_driver :selenium do |app|
     options.headless!
     driver = Capybara::Selenium::Driver.new app, browser: :firefox, options: options
   when 'chrome'
-#    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-#      chromeOptions: { args: %w(headless disable-gpu --no-sandbox --disable-dev-shm-usage) }
-#    )
-#    driver = Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
-#    profile = Selenium::WebDriver::Chome::Profile.new
-#    options = Selenium::WebDriver::Chome::Options.new
     options = ::Selenium::WebDriver::Chrome::Options.new
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
-#    options.add_argument('--window-size=1400,1400')
-#    options.headless!
     driver = Capybara::Selenium::Driver.new app, browser: :chrome, options: options
   else
     puts '[ERROR] :: Unsupported web browser, use Firefox 60.3.0 instead.'
@@ -41,25 +33,3 @@ Capybara.default_max_wait_time = 60
 Capybara.server_host = "localhost"
 
 World(Capybara)
-
-
-#Capybara.register_driver :selenium do |app|
-#  Capybara::Selenium::Driver.load_selenium
-#  browser_options = ::Selenium::WebDriver::Chrome::Options.new
-#  browser_options.args << '--headless'
-#  browser_options.args << '--disable-gpu'
-#  # Sandbox cannot be used inside unprivileged Docker container
-#  browser_options.args << '--remote-debugging-port=9222'
-#  browser_options.args << '--proxy-bypass-list=*'
-#  browser_options.args << " --proxy-server='direct://'"
-#  browser_options.args << '--no-sandbox'
-#  browser_options.args << '--mute-audio'
-#  browser_options.args << '--hide-scrollbars'
-#  browser_options.args << '--disable-software-rasterizer'
-#  browser_options.args << '--disable-extensions'
-#  browser_options.args << '--disable-dev-shm-usage'
-#  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
-#end
-#
-#Capybara.javascript_driver = :selenium
-#World(Capybara)
