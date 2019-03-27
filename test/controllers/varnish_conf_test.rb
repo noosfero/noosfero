5315414dcdc5b55c08b7b1f639edf3f36da91538
Leandro Nunes dos Santos <leandro.santos@serpro.gov.br>
2019-03-27 16:36:18 -0300

    removing duplicate classes from integration tests

--- test/controllers/varnish_conf_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class VarnishConfTest < ActiveSupport::TestCase
-
-   def test_not_use_return_in_varnish_noosfero
-    refute system('grep "return.*pass" etc/noosfero/varnish-noosfero.vcl')
-  end
-
-end
